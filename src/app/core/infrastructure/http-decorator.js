e7ffb4337635f47ee69dfa09c09885e7f3cb34d4
Pedro De almeida <pedro.dealmeida@bit.admin.ch>
2015-03-13 14:37:02 +0100

    docs($httpDecorator): document and refactor `$httpDecorator`

--- src/app/core/infrastructure/http-decorator.js
+++ src/app/core/infrastructure/http-decorator.js
@@ -3,26 +3,27 @@
 	"use strict";
 
 	/**
+	 * Decorates the `$http` service with shortcut methods for including contextual API url.
+	 *
 	 * Reworked from standard decorator which uses $delegate to service which is called during application
 	 * initialization in app-module.js (run method) to prevent circular dependency (3rd party upload service)
 	 **/
-
 	angular
 		.module('__MODULE__.core')
 		.factory('$httpDecorator', function (CONFIG, $http) {
 
 			return {
 				decorate: function () {
-					$http.apiHead = useApiUrl($http.head);
-					$http.apiGet = useApiUrl($http.get);
-					$http.apiPost = useApiUrl($http.post);
-					$http.apiPut = useApiUrl($http.put);
-					$http.apiDelete = useApiUrl($http['delete']);
-					$http.apiPatch = useApiUrl($http.patch);
+					$http.apiHead = withApiUrl($http.head);
+					$http.apiGet = withApiUrl($http.get);
+					$http.apiPost = withApiUrl($http.post);
+					$http.apiPut = withApiUrl($http.put);
+					$http.apiDelete = withApiUrl($http['delete']);
+					$http.apiPatch = withApiUrl($http.patch);
 				}
 			};
 
-			function useApiUrl(httpFunction) {
+			function withApiUrl(httpFunction) {
 				return function () {
 					var args = [].slice.call(arguments);
 					args[0] = CONFIG.api.url + args[0];
