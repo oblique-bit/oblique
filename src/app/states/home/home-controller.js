97fdf854df312e299f3718930aa292ddc8606e6c
Pedro De almeida <pedro.dealmeida@bit.admin.ch>
2015-09-11 13:15:57 +0200

    Rework the *Home* view.

--- src/app/states/home/home-controller.js
+++ src/app/states/home/home-controller.js
@@ -5,12 +5,6 @@
 		.module('__MODULE__.home')
 
 		.controller('HomeController', function ($scope, $state) {
-			$scope.query = null;
 
-			$scope.search = function () {
-				$state.go('movies', {
-					query: $scope.query
-				});
-			};
 		});
 }());
