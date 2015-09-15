fa4c7c57035f20e8cca66f58f81dfb45a74ccf2a
Pedro De almeida <pedro.dealmeida@bit.admin.ch>
2015-09-15 18:52:43 +0200

    Add sample date filters.

--- src/app/common/common-module.js
+++ src/app/common/common-module.js
@@ -1,5 +1,30 @@
 (function () {
 	'use strict';
 
-	angular.module('__MODULE__.common', []);
+	angular.module('__MODULE__.common', [])
+		.filter("asDate", ['$filter', function ($filter) {
+			return function (input) {
+				return input ? $filter('date')(new Date(input), 'dd.MM.yyyy') : input;
+			};
+		}])
+		.filter("asLongDate", ['$filter', function ($filter) {
+			return function (input) {
+				return input ? $filter('date')(new Date(input), 'EEEE d MMMM yyyy') : input;
+			};
+		}])
+		.filter("asMediumDate", ['$filter', function ($filter) {
+			return function (input) {
+				return input ? $filter('date')(new Date(input), 'EEE, d MMM yyyy') : input;
+			};
+		}])
+		.filter("asLongMonth", ['$filter', function ($filter) {
+			return function (input) {
+				return input ? $filter('date')(new Date(input), 'MMMM yyyy') : input;
+			};
+		}])
+		.filter("asDateTime", ['$filter', function ($filter) {
+			return function (input) {
+				return input ? $filter('date')(new Date(input), 'dd.MM.yyyy HH:mm') : input;
+			};
+		}]);
 }());
