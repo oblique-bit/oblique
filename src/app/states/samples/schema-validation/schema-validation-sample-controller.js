aa9d6421572f1df93174ffe536e07acbc5f7fcc1
Schnabel Wolfgang <wolfgang.schnabel@bit.admin.ch>
2015-10-08 15:00:07 +0200

    adding date min/max validation to the datepicker:  problem left: adding parameter to date min error message  and add it to the schema validation sample

--- src/app/states/samples/schema-validation/schema-validation-sample-controller.js
+++ src/app/states/samples/schema-validation/schema-validation-sample-controller.js
@@ -1,32 +1,40 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	angular.module('__MODULE__.samples')
-	.controller('SchemaValidationSampleController', function ($scope, $state, $filter, schema, data, NotificationService) {
-		$scope.sampleSchema = schema;
-		$scope.data = data;
+    angular.module('__MODULE__.samples')
+        .controller('SchemaValidationSampleController', function ($scope, $state, $filter, schema, data, NotificationService) {
+            $scope.sampleSchema = schema;
+            
+            $scope.data = data;
+            // validating date against min/max date should come with json schema 5
+            $scope.data.minDate = moment().subtract(1, 'd').toDate();
+            $scope.data.maxDate = moment().add(1, 'd').toDate();
+            
+            $scope.today = function () {
+                $scope.data.date = Date.now();
+                $scope.data.minDate = moment($scope.data.date).subtract(1, 'd').toDate();
+                $scope.data.maxDate = moment($scope.data.date).add(1, 'd').toDate();
+            };
 
-		$scope.today = function() {
-			$scope.data.date = new Date();
-		};
+            $scope.toISO = function () {
+                $scope.data.date = (new Date()).toISOString();
+                $scope.data.minDate = moment($scope.data.date).subtract(1, 'd').format('YYYY-MM-DD');
+                $scope.data.maxDate = moment($scope.data.date).add(1, 'd').format('YYYY-MM-DD');
+            };
 
-		$scope.toISO = function() {
-			$scope.data.date = (new Date()).toISOString();
-		};
+            $scope.toTimestamp = function () {
+                $scope.data.date = Date.now();
+            };
 
-		$scope.toTimestamp = function() {
-			$scope.data.date = Date.now();
-		};
+            $scope.save = function (form) {
+                $scope.$broadcast('validationSchemaEvent');
+                if (form.$valid) {
+                    NotificationService.success('Congratulations, form is valid!');
+                }
+            };
 
-		$scope.save = function(form) {
-			$scope.$broadcast('validationSchemaEvent');
-			if(form.$valid) {
-				NotificationService.success('Congratulations, form is valid!');
-			}
-		};
-
-		$scope.jsonSource = function(data) {
-			return '<pre>' + $filter('json')(data) + '</pre>';
-		};
-	});
+            $scope.jsonSource = function (data) {
+                return '<pre>' + $filter('json')(data) + '</pre>';
+            };
+        });
 }());
