f5d18c2479ada08a841441a9812fd3d40f0da4e7
Christian Ulmann <christian.ulmann@bit.admin.ch>
2016-09-13 10:43:48 +0200

    chore(nexus): Deploy on old nexus

--- src/oblique-reactive.ts
+++ src/oblique-reactive.ts
@@ -1,7 +1,3 @@
-//TODO: remove this, as soon as typescript 2 is supported by IntelliJ
-//Fixes the types 
-/// <reference path="../typings/index.d.ts" />
-
 /*
  * This is the root-folder of oblique-reactive.
  * Every component from oblique-reactive can be imported from this file.
