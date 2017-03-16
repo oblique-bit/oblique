08e77cc00a8744f27bedcae4f60f65835eae6ba3
Christian Ulmann <christian.ulmann@bit.admin.ch>
2017-03-16 11:37:08 +0100

    feat(date-picker): add option `showErrorMessages` on the `DatepickerPopupConfig`, that handles if the error-messages should be shown by the date-picker or not (default: true)
    ```
       angular.module('module').config((uibDatepickerPopupConfig:DatepickerPopupConfig) => {
            uibDatepickerPopupConfig.showErrorMessages = true;
        });
    ```

--- src/oblique/date-picker/date-picker-module.ts
+++ src/oblique/date-picker/date-picker-module.ts
@@ -1,12 +1,16 @@
 import {DatePickerComponent} from './date-picker-component';
 import {DatePickerPopupDirective} from './date-picker-popup-directive';
 import {ORErrorMessagesModule} from '../error-messages/index';
+import {DatepickerPopupConfig} from './datepicker-config';
 
 import '../../oblique-reactive-templates';
 
 export const ORDatepickerModule = 'oblique-reactive.datepicker';
 
 angular.module(ORDatepickerModule, ['oblique-reactive.app-templates', ORErrorMessagesModule])
+    .config((uibDatepickerPopupConfig:DatepickerPopupConfig) => {
+        uibDatepickerPopupConfig.showErrorMessages = true;
+    })
     .component('datePicker', new DatePickerComponent())
     .directive('uibDatepickerPopup', (dateFilter, uibDateParser, uibDatepickerPopupConfig) => new DatePickerPopupDirective(
         dateFilter,
--- src/oblique/date-picker/datepicker-config.ts
+++ src/oblique/date-picker/datepicker-config.ts
@@ -1,6 +1,6 @@
 export interface DatepickerPopupConfig extends ng.ui.bootstrap.IDatepickerPopupConfig {
-	modelAsIsoFormat:string|boolean;
-	altInputFormats:string[];
+	modelAsIsoFormat: string|boolean;
+	altInputFormats: string[];
 	// TODO: remove this when https://github.com/DefinitelyTyped/DefinitelyTyped/issues/9342
 	minDate;
 	maxDate;
