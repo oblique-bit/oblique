f1ee0f8c91d43dbce448719d3aaba328bea5e071
René Giller <rene.giller@bit.admin.ch>
2017-04-19 11:26:29 +0200

    feat(unsavedChanges):  prepare for nested forms

--- showcase/app/samples/unsaved-changes/unsaved-changes-sample.component.ts
+++ showcase/app/samples/unsaved-changes/unsaved-changes-sample.component.ts
@@ -10,7 +10,7 @@ import {NgbTabChangeEvent} from '@ng-bootstrap/ng-bootstrap';
 	// styleUrls: ['./unsaved-changes.component.css']
 })
 export class UnsavedChangesComponent {
-	public tabs: {one: {}; two: {}; three: {}};
+	public tabs: {one: {}; two: {}; three: {}, four: {}, five: {}, six: {}};
 
 	constructor(private notificationService: NotificationService, private unsavedChangesService: UnsavedChangesService) {}
 
@@ -18,12 +18,19 @@ export class UnsavedChangesComponent {
 		this.tabs = {
 			one: {},
 			two: {},
-			three: {}
+			three: {},
+			four: {},
+			five: {},
+			six: {}
 		};
 	}
 
 	tabChange(event: NgbTabChangeEvent) {
 		this.unsavedChangesService.checkForTabChanges(event);
+	}
+
+	nestedFormTabChange(event: NgbTabChangeEvent) {
+		this.unsavedChangesService.canDeactivate();
 	}
 
 	save(form: NgForm) {
