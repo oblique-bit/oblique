33c7c4aef2ca1677acbde9f3d4f710e5c62bea11
René Giller <rene.giller@bit.admin.ch>
2017-04-19 09:28:40 +0200

    feat(unsavedChanges): cleanup

--- src/unsaved-changes/unsaved-changes.service.ts
+++ src/unsaved-changes/unsaved-changes.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import {Injectable, HostListener} from '@angular/core';
 import {ControlContainer} from '@angular/forms';
 import {TranslateService} from '@ngx-translate/core';
 import 'rxjs/add/operator/filter';
@@ -6,50 +6,68 @@ import 'rxjs/add/operator/filter';
 //TODO: Handle modals
 @Injectable()
 export class UnsavedChangesService {
-    private forms: ControlContainer[] = [];
+	private forms: ControlContainer[] = [];
+	private tabs = {};
 
-    constructor(private translateService: TranslateService) {
-        window.addEventListener('beforeunload', (e) => {
-            return this.onUnload(e);
-        });
-    }
+	constructor(private translateService: TranslateService) {
+		window.addEventListener('beforeunload', (e) => this.onUnload(e));
+	}
 
-    watch(form: ControlContainer) {
-        this.forms.push(form);
-    }
+	watch(form: ControlContainer, tab) {
+		console.log(form);
+		console.log(tab);
+		this.forms.push(form);
+		// if (tab)
+		// 	this.tabs
+	}
 
-    unWatch(form: ControlContainer) {
-        this.forms.splice(this.forms.indexOf(form));
-    }
 
-    canDeactivate(nestedForm?: ControlContainer) {
-        if ((nestedForm && nestedForm.dirty) || this.hasUnsavedChanges()) {
-            return window.confirm(this.message());
-        }
-        return true;
-    }
+	checkForChanges(event) {
+		if (this.hasPendingChanges() && !confirm(this.message()))
+			event.preventDefault();
+	}
 
-    onUnload(event: BeforeUnloadEvent) {
-        if (this.hasUnsavedChanges()) {
-            const confirmationMessage = this.message();
+	hasUnsavedChanges() {
+		return this.hasPendingChanges() || confirm(this.message());
+	}
 
-            event.returnValue = confirmationMessage;
-            return confirmationMessage;
-        }
+	watchTab(tab) {
+		console.log(tab);
+		// this.tabs[];
+	}
 
-        return null;
-    }
+	unWatch(form: ControlContainer) {
+		this.forms.splice(this.forms.indexOf(form));
+	}
 
-    private hasUnsavedChanges() {
-        for (const form of this.forms) {
-            if (form.dirty) {
-                return true;
-            }
-        }
-        return false;
-    }
+	canDeactivate(nestedForm?: ControlContainer) {
+		if ((nestedForm && nestedForm.dirty) || this.hasPendingChanges()) {
+			return window.confirm(this.message());
+		}
+		return true;
+	}
 
-    private message() {
-        return this.translateService.instant('i18n.validation.unsavedChanges');
-    }
+	onUnload(event: BeforeUnloadEvent) {
+		if (this.hasPendingChanges()) {
+			const confirmationMessage = this.message();
+
+			event.returnValue = confirmationMessage;
+			return confirmationMessage;
+		}
+
+		return null;
+	}
+
+	private hasPendingChanges() {
+		for (const form of this.forms) {
+			if (form.dirty) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private message() {
+		return this.translateService.instant('i18n.validation.unsavedChanges');
+	}
 }
