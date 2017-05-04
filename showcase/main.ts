6529f9112d9d91b47772e897a9590d2ff73734b0
Pedro De Almeida <pedro.dealmeida@bit.admin.ch>
2017-05-04 16:11:31 +0200

    Cleanup bootstrapping of root components.

--- showcase/main.ts
+++ showcase/main.ts
@@ -5,29 +5,13 @@ import {environment} from './environments/environment';
 // Project resources:
 import * as ProjectConfig from '../project.conf.js';
 
-// Oblique resources:
-import {SpinnerComponent, TopControlComponent, BrandingAppTitleComponent} from '../src';
-
 // Application resources:
-import {AppModule, BOOTSTRAP_COMPONENTS_TOKEN} from './app/app.module';
-import {AppComponent} from './app/app.component';
-import {LayoutControlsComponent} from './app/layout/controls/controls.component';
-import {LayoutNavigationComponent} from './app/layout/navigation/navigation.component';
+import {AppModule} from './app/app.module';
 
 if (environment.production) {
 	enableProdMode();
 }
 
-const components = [
-	{type: AppComponent},
-	{type: BrandingAppTitleComponent},
-	{type: SpinnerComponent},
-	{type: TopControlComponent},
-	{type: LayoutControlsComponent},
-	{type: LayoutNavigationComponent}
-];
-
 platformBrowserDynamic([
-	{provide: BOOTSTRAP_COMPONENTS_TOKEN, useValue: components},
 	{provide: 'ObliqueReactive.CONFIG', useValue: ProjectConfig.app}
 ]).bootstrapModule(AppModule);
