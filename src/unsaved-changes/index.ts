3514abdad8495e612cced6d69a19d2867db58d47
René Giller BIT <rene.giller@bit.admin.ch>
2017-05-24 11:10:00 +0200

    fix(unsavedChanges): remove unsavedChangesTabDirective

--- src/unsaved-changes/index.ts
+++ src/unsaved-changes/index.ts
@@ -2,4 +2,3 @@ export * from './unsaved-changes.guard';
 export * from './unsaved-changes.directive';
 export * from './unsaved-changes.module';
 export * from './unsaved-changes.service';
-export * from './unsaved-changes-tab.directive';
--- src/unsaved-changes/unsaved-changes.module.ts
+++ src/unsaved-changes/unsaved-changes.module.ts
@@ -3,14 +3,13 @@ import {CommonModule} from '@angular/common';
 import {UnsavedChangesDirective} from './unsaved-changes.directive';
 import {UnsavedChangesService} from './unsaved-changes.service';
 import {UnsavedChangesGuard} from './unsaved-changes.guard';
-import {UnsavedChangesTabDirective} from './unsaved-changes-tab.directive';
 
 @NgModule({
 	imports: [
 		CommonModule
 	],
-	declarations: [UnsavedChangesDirective, UnsavedChangesTabDirective],
-	exports: [UnsavedChangesDirective, UnsavedChangesTabDirective]
+	declarations: [UnsavedChangesDirective],
+	exports: [UnsavedChangesDirective]
 })
 export class UnsavedChangesModule {
 	static forRoot(): ModuleWithProviders {
