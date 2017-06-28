ff22ab8d234f321c8f0b784eedc78ef47761e6e9
René Giller BIT <rene.giller@bit.admin.ch>
2017-06-28 11:47:04 +0200

    fix(filterBox): use correct name for events and use inline template

--- src/lib/ng/filter-box/filter-box.component.ts
+++ src/lib/ng/filter-box/filter-box.component.ts
@@ -2,7 +2,26 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
 	selector: 'or-filter-box',
-	templateUrl: './filter-box.component.html',
+	template: `
+		<form novalidate>
+			<div class="form-group">
+				<div class="input-group">
+					<ng-content select="[filter-box-before]"></ng-content>
+					<div class="control-action">
+						<input class="form-control" type="text"
+							   placeholder="{{placeholder | translate}}"
+							   [ngModel]="pattern" (ngModelChange)="onPatternChanged($event)" name="search" #filterControl>
+						<button class="control-action-trigger" type="button" role="button"
+								(click)="onPatternCleared(); filterControl.focus();">
+							<span class="fa fa-times-circle"></span>
+							<span class="sr-only">{{'i18n.common.clear' | translate}}</span>
+						</button>
+					</div>
+					<ng-content select="[filter-box-after]"></ng-content>
+				</div>
+			</div>
+		</form>
+	`,
 	styles: [`
 		:host {
 			display: block;
@@ -20,19 +39,19 @@ export class FilterBoxComponent {
 	placeholder: string = 'i18n.common.search.placeholder';
 
 	@Output()
-	patternChanged = new EventEmitter<string>();
+	patternChange = new EventEmitter<string>();
 
 	@Output()
-	patternCleared = new EventEmitter();
+	patternClear= new EventEmitter();
 
 	onPatternCleared() {
 		this.pattern = undefined;
-		this.patternCleared.emit();
-		this.patternChanged.emit();
+		this.patternClear.emit();
+		this.patternChange.emit();
 	}
 
 	onPatternChanged(pattern) {
 		this.pattern = pattern;
-		this.patternChanged.emit(this.pattern);
+		this.patternChange.emit(this.pattern);
 	}
 }
--- src/showcase/app/samples/samples.module.ts
+++ src/showcase/app/samples/samples.module.ts
@@ -19,7 +19,7 @@ import {NavTreeSampleComponent, NavTreeDetailSampleComponent} from './nav-tree/n
 import {SampleDataResolver} from '../resolvers/sample-data.resolver';
 import {SamplesRoutingModule} from './samples-routing.module';
 import {SchemaValidationComponent} from './schema-validation/schema-validation-sample.component';
-import {FilterBoxSampleComponent} from './filter-box-sample/filter-box-sample.component';
+import {FilterBoxSampleComponent, PatternFilterPipe} from './filter-box-sample/filter-box-sample.component';
 
 @NgModule({
 	imports: [
@@ -44,7 +44,8 @@ import {FilterBoxSampleComponent} from './filter-box-sample/filter-box-sample.co
 		LayoutManagerSampleComponent,
 		UnsavedChangesSampleComponent,
 		UnsavedChangesSampleModalComponent,
-		FilterBoxSampleComponent
+		FilterBoxSampleComponent,
+		PatternFilterPipe
 	],
 	exports: [
 		RouterModule
