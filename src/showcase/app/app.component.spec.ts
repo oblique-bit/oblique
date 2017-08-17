2d13557a475ff48175a47c90b636972a64e148a1
Pedro De Almeida <pedro.dealmeida@bit.admin.ch>
2017-08-17 17:09:55 +0200

    Fix failing test.

--- src/showcase/app/app.component.spec.ts
+++ src/showcase/app/app.component.spec.ts
@@ -3,10 +3,11 @@
 import {TestBed, async} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {NgbTooltipModule, NgbDatepickerModule} from '@ng-bootstrap/ng-bootstrap';
 import {NotificationService, SpinnerService} from '../../lib';
 import {MockTranslatePipe} from '../../../testhelpers';
+import {RouterTestingModule} from '@angular/router/testing';
 import {TranslateService} from '@ngx-translate/core';
-import {NgbTooltipModule, NgbDatepickerModule} from '@ng-bootstrap/ng-bootstrap';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
@@ -18,6 +19,7 @@ describe('AppComponent', () => {
 				MockTranslatePipe
 			],
 			imports: [
+				RouterTestingModule,
 				NgbTooltipModule.forRoot(),
 				NgbDatepickerModule.forRoot(),
 			],
