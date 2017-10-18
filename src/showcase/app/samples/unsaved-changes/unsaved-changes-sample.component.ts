90b2f1775ca046d5af1b7d6c6dc1e43de5c4963e
René Giller BIT <rene.giller@bit.admin.ch>
2017-10-18 08:40:52 +0200

    add reactive forms to unsaved changes

--- src/showcase/app/samples/unsaved-changes/unsaved-changes-sample.component.ts
+++ src/showcase/app/samples/unsaved-changes/unsaved-changes-sample.component.ts
@@ -1,6 +1,5 @@
 import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {NotificationService} from '../../../../lib/ng/notification/notification.service';
+import {FormBuilder, FormGroup} from '@angular/forms';
 import {UnsavedChangesService} from '../../../../lib/ng/unsaved-changes/unsaved-changes.service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {UnsavedChangesSampleModalComponent} from './unsaved-changes-sample-modal.component';
@@ -9,6 +8,9 @@ import {UnsavedChangesSampleModalComponent} from './unsaved-changes-sample-modal
 	selector: 'app-unsaved-changes',
 	templateUrl: 'unsaved-changes-sample.component.html',
 	styles: [`
+		.unsaved-changes .form-horizontal label {
+			text-align: right;
+		}
 		.unsaved-changes .tab-content {
 			padding: 15px;
 			border: 1px solid #ddd;
@@ -24,36 +26,45 @@ import {UnsavedChangesSampleModalComponent} from './unsaved-changes-sample-modal
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UnsavedChangesSampleComponent implements OnInit {
-	public tabModels: {zero: any, one: any; two: any; three: any, four: any, five: any, six: any};
+	public standAloneReactive: FormGroup;
+	public nestedReactive: FormGroup;
+	public tabForm8Reactive: FormGroup;
+	public tabForm9Reactive: FormGroup;
+	public tabModels = {
+		standAloneTemplate: {},
+		nestedForm1: {},
+		nestedForm2: {},
+		nestedForm3: {},
+		tabForm1: {}
+	};
 
 
 
-	constructor(private notificationService: NotificationService,
-				private modalService: NgbModal,
+	constructor(private modalService: NgbModal,
+				private formBuilder: FormBuilder,
 				public unsavedChangesService: UnsavedChangesService) {
 	}
 
 	ngOnInit() {
-		this.tabModels = {
-			zero: {},
-			one: {},
-			two: {},
-			three: {},
-			four: {},
-			five: {},
-			six: {}
-		};
+		this.standAloneReactive = this.formBuilder.group({text: '', number: '', integer: '', date: ''});
+		this.nestedReactive = this.formBuilder.group({
+			form4: this.formBuilder.group({text: '', number: '', integer: '', date: ''}),
+			form5: this.formBuilder.group({text: '', number: '', integer: '', date: ''}),
+			form6: this.formBuilder.group({text: '', number: '', integer: '', date: ''})
+		});
+		this.tabForm8Reactive = this.formBuilder.group({text: '', number: '', integer: '', date: ''});
+		this.tabForm9Reactive = this.formBuilder.group({text: '', number: '', integer: '', date: ''});
 	}
 
-	save(form: NgForm) {
-		if (form.valid) {
-			form.resetForm();
-			this.notificationService.success('Form has been successfully saved!');
-		}
-		return false;
+	reset() {
+		this.standAloneReactive.reset();
 	}
 
 	modal() {
-		this.modalService.open(UnsavedChangesSampleModalComponent)
+		this.modalService.open(UnsavedChangesSampleModalComponent, {
+			beforeDismiss: () => {
+				return this.unsavedChangesService.discardChanges('template');
+			}
+		})
 	}
 }
