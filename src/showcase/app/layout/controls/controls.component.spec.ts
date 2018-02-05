c78071932bb956300f8facf7433b8b7bfc6aa5df
René Giller <rene.giller@bit.admin.ch>
2018-02-05 14:42:39 +0100

    fix control components tests

--- src/showcase/app/layout/controls/controls.component.spec.ts
+++ src/showcase/app/layout/controls/controls.component.spec.ts
@@ -1,4 +1,3 @@
-/* tslint:disable:no-unused-variable */
 import {MasterLayoutApplicationService} from '../../../../lib';
 import {ProjectConfig} from '../../../../../project.conf';
 
@@ -8,7 +7,7 @@ import {LayoutControlsComponent} from './controls.component';
 import {MockTranslatePipe} from '../../../../../testhelpers';
 import {TranslateService} from '@ngx-translate/core';
 
-xdescribe('LayoutControls', () => {
+describe('LayoutControls', () => {
 	const mockTranslateService = jasmine.createSpyObj('TranslateService', ['setDefaultLang', 'use']);
 	let mockLayoutApplicationService = {
 		userLang: 'en',
@@ -18,24 +17,23 @@ xdescribe('LayoutControls', () => {
 	let component: LayoutControlsComponent;
 	let fixture: ComponentFixture<LayoutControlsComponent>;
 
-	// TODO fix this
-	// beforeEach(async(() => {
-	// 	TestBed.configureTestingModule({
-	// 		providers: [
-	// 			{provide: TranslateService, useValue: mockTranslateService},
-	// 			{provide: MasterLayoutApplicationService, useValue: mockLayoutApplicationService},
-	// 			{provide: 'ObliqueReactive.CONFIG', useValue: ProjectConfig.app}
-	// 		],
-	// 		declarations: [LayoutControlsComponent, MockTranslatePipe]
-	// 	})
-	// 		.compileComponents();
-	// }));
-	//
-	// beforeEach(() => {
-	// 	fixture = TestBed.createComponent(LayoutControlsComponent);
-	// 	component = fixture.componentInstance;
-	// 	fixture.detectChanges();
-	// });
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			providers: [
+				{provide: TranslateService, useValue: mockTranslateService},
+				{provide: MasterLayoutApplicationService, useValue: mockLayoutApplicationService},
+				{provide: 'ObliqueReactive.CONFIG', useValue: ProjectConfig.app}
+			],
+			declarations: [LayoutControlsComponent, MockTranslatePipe]
+		})
+			.compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(LayoutControlsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
