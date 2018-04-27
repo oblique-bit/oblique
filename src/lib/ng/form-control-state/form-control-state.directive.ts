3eea243bf836ff34c8bdff93a0f055d2730c98bf
Louis Bernath <louis.bernath@bit.admin.ch>
2018-04-27 12:03:24 +0200

    Allow dynamic mandatory fields

--- src/lib/ng/form-control-state/form-control-state.directive.ts
+++ src/lib/ng/form-control-state/form-control-state.directive.ts
@@ -22,6 +22,7 @@ export class FormControlStateDirective extends Unsubscribable implements AfterVi
 	private form: NgForm | FormGroupDirective;
 	private group: NgModelGroup | FormGroupName;
 	private inputContainer;
+	private inputElement;
 
 	constructor(@Optional() ngForm: NgForm,
 				@Optional() formGroupDirective: FormGroupDirective,
@@ -45,15 +46,12 @@ export class FormControlStateDirective extends Unsubscribable implements AfterVi
 
 		// NOTE: [ngModel] is for template-driven forms, [formControlName] is for reactive forms and [name] is for
 		// template-driven forms with 2 ways binding because querySelector cannot match "[(ngModel)]"
-		let inputElement = this.elementRef.nativeElement.querySelector('[ngModel], [formControlName], [name]');
-		if (!inputElement) {
+		this.inputElement = this.elementRef.nativeElement.querySelector('[ngModel], [formControlName], [name]');
+		if (!this.inputElement) {
 			throw new Error('you need to provide either a ngModel or a formControlName for the FormControlStateDirective!');
 		}
 
-		this.inputContainer = inputElement.parentElement;
-		this.mandatory = this.mandatory
-			|| inputElement.hasAttribute('required')
-			|| (this.ngControl.errors && this.ngControl.errors.required);
+		this.inputContainer = this.inputElement.parentElement;
 
 		Observable.merge(
 			this.form.ngSubmit,
@@ -73,17 +71,25 @@ export class FormControlStateDirective extends Unsubscribable implements AfterVi
 		}
 	}
 
+	private isMandatory(): boolean {
+		return this.mandatory
+			|| this.inputElement.hasAttribute('required')
+			|| (this.ngControl.errors && this.ngControl.errors.required);
+	}
+
 	private generateState(): void {
 		this.hasErrorClass = (this.form.submitted || !this.ngControl.pristine || this.pristineValidation)
 			? this.ngControl.invalid
 			: false;
 
-		if (this.mandatory) {
+		if (this.isMandatory()) {
 			if (this.ngControl.value) {
 				this.renderer.removeClass(this.inputContainer, 'control-mandatory');
 			} else {
 				this.renderer.addClass(this.inputContainer, 'control-mandatory');
 			}
+		} else {
+			this.renderer.removeClass(this.inputContainer, 'control-mandatory');
 		}
 	}
 }
