f259a5d759825382b40a0df863b5493eb8c6758a
Pedro De Almeida <pedro.dealmeida@bit.admin.ch>
2018-05-03 17:11:06 +0200

    fix(datepicker-i18n): fix german translation for short months labels.
    Closes #579.
    
    (cherry picked from commit 173bb6e)

--- src/lib/ng/datepicker/datepicker-i18n.service.ts
+++ src/lib/ng/datepicker/datepicker-i18n.service.ts
@@ -5,34 +5,46 @@ import {TranslateService} from '@ngx-translate/core';
 const I18N_VALUES = {
 	en: {
 		weekdays: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-		monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+		monthsShort: [
+			'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+			'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+		],
 		monthFull: [
-			'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',
-			'December'
+			'January', 'February', 'March', 'April', 'May', 'June',
+			'July', 'August', 'September', 'October', 'November', 'December'
 		]
 	},
 	fr: {
 		weekdays: ['Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa', 'Di'],
-		monthsShort: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Déc'],
+		monthsShort: [
+			'Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin',
+			'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Déc'
+		],
 		monthFull: [
-			'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre',
-			'Décembre'
+			'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+			'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
 		]
 	},
 	de: {
 		weekdays: ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],
-		monthsShort: ['Jan', 'Feb', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+		monthsShort: [
+			'Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
+			'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'
+		],
 		monthFull: [
-			'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November',
-			'Dezember'
+			'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+			'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'
 		]
 	},
 	it: {
 		weekdays: ['Lu', 'Ma', 'Me', 'Gi', 'Ve', 'Sa', 'Do'],
-		monthsShort: ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'],
+		monthsShort: [
+			'Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu',
+			'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'
+		],
 		monthFull: [
-			'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre',
-			'Dicembre'
+			'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno',
+			'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'
 		]
 	}
 };
@@ -55,5 +67,4 @@ export class DatepickerI18nService extends NgbDatepickerI18n {
 	getMonthFullName(month: number): string {
 		return I18N_VALUES[this.translateService.currentLang].monthFull[month - 1];
 	}
-
 }
