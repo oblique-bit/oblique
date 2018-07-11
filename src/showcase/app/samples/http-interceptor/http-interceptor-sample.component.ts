4143ca9df4e11367e72d9576fd18dddbbe3fb781
Rene Giller <rene.giller@bit.admin.ch>
2018-07-11 10:17:17 +0200

    sample: add `isSilent` and `isBackground` options

--- src/showcase/app/samples/http-interceptor/http-interceptor-sample.component.ts
+++ src/showcase/app/samples/http-interceptor/http-interceptor-sample.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
-import {NotificationService, NotificationConfig, NotificationType} from '../../../../lib';
+import {NotificationService, ObliqueRequest} from '../../../../lib';
 import {HttpClient} from '@angular/common/http';
 import {ObliqueHttpInterceptorConfig} from '../../../../lib/ng/http';
+import {first} from 'rxjs/operators';
 
 @Component({
 	selector: 'oblique-http-interceptor-sample',
@@ -12,6 +13,8 @@ export class HttpInterceptorSampleComponent {
 	public API_URL = 'https://jsonplaceholder.typicode.com';
 
 	logs = [];
+	isSilent = false;
+	isBackground = false;
 
 	constructor(private notificationService: NotificationService,
 				private http: HttpClient,
@@ -23,25 +26,34 @@ export class HttpInterceptorSampleComponent {
 	request200() {
 		const url = this.API_URL + '/users';
 		this.log(`GET ${url}, expecting 200 OK...`);
+		this.configInterceptor();
 		this.http.get(url).subscribe(
 			data => {
 				this.log(`Received: ${data}`);
 			}
-		)
+		);
 	}
 
 	request404() {
 		const url = 'https://jsonplaceholder.typicode.com/unknown';
 		this.log(`GET ${url}, expecting 404 NOT FOUND...`);
+		this.configInterceptor();
 		this.http.get(url).subscribe(
 			data => {
 				this.log(`Received: ${data}`);
 			}
-		)
+		);
 	}
 
 	// Debug:
 	log(message: string) {
 		this.logs.unshift(`${this.logs.length} - ${message}`);
+	}
+
+	private configInterceptor() {
+		this.config.requestIntercepted.pipe(first()).subscribe((evt: ObliqueRequest) => {
+			evt.isSilent = this.isSilent;
+			evt.isBackground = this.isBackground;
+		});
 	}
 }
