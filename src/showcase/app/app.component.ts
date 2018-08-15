14382fe1fbd125dcaa8eae12a103a2191f81fde3
Rene Giller <rene.giller@bit.admin.ch>
2018-08-15 14:32:37 +0200

    project footer content

--- src/showcase/app/app.component.ts
+++ src/showcase/app/app.component.ts
@@ -1,8 +1,13 @@
 import {Component} from '@angular/core';
+import {ORFooterLink} from '../../lib/ng/master-layout/master-layout-footer.component';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html'
 })
 export class AppComponent {
+	footerLinks: ORFooterLink[] = [
+		{url: 'http://www.disclaimer.admin.ch', label: 'Legal', title: 'Terms and conditions'},
+		{url: 'test', label: 'test'},
+	];
 }
