d3a0f344c3ea8acac9bcac7ca56281f5c4931751
Rene Giller <rene.giller@bit.admin.ch>
2018-08-15 15:11:56 +0200

    remove footer component

--- src/showcase/app/layout/layout.module.ts
+++ src/showcase/app/layout/layout.module.ts
@@ -2,12 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {TranslateModule} from '@ngx-translate/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {MasterLayoutModule} from '../../../lib/ng';
 import {LayoutControlsComponent} from './controls/controls.component';
 import {LayoutNavigationComponent} from './navigation/navigation.component';
-import {LayoutFooterComponent} from './footer/footer.component';
-
 
 @NgModule({
 	imports: [
@@ -18,12 +15,10 @@ import {LayoutFooterComponent} from './footer/footer.component';
 	],
 	declarations: [
 		LayoutControlsComponent,
-		LayoutFooterComponent,
 		LayoutNavigationComponent
 	],
 	exports: [
 		LayoutControlsComponent,
-		LayoutFooterComponent,
 		LayoutNavigationComponent
 	]
 })
