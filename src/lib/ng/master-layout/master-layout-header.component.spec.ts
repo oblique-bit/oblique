59e356b5041c978aea7019c4dca0267d9f47e328
Rene Giller <rene.giller@bit.admin.ch>
2018-08-15 17:33:17 +0200

    fix tests

--- src/lib/ng/master-layout/master-layout-header.component.spec.ts
+++ src/lib/ng/master-layout/master-layout-header.component.spec.ts
@@ -1,25 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {EventEmitter} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
-import { MasterLayoutHeaderComponent } from './master-layout-header.component';
+import {MasterLayoutHeaderComponent} from './master-layout-header.component';
+import {MasterLayoutService} from './master-layout.service';
+import {MockTranslatePipe} from '../../../../testhelpers';
 
-describe('MasterLayoutHeaderComponent', () => {
-  let component: MasterLayoutHeaderComponent;
-  let fixture: ComponentFixture<MasterLayoutHeaderComponent>;
+fdescribe('MasterLayoutHeaderComponent', () => {
+	let component: MasterLayoutHeaderComponent;
+	let fixture: ComponentFixture<MasterLayoutHeaderComponent>;
+	const mockTranslateService = jasmine.createSpyObj('TranslateService', ['setDefaultLang', 'use', 'getDefaultLang']);
+	mockTranslateService.onLangChange = new EventEmitter();
+	mockTranslateService.userLang = 'en';
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MasterLayoutHeaderComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports: [RouterTestingModule],
+			declarations: [MasterLayoutHeaderComponent, MockTranslatePipe],
+			providers: [MasterLayoutService, {provide: TranslateService, useValue: mockTranslateService}]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MasterLayoutHeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(MasterLayoutHeaderComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
