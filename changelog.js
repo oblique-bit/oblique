ecb06894479544e2635508532fbe03757ad75d56
René Giller <rene.giller@bit.admin.ch>
2018-11-27 09:52:15 +0100

    chore: add post changelog task to fix stash links

--- changelog.js
+++ changelog.js
@@ -8,7 +8,9 @@ conventionalChangelog({
 	preset: 'angular'
 }).pipe(writestream);
 writestream.on('finish', function () {
-	fs.appendFileSync('CHANGELOG.md', changelog);
+	fs.appendFileSync('CHANGELOG.md', changelog)
+	fs.readFile('CHANGELOG.md', 'utf-8', function(err, data) {
+		var result = data.replace(/\/scm\/oui\/oblique2-reactive\/compare\/(?:\d\.\d\.\d)(?:-RC\.\d)?\.\.\.(.+)/, '/projects/OUI/repos/oblique2-reactive/browse?at=$1')
+		fs.writeFileSync('CHANGELOG.md', result);
+	});
 });
-
-
