80e336233c4f36b95031c4f5a4c91f2e3299e363
René Giller <rene.giller@bit.admin.ch>
2019-06-18 14:31:48 +0200

    feat(themes): provide minified css files in the dist
    OUI-675

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,6 +3,8 @@ const fs = require('fs'),
 	git = require('gulp-git'),
 	gulpFile = require('gulp-file'),
 	header = require('gulp-header'),
+	sass = require('node-sass'),
+	path = require('path'),
 	replace = require('gulp-replace'),
 	paths = {
 		dist: './dist/oblique-reactive/'
@@ -17,6 +19,12 @@ const fs = require('fs'),
 
 const distStyles = () =>
 	gulp.src(['projects/oblique-reactive/src/styles/**/*']).pipe(gulp.dest(paths.dist + 'styles'));
+
+const distMaterialCss = (done) => transpile('material', 'themes', done);
+const distBootstrapCss = (done) => transpile('bootstrap', 'themes', done);
+const distCoreCss = (done) => transpile('core', '', done);
+const distUtilCss = (done) => transpile('utilities', '', done);
+const distComponentsCss = (done) => transpileComponents('projects/oblique-reactive/src/lib', done);
 
 const distTestHelpers = () => gulp.src(['test_helpers/*']).pipe(gulp.dest(paths.dist + 'test_helpers'));
 
@@ -43,17 +51,33 @@ const distBundle = () => {
 		.pipe(gulp.dest(paths.dist + 'bundles'));
 };
 
+const distCss = () => {
+	const meta = reload('./package.json');
+	return gulp.src(paths.dist + 'styles/css/*')
+		.pipe(header(banner(meta)))
+		.pipe(gulp.dest(paths.dist + 'styles/css'));
+};
+
 const commit = () => gulp.src('.')
 	.pipe(git.add())
 	.pipe(git.commit('chore(version): release version ' + getPackageJsonVersion()));
-
 
 gulp.task(
 	'dist',
 	gulp.parallel(
 		distTestHelpers,
 		distMeta,
-		distStyles,
+		gulp.series(
+			distStyles,
+			gulp.parallel(
+				distMaterialCss,
+				distBootstrapCss,
+				distCoreCss,
+				distUtilCss,
+				distComponentsCss
+			),
+			distCss
+		),
 		distBundle
 	)
 );
@@ -62,6 +86,7 @@ gulp.task(
 	'publish',
 	gulp.series(commit)
 );
+
 
 function reload(module) {
 	// Uncache module:
@@ -75,4 +100,80 @@ function getPackageJsonVersion() {
 	// We parse the json file instead of using require because require caches
 	// multiple calls so the version number won't be updated
 	return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+}
+
+function fixPath(url, prev, relative) {
+	if (!url.startsWith('~')) {
+		return url;
+	}
+
+	if (!relative) {
+		return url.replace('~', 'node_modules/');
+	}
+
+	const level = prev.split('/').reverse().indexOf('scss');
+	const base = ['..', '..', '..', 'node_modules', ''];
+	for (let i = 0; i < level; i++) {
+		base.unshift('..');
+	}
+	return url.replace('~', base.join('/'));
+
+}
+
+function transpile(target, dir, cb) {
+	transpileFile('dist/oblique-reactive/styles/scss/' + dir + '/oblique-' + target + '.scss', target, true, cb);
+}
+
+function transpileFile(file, target, relative, cb) {
+	sass.render({
+		file: file,
+		importer: (url, prev, cbb) => {
+			cbb({file: fixPath(url, prev, relative)});
+		},
+		outputStyle: 'compressed',
+		sourceMap: false, // doesn't get generated correctly
+		outFile: 'dist/oblique-reactive/styles/css/oblique-' + target + '.css'
+	}, function (error, result) {
+		if (error) {
+			console.log(error.message);
+		} else {
+			if (!fs.existsSync('dist/oblique-reactive/styles/css')) {
+				fs.mkdirSync('dist/oblique-reactive/styles/css');
+			}
+			fs.writeFile('dist/oblique-reactive/styles/css/oblique-' + target + '.css', result.css, (err) => {
+				if (err) {
+					console.log(err);
+				}
+				cb();
+			});
+		}
+	});
+}
+
+function deleteFile(component) {
+	if (fs.existsSync(component)) {
+		fs.unlinkSync(component);
+	}
+}
+
+function generateComponentsStyles(dir, component) {
+	fs.readdirSync(dir).forEach(d => {
+		if (fs.statSync(path.join(dir, d)).isDirectory()) {
+			fs.readdirSync(path.join(dir, d)).forEach(f => {
+				if (f.endsWith('scss')) {
+					fs.appendFileSync(component, '@import "' + path.join(dir, d, f) + '";\n');
+				}
+			});
+		}
+	});
+}
+
+function transpileComponents(dir, cb) {
+	const component = 'components.scss';
+	deleteFile(component);
+	generateComponentsStyles(dir, component);
+	transpileFile('components.scss', 'components', false, () => {
+		deleteFile(component);
+		cb();
+	});
 }
