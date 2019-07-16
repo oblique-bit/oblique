686f8d7ceaf6e263c87e2328df852fedae83abea
René Giller <rene.giller@bit.admin.ch>
2019-07-16 07:15:52 +0200

    refactor(toggle): use @Input() instead of classes for toggle direction
    OUI-797
    BREAKING CHANGE:
    * 'toggle' class is set automatically by the directive and shouldn't be manually specified
    * `toggle-*-*` direction classes have been removed in favor of values passed to the `orToggle` directive:
    ** `down-up` (default)
    ** `down-right`
    ** `down-left`
    ** `up-down`
    ** `up-right`
    ** `up-left`
    ** `right-left`
    ** `right-down`
    ** `right-up`
    ** `left-right`
    ** `left-down`
    ** `left-up`

--- projects/oblique-reactive/src/lib/toggle/toggle.directive.ts
+++ projects/oblique-reactive/src/lib/toggle/toggle.directive.ts
@@ -1,12 +1,23 @@
-import {Directive, HostBinding, HostListener} from '@angular/core';
+import {Directive, HostBinding, HostListener, Input, OnInit} from '@angular/core';
 
 @Directive({
 	selector: '[orToggle]',
 	exportAs: 'orToggle'
 })
-export class ToggleDirective {
+export class ToggleDirective implements OnInit {
 	@HostBinding('class.show') active = false;
+	@HostBinding('class') @Input('class') hostClass: string;
+	@Input('orToggle') direction: string;
+
 	@HostListener('click') activate() {
 		this.active = !this.active;
+	}
+
+	ngOnInit() {
+		// use a Set to ensure there are no duplicates
+		const classes = new Set((this.hostClass || '').split(' '))
+			.add('toggle')
+			.add(`toggle-${this.direction || 'down-up'}`);
+		this.hostClass = Array.from(classes).join(' ');
 	}
 }
