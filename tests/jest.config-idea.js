15416d2e40f84ed489449104bbe9bebf1ef03fbc
René Giller <rene.giller@bit.admin.ch>
2019-11-25 10:08:12 +0100

    feat(toolchain): remove `test_helpers`
    OUI-870
    BREAKING CHANGE:
    `test_helpers` directory have been removed in favor of `ObliqueTestingModule`

--- tests/jest.config-idea.js
+++ tests/jest.config-idea.js
@@ -5,6 +5,5 @@ module.exports = require('../../tests/jest.config.js');
 module.exports.rootDir =  '/home/dev/development/projects/oblique2-reactive/';
 module.exports.setupFilesAfterEnv = ['/home/dev/development/projects/oblique2-reactive/tests/setupJest.ts'];
 module.exports.moduleNameMapper = {
-	'oblique': '/home/dev/development/projects/oblique2-reactive/projects/oblique/src/public_api.ts',
-	'tests': '/home/dev/development/projects/oblique2-reactive/test_helpers'
+	'oblique': '/home/dev/development/projects/oblique2-reactive/projects/oblique/src/public_api.ts'
 };
