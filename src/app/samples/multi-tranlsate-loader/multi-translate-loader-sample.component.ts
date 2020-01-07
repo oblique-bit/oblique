8ecea04a89ad34293e7dd92c727b7d092848c0b2
René Giller <rene.giller@bit.admin.ch>
2020-01-07 14:28:24 +0100

    showcase(multi-translate-loader): document feature
    OUI-920

---
+++ src/app/samples/multi-tranlsate-loader/multi-translate-loader-sample.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app-multi-tranlsate-loader',
+	templateUrl: './multi-tranlsate-loader-sample.component.html'
+})
+export class MultiTranslateLoaderSampleComponent {
+}
