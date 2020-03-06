f12978702c932c56e06f623d9a8b11dcc1da1326
René Giller <rene.giller@bit.admin.ch>
2020-03-06 22:24:33 +0100

    fix(toolchain): fix SCSS import paths in the library
    OUI-968

--- gulpfile.ts
+++ gulpfile.ts
@@ -26,6 +26,18 @@ const distUtilCss = (done) => transpile('utilities', '', done);
 const distCompatCss = (done) => transpile('compat', '', done);
 const distComponentsCss = (done) => transpileComponents(`${paths.src}/lib`, done);
 
+const distStylesThemeRename = () => gulp.src(`${paths.dist}/styles/scss/**/*.scss`)
+	.pipe(replace('projects/oblique/src', paths.dist))
+	.pipe(gulp.dest(`${paths.dist}/styles/scss`));
+
+const distStylesDistRename = () => gulp.src(`${paths.dist}/styles/scss/**/*.scss`)
+	.pipe(replace(paths.dist, '~@oblique/oblique'))
+	.pipe(gulp.dest(`${paths.dist}/styles/scss`));
+
+const distCompatRename = () => gulp.src(`${paths.dist}/styles/css/oblique-compat.css`)
+	.pipe(replace('~@oblique/oblique/styles/', '../'))
+	.pipe(gulp.dest(`${paths.dist}/styles/css`));
+
 const addBanner = () => gulp.src([`${paths.dist}/**/*.js`, `${paths.dist}/**/*.css`])
 	.pipe(header(
 		`/**
@@ -91,6 +103,7 @@ gulp.task(
 		distFontAwesome,
 		gulp.series(
 			distStyles,
+			distStylesThemeRename,
 			gulp.parallel(
 				distMaterialCss,
 				distBootstrapCss,
@@ -99,6 +112,7 @@ gulp.task(
 				distCompatCss,
 				distComponentsCss
 			),
+			distCompatRename,
 			distScss,
 			distCss,
 			distRename,
@@ -106,6 +120,7 @@ gulp.task(
 				addBanner,
 				distMap
 			),
+			distStylesDistRename,
 			clean
 		)
 	)
@@ -119,6 +134,7 @@ gulp.task(
 gulp.task('themes',
 	gulp.series(
 		distStyles,
+		distStylesThemeRename,
 		gulp.parallel(
 			distMaterialCss,
 			distBootstrapCss,
