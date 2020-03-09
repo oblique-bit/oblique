61d60175ef395540b8f200c7116da2fce2fe1fef
René Giller <rene.giller@bit.admin.ch>
2020-03-09 10:18:16 +0100

    fix(master-layout): fix css selector name

--- projects/oblique/src/lib/off-canvas/mock/mock-off-canvas-container.directive.ts
+++ projects/oblique/src/lib/off-canvas/mock/mock-off-canvas-container.directive.ts
@@ -1,7 +1,7 @@
 import {Directive} from '@angular/core';
 
 @Directive({
-	selector: '.offcanvas, or-master-layout'
+	selector: '.offcanvas, ob-master-layout'
 })
 export class ObMockOffCanvasContainerDirective {
 	open = false;
