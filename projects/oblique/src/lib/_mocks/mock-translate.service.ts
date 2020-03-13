f585e64961fdc26dae89d31657a9b9b3288108ad
René Giller <rene.giller@bit.admin.ch>
2020-03-13 23:27:04 +0100

    fix(toolchain): add missing functions and properties to mock translate

--- projects/oblique/src/lib/_mocks/mock-translate.service.ts
+++ projects/oblique/src/lib/_mocks/mock-translate.service.ts
@@ -7,6 +7,8 @@ export class ObMockTranslateService {
 	onLangChange = new EventEmitter<LangChangeEvent>();
 	onTranslationChange = new EventEmitter<TranslationChangeEvent>();
 	onDefaultLangChange = new EventEmitter<DefaultLangChangeEvent>();
+	defaultLang = 'en';
+	langs = ['en'];
 
 	currentLang(): string {
 		return 'en';
@@ -20,14 +22,49 @@ export class ObMockTranslateService {
 		return 'en';
 	}
 
+	setDefaultLang(lang: string): void {
+	}
+
 	get(): Observable<string> {
 		return of('');
 	}
 
-	use(lang: string): void {
+	use(lang: string): Observable<any> {
+		return of('');
 	}
 
 	instant(string: string): string {
 		return string;
+	}
+
+	getTranslation(lang: string): Observable<any> {
+		return of('');
+	}
+
+	getLangs(): Array<string> {
+		return this.langs;
+	}
+
+	addLangs(langs: Array<string>): void {
+		this.langs = langs;
+	}
+
+	getParsedResult(translations: any, key: any, interpolateParams?: Object): any {
+		return '';
+	}
+
+	set(key: string, value: string, lang?: string): void {
+	}
+
+	reloadLang(lang: string): Observable<any> {
+		return of('');
+	}
+
+	resetLang(lang: string): void {
+
+	}
+
+	getBrowserCultureLang(): string {
+		return 'en';
 	}
 }
