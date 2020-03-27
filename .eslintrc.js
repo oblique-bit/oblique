f22cb7adec0f9f76d7095e6e3e502cd48d6ca4ae
Stefan Tanner <stefan.tanner@bit.admin.ch>
2020-03-27 10:41:25 +0100

    feat(toolchain): add `prettier`
    OUI-764

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,153 +1,92 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": "tsconfig.json",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-		"angular",
-		"@angular-eslint"
-    ],
-    "rules": {
-        "@typescript-eslint/class-name-casing": "error",
-        "@typescript-eslint/consistent-type-definitions": "error",
-        "@typescript-eslint/explicit-member-accessibility": [
-            "off",
-            {
-                "accessibility": "explicit"
-            }
-        ],
-        "@typescript-eslint/indent": [
-            "error",
-			"tab"
-        ],
-        "@typescript-eslint/member-delimiter-style": [
-            "error",
-            {
-                "multiline": {
-                    "delimiter": "semi",
-                    "requireLast": true
-                },
-                "singleline": {
-                    "delimiter": "semi",
-                    "requireLast": false
-                }
-            }
-        ],
-        "@typescript-eslint/member-ordering": [
-            "error",
-            {
-                "default": [
-                    "public-static-field",
-                    "public-instance-field",
-                    "protected-static-field",
-                    "protected-instance-field",
-                    "private-static-field",
-                    "private-instance-field",
-                    "public-constructor",
-                    "protected-constructor",
-                    "private-constructor",
-                    "public-static-method",
-                    "public-instance-method",
-                    "protected-static-method",
-                    "protected-instance-method",
-                    "private-static-method",
-                    "private-instance-method"
-                ]
-            }
-        ],
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-empty-interface": "error",
-        "@typescript-eslint/no-inferrable-types": "error",
-        "@typescript-eslint/no-misused-new": "error",
-        "@typescript-eslint/no-non-null-assertion": "error",
-        "@typescript-eslint/prefer-function-type": "error",
-        "@typescript-eslint/prefer-readonly": "error",
-        "@typescript-eslint/quotes": [
-            "error",
-            "single"
-        ],
-        "@typescript-eslint/semi": [
-            "error",
-            "always"
-        ],
-        "@typescript-eslint/type-annotation-spacing": "error",
-        "@typescript-eslint/unified-signatures": "error",
-        "arrow-body-style": "error",
-        "camelcase": "off",
-        "constructor-super": "error",
-        "curly": "error",
-        "dot-notation": "off",
-        "eol-last": "error",
-        "eqeqeq": [
-            "error",
-            "smart"
-        ],
-        "guard-for-in": "error",
-        "id-blacklist": "off",
-        "id-match": "off",
-        "max-len": [
-            "error",
-            {
-                "code": 160
-            }
-        ],
-        "no-bitwise": "error",
-        "no-caller": "error",
-        "no-console": [
-            "error",
-            {
-                "allow": [
-                    "log",
-                    "dirxml",
-                    "warn",
-                    "error",
-                    "dir",
-                    "timeLog",
-                    "assert",
-                    "clear",
-                    "count",
-                    "countReset",
-                    "group",
-                    "groupCollapsed",
-                    "groupEnd",
-                    "table",
-                    "Console",
-                    "markTimeline",
-                    "profile",
-                    "profileEnd",
-                    "timeline",
-                    "timelineEnd",
-                    "timeStamp",
-                    "context"
-                ]
-            }
-        ],
-        "no-debugger": "error",
-        "no-empty": "off",
-        "no-eval": "error",
-        "no-fallthrough": "error",
-        "no-new-wrappers": "error",
-        "no-restricted-imports": "error",
-        "no-shadow": [
-            "error",
-            {
-                "hoist": "all"
-            }
-        ],
-        "no-throw-literal": "error",
-        "no-trailing-spaces": "error",
-        "no-undef-init": "error",
-        "no-underscore-dangle": "off",
-        "no-unused-expressions": "error",
-        "no-unused-labels": "error",
-        "no-var": "error",
-        "prefer-const": "error",
+	"env": {
+		"browser": true,
+		"node": true
+	},
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"project": "tsconfig.json",
+		"sourceType": "module",
+		"ecmaFeatures": {
+			"modules": true
+		}
+	},
+	"plugins": [
+		"@typescript-eslint",
+		"@angular-eslint",
+		"prettier"
+	],
+	"extends": ["plugin:prettier/recommended"],
+	"rules": {
+		"@typescript-eslint/explicit-member-accessibility": "off",
+		"@typescript-eslint/member-ordering": [
+			"error",
+			{
+				"default": [
+					"public-static-field",
+					"public-instance-field",
+					"protected-static-field",
+					"protected-instance-field",
+					"private-static-field",
+					"private-instance-field",
+					"public-constructor",
+					"protected-constructor",
+					"private-constructor",
+					"public-static-method",
+					"public-instance-method",
+					"protected-static-method",
+					"protected-instance-method",
+					"private-static-method",
+					"private-instance-method"
+				]
+			}
+		],
+		"@typescript-eslint/prefer-readonly": "error",
+		"@typescript-eslint/unified-signatures": "error",
+		"arrow-parens": ["error", "as-needed"],
+		"arrow-body-style": "error",
+		"constructor-super": "error",
+		"curly": "error",
+		"dot-notation": "off",
+		"eol-last": "error",
+		"eqeqeq": ["error", "smart"],
+		"guard-for-in": "error",
+		"max-len": [
+			"error",
+			{
+				"code": 160
+			}
+		],
+		"no-bitwise": "error",
+		"no-caller": "error",
+		"no-console": [
+			"error",
+			{
+				"allow": [
+					"info",
+					"warn",
+					"error"
+				]
+			}
+		],
+		"no-debugger": "error",
+		"no-empty": "off",
+		"no-eval": "error",
+		"no-fallthrough": "error",
+		"no-new-wrappers": "error",
+		"no-restricted-imports": "error",
+		"no-shadow": [
+			"error",
+			{
+				"hoist": "all"
+			}
+		],
+		"no-throw-literal": "error",
+		"no-trailing-spaces": "error",
+		"no-undef-init": "error",
+		"no-underscore-dangle": "off",
+		"no-unused-expressions": "error",
+		"no-unused-labels": "error",
 		"radix": "error",
 		"object-curly-spacing": ["error", "never"],
 		'@angular-eslint/component-class-suffix': "error",
@@ -171,6 +110,6 @@ module.exports = {
 				"style": "camelCase"
 			}
 		],
-		"function-paren-newline": ["error", "multiline-arguments"]
-    }
+		"prettier/prettier": ["error"]
+	}
 };
