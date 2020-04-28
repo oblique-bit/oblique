1a425582e46410a8c8006236638a810158a18c8e
René Giller <rene.giller@bit.admin.ch>
2020-04-28 10:24:45 +0200

    fix(error-messages): unsubscribe from Observable in `mat-error`
    OUI-1025

--- projects/oblique/src/lib/error-messages/mat-error.directive.ts
+++ projects/oblique/src/lib/error-messages/mat-error.directive.ts
@@ -1,23 +1,25 @@
 import {Directive, ElementRef, OnInit, Optional} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
-import {filter} from 'rxjs/operators';
+import {filter, takeUntil} from 'rxjs/operators';
 import {ObTranslateParamsPipe} from '../translate-params/translate-params.module';
+import {ObUnsubscribable} from '../unsubscribe.class';
 import {ObErrorMessagesDirective} from './error-messages.directive';
 
 @Directive({
 	// eslint-disable-next-line @angular-eslint/directive-selector
 	selector: 'mat-error'
 })
-export class ObMatErrorDirective implements OnInit {
+export class ObMatErrorDirective extends ObUnsubscribable implements OnInit {
 	private readonly pipe: ObTranslateParamsPipe;
 
 	constructor(@Optional() private readonly control: ObErrorMessagesDirective, private readonly el: ElementRef, translate: TranslateService) {
+		super();
 		this.pipe = new ObTranslateParamsPipe(translate);
 	}
 
 	ngOnInit() {
 		if (this.control) {
-			this.control.errors$.pipe(filter(evt => !!evt)).subscribe(evt => {
+			this.control.errors$.pipe(filter(evt => !!evt), takeUntil(this.unsubscribe)).subscribe(evt => {
 				this.el.nativeElement.innerText = Object.keys(evt).map(key =>
 					this.pipe.transform(`i18n.validation.${key}`, evt[key])).join('\n');
 			});
