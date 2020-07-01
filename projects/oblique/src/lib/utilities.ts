f43c6f873e4bc47b5a42e0c119a08b3b794ef0e6
Stefan Tanner <stefan.tanner@bit.admin.ch>
2020-07-01 14:35:09 +0200

    feat(utilities): add `OB_MATERIAL_CONFIG` to overwrite default Material config
    OUI-792

--- projects/oblique/src/lib/utilities.ts
+++ projects/oblique/src/lib/utilities.ts
@@ -1,7 +1,9 @@
 import {HttpClient} from '@angular/common/http';
-import {InjectionToken, Optional} from '@angular/core';
+import {InjectionToken, Optional, Provider} from '@angular/core';
 import {TranslateLoader, TranslateModuleConfig} from '@ngx-translate/core';
-import {ObMultiTranslateLoader, TRANSLATION_FILES, ObITranslationFile} from './multi-translate-loader/multi-translate-loader';
+import {ObITranslationFile, ObMultiTranslateLoader, TRANSLATION_FILES} from './multi-translate-loader/multi-translate-loader';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions} from '@angular/material/form-field';
+import {STEPPER_GLOBAL_OPTIONS, StepperOptions} from '@angular/cdk/stepper';
 
 export const WINDOW = new InjectionToken<Window>('Window');
 
@@ -21,5 +23,34 @@ export function multiTranslateLoader(config: TranslateModuleConfig = {}): Transl
 }
 
 export function getTranslateLoader(http: HttpClient, files: ObITranslationFile[]): ObMultiTranslateLoader {
-	return new ObMultiTranslateLoader(http, [{prefix: './assets/styles/i18n/', suffix: '.json'}, ...(files || [{prefix: './assets/i18n/', suffix: '.json'}])]);
+	return new ObMultiTranslateLoader(http, [
+		{
+			prefix: './assets/styles/i18n/',
+			suffix: '.json'
+		},
+		...(files || [{prefix: './assets/i18n/', suffix: '.json'}])
+	]);
+}
+
+export function matFormFieldDefaultOptionsProvider(config?: ObIMaterialConfig): MatFormFieldDefaultOptions {
+	return config?.MAT_FORM_FIELD_DEFAULT_OPTIONS || {appearance: 'outline'};
+}
+
+export function stepperOptionsOptionsProvider(config?: ObIMaterialConfig): StepperOptions {
+	return config?.STEPPER_GLOBAL_OPTIONS || {displayDefaultIndicatorType: false};
+}
+
+export interface ObIMaterialConfig {
+	MAT_FORM_FIELD_DEFAULT_OPTIONS?: MatFormFieldDefaultOptions;
+	STEPPER_GLOBAL_OPTIONS?: StepperOptions;
+}
+
+export const OB_MATERIAL_CONFIG = new InjectionToken<ObIMaterialConfig>('ObIMaterialConfig');
+
+export function obliqueProviders(): Provider[] {
+	return [
+		{provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useFactory: matFormFieldDefaultOptionsProvider, deps: [[new Optional(), OB_MATERIAL_CONFIG]]},
+		{provide: STEPPER_GLOBAL_OPTIONS, useFactory: stepperOptionsOptionsProvider, deps: [[new Optional(), OB_MATERIAL_CONFIG]]},
+		{provide: WINDOW, useFactory: windowProvider}
+	];
 }
