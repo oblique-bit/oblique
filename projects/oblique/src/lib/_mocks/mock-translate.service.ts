6f7f71675d0d3f62dacddfb8283358af45c98aa5
Rene Giller <rene.giller@bit.admin.ch>
2020-07-02 10:11:11 +0200

    fix: fix `ObMockTranslateService`

--- projects/oblique/src/lib/_mocks/mock-translate.service.ts
+++ projects/oblique/src/lib/_mocks/mock-translate.service.ts
@@ -1,17 +1,25 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {DefaultLangChangeEvent, LangChangeEvent, TranslationChangeEvent} from '@ngx-translate/core';
-import {Observable, of} from 'rxjs';
+import {EMPTY, Observable, of} from 'rxjs';
 
 @Injectable()
 export class ObMockTranslateService {
+	translations: any;
 	onLangChange = new EventEmitter<LangChangeEvent>();
 	onTranslationChange = new EventEmitter<TranslationChangeEvent>();
 	onDefaultLangChange = new EventEmitter<DefaultLangChangeEvent>();
 	defaultLang = 'en';
 	langs = ['en'];
+	currentLang = 'en';
 
-	currentLang(): string {
-		return 'en';
+	setTranslation(lang: string, translations: Object, shouldMerge?: boolean): void {}
+
+	getStreamOnTranslationChange(key: string | string[], interpolateParams?: Object): Observable<any> {
+		return EMPTY;
+	}
+
+	stream(key: string | string[], interpolateParams?: Object): Observable<any> {
+		return EMPTY;
 	}
 
 	getBrowserLang(): string {
