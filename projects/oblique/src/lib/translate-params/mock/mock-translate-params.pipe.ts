ae8ebb56f761d389a09d976e6e69b9980a818f3b
Rene Giller <rene.giller@bit.admin.ch>
2020-07-13 21:42:37 +0200

    fix(translate-params): mock returns given value instead of empty string

--- projects/oblique/src/lib/translate-params/mock/mock-translate-params.pipe.ts
+++ projects/oblique/src/lib/translate-params/mock/mock-translate-params.pipe.ts
@@ -3,6 +3,6 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'obTranslateParams'})
 export class ObMockTranslateParamsPipe implements PipeTransform {
 	transform(value: string, params?: any): string {
-		return '';
+		return value;
 	}
 }
