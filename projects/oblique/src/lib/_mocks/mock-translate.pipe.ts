d4131c7275e76b99b801270d1bbecb02035e8ef4
Rene Giller <rene.giller@bit.admin.ch>
2020-07-13 21:43:00 +0200

    fix(toolchain): fix types for `ObMockTranslatePipe`

--- projects/oblique/src/lib/_mocks/mock-translate.pipe.ts
+++ projects/oblique/src/lib/_mocks/mock-translate.pipe.ts
@@ -2,7 +2,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({name: 'translate'})
 export class ObMockTranslatePipe implements PipeTransform {
-	transform(value: number): number {
+	transform(value: string): string {
 		return value;
 	}
 }
