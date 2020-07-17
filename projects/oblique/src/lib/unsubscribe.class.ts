cdf2fca8028c78ffb36499fb86c136e67b032901
Rene Giller <rene.giller@bit.admin.ch>
2020-07-17 13:31:57 +0200

    chore(unsubscribe): add deprecated notice
    The feature is not supported by Angular 10

--- projects/oblique/src/lib/unsubscribe.class.ts
+++ projects/oblique/src/lib/unsubscribe.class.ts
@@ -2,6 +2,9 @@ import {OnDestroy} from '@angular/core';
 import {Subject} from 'rxjs';
 
 // http://stackoverflow.com/questions/38008334/angular-rxjs-when-should-i-unsubscribe-from-subscription
+/**
+ * @deprecated since version 5.1.0. This feature is not supported by Angular 10 and will be removed in Oblique 6.0.0
+ */
 export class ObUnsubscribable implements OnDestroy {
 	protected unsubscribe: Subject<any> = new Subject();
 
