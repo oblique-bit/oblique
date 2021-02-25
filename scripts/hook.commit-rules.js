8123532b9f2a992719b012f8110ac1bd3b0f63fb
Müller Nicolas BIT <nicolas.mueller@bit.admin.ch>
2021-02-25 15:24:46 +0100

    feat(toolchain): enforce commit rules from readme OUI-1076

--- scripts/hook.commit-rules.js
+++ scripts/hook.commit-rules.js
@@ -1,20 +1,20 @@
 /* eslint-disable */
 
 const fs = require('fs');
-const lines = fs.readFileSync('.git/COMMIT_EDITMSG').toString().split('\n');
+const message = fs.readFileSync('.git/COMMIT_EDITMSG').toString().split('\n');
 try {
-	checkLineLength(lines, 100);
-	checkEmptyLine(lines);
-	checkHeader(lines[0]);
+	checkLineLength(message, 100);
+	checkEmptyLine(message);
+	checkHeader(message[0]);
+	checkBreakingChanges(message);
 } catch (err) {
-	console.error(`\nInvalid commit message: ${err.message}.\n\nSee details in README.md.\n`);
+	console.error(`\nInvalid commit message:\n${err.message}.\n\nSee details in README.md.\n`);
 	process.exit(1);
 }
 
-
 function checkLineLength(lines, length) {
 	const error = lines
-		.map((line, i) => line.length > length ? numeral(i + 1) : '')
+		.map((line, i) => line.length > length ? numeral(i) : '')
 		.filter(line => !!line)
 		.join(', ')
 		.replace(/,(?=[^,]*$)/, ' and');
@@ -31,17 +31,74 @@ function checkEmptyLine(lines) {
 }
 
 function checkHeader(header) {
+	const {type, scope, subject} = checkFormat(header);
 	const readme = fs.readFileSync('README.md', 'utf8');
 	const types = extractList(readme, 'Type');
 	const scopes = extractList(readme, 'Scope');
-	const regex = new RegExp(`^(?:${types.join('|')})(?:\\((?:${scopes.join('|')})\\))?: [a-z]`);
-	if (!regex.test(header)) {
+	checkType(type, types);
+	checkScope(scope, scopes);
+	checkSubject(subject);
+}
+
+function checkFormat(header) {
+	const matches = header.match(/^(?<type>[a-z-]+)(?:\((?<scope>[a-z-]+)\))?:\s(?<subject>.+)$/);
+	if (!matches || !matches.groups) {
 		throw new Error(`1st line doesn't follow "type(scope): subject" format`);
+	}
+	return {
+		type: matches.groups.type,
+		scope: matches.groups.scope,
+		subject: matches.groups.subject
+	};
+}
+
+function checkType(type, types) {
+	if(!types.includes(type)){
+		throw new Error(`1st line has an invalid type '${type}'. Allowed types are: ${types.join(', ').replace(/,(?=[^,]*$)/, ' and')}`);
+	}
+}
+
+function checkScope(scope, scopes) {
+	if(scope && !scopes.includes(scope)){
+		throw new Error(`1st line has an invalid scope '${scope}'. Allowed types are: ${scopes.join(', ').replace(/,(?=[^,]*$)/, ' and')}`);
+	}
+}
+
+function checkSubject(subject) {
+	if(/^[A-Z]/.test(subject)) {
+		throw new Error(`1st line has an invalid subject, the first letter must be lower case`);
+	}
+
+	if(/\.$/.test(subject)) {
+		throw new Error('1st line has an invalid subject, it must not end with a dot "."');
+	}
+}
+
+function checkBreakingChanges(lines) {
+	const breakingLineIndex = lines.findIndex(line => line.toLowerCase().includes('breaking change'));
+	if (breakingLineIndex > -1) {
+		const breaking = lines[breakingLineIndex];
+		const start = `${numeral(breakingLineIndex)} line doesn't follow "BREAKING CHANGE:" format,`;
+		if(!/^BREAKING CHANGE/i.test(breaking)) {
+			throw new Error(`${start} it must start on a new line`);
+		}
+		if(!/^BREAKING CHANGE/.test(breaking)) {
+			throw new Error(`${start} it must be written in uppercase letter`);
+		}
+		if(!/^BREAKING CHANGE(?!S)/.test(breaking)) {
+			throw new Error(`${start} it must not be plural`);
+		}
+		if(!/^BREAKING CHANGE(?!S):/.test(breaking)) {
+			throw new Error(`${start} it must be terminated with a colon without a whitespace`);
+		}
+		if(!lines[breakingLineIndex + 1] || !lines[breakingLineIndex + 1].length) {
+			throw new Error(`${numeral(breakingLineIndex + 1)} line cannot be empty as it follows a breaking change declaration`);
+		}
 	}
 }
 
 function numeral(i) {
-	switch (i) {
+	switch (i + 1) {
 		case 1:
 			return '1st';
 		case 2:
@@ -49,7 +106,7 @@ function numeral(i) {
 		case 3:
 			return '3rd';
 		default:
-			return  `${i}th`
+			return  `${i + 1}th`
 	}
 }
 
