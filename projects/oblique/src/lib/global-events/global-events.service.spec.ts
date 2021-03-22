f1198ef4fcae171159ad139d926316b59fd9d349
Rene Giller <rene.giller@bit.admin.ch>
2021-03-22 13:42:14 +0100

    refactor(global-events): remove `outsideClick$` function
    OUI-1223

--- projects/oblique/src/lib/global-events/global-events.service.spec.ts
+++ projects/oblique/src/lib/global-events/global-events.service.spec.ts
@@ -1,9 +1,6 @@
-import {fakeAsync, TestBed, tick} from '@angular/core/testing';
-import {WINDOW, windowProvider} from '../utilities';
-import {ObGlobalEventsService} from 'oblique';
-import {tap} from 'rxjs/operators';
-import {DOCUMENT} from '@angular/common';
-import {Observable} from 'rxjs';
+import {TestBed} from '@angular/core/testing';
+import {WINDOW} from '../utilities';
+import {ObGlobalEventsService} from './global-events.service';
 
 describe('ObGlobalEventsService', () => {
 	let service: ObGlobalEventsService;
@@ -108,50 +105,5 @@ describe('ObGlobalEventsService', () => {
 			});
 			window.dispatchEvent(new UIEvent('resize'));
 		});
-	});
-
-	describe('outsideClick$', () => {
-		let div1: HTMLDivElement;
-		let div2: HTMLDivElement;
-		let span: HTMLSpanElement;
-		beforeEach(() => {
-			div1 = document.createElement('div');
-			div2 = document.createElement('div');
-			span = document.createElement('span');
-			div1.appendChild(span);
-			document.querySelector('body').appendChild(div1);
-			document.querySelector('body').appendChild(div2);
-		});
-		it('should return an observable', () => {
-			expect(service.outsideClick$() instanceof Observable).toBe(true);
-		});
-
-		it('should emit on document click', done => {
-			service.outsideClick$(div1, div2).subscribe(event => {
-				expect(event instanceof MouseEvent).toBe(true);
-				done();
-			});
-			document.querySelector('body').click();
-		});
-
-		it('should not emit on excluded element click', fakeAsync(() => {
-			let emitted = false;
-			service.outsideClick$(div1, div2).subscribe(event => {
-				emitted = true;
-			});
-			div1.click();
-			tick(1000);
-			expect(emitted).toBe(false);
-		}));
-
-		it("should not emit on excluded element's child click", fakeAsync(() => {
-			let emitted = false;
-			service.outsideClick$(div1, div2).subscribe(event => {
-				emitted = true;
-			});
-			span.click();
-			tick(1000);
-			expect(emitted).toBe(false);
-		}));
 	});
 });
