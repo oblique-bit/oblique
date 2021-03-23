d132d0e03c2a98e264b2e1fc64b683869557ff34
Nina Egger <nina.egger@bit.admin.ch>
2021-03-23 14:15:58 +0100

    showcase(theme): add example for `ob-inline-form `
    OUI-1172

---
+++ src/app/samples/horizontal-forms/horizontal-forms.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {ObMasterLayoutService, ObThemeService} from 'oblique';
+
+@Component({
+	selector: 'ob-horizontal-forms-sample',
+	templateUrl: './horizontal-forms.component.html'
+})
+export class ObHorizontalFormsSampleComponent {
+	material: Observable<boolean>;
+
+	constructor(masterLayout: ObMasterLayoutService, theme: ObThemeService) {
+		this.material = theme.theme$.pipe(map(() => theme.isMaterial()));
+	}
+}
