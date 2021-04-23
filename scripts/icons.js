dc6f2f783befa1d79834b0017c4fd16602fab587
Ciril Saner <Ciril.Saner@bit.admin.ch>
2021-04-23 14:01:12 +0200

    fix(icon): remove borders from the icons and clean them up
    OUI-1267

--- scripts/icons.js
+++ scripts/icons.js
@@ -6,14 +6,13 @@ const fs = require('fs'),
 const iconsPath = path.join('projects', 'oblique', 'icons');
 const iconSetPath = path.join('projects', 'oblique', 'src', 'assets', 'obliqueIcons.svg');
 
-fs.writeFileSync(iconSetPath, '<svg>\n\t<defs>\n');
+fs.writeFileSync(iconSetPath, '<svg>\n\t<defs>');
 fs.readdirSync(iconsPath).forEach(file => {
 	const id = file.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase().replace('.svg', '');
 	const icon = fs.readFileSync(path.join(iconsPath, file)).toString()
-		.replace(/cls-/g, `cls-${id}-`)
+		.replace(/\n*/g, '')
 		.replace(/#171717/g, 'currentColor')
-		.replace(/#000/g, 'currentColor')
 		.replace('<svg ', `<svg id="${id}" `);
-	fs.appendFileSync(iconSetPath, `\t\t${icon}`);
+	fs.appendFileSync(iconSetPath, `\n\t\t${icon}`);
 });
 fs.appendFileSync(iconSetPath, '\t</defs>\n</svg>');
