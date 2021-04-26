76b99cd12b272c4f4b2cc6dcf8eb1284deb62c6e
Ciril Saner <Ciril.Saner@bit.admin.ch>
2021-04-26 14:33:40 +0200

    showcase(mandatory): add feature
    OUI-913

---
+++ src/app/material/mandatory/mandatory.sample.component.ts
@@ -0,0 +1,54 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+	selector: 'ob-mandatory.sample',
+	templateUrl: './mandatory.sample.component.html'
+})
+export class ObMandatorySampleComponent implements OnInit {
+	testForm: FormGroup;
+
+	constructor(private readonly formBuilder: FormBuilder) {}
+
+	ngOnInit() {
+		this.testForm = this.formBuilder.group({
+			inputRequired: ['', Validators.required],
+			inputDynamicRequired: '',
+			inputNonRequired: '',
+			inputDateRequired: ['', Validators.required],
+			inputDateDynamicRequired: '',
+			inputDateNonRequired: '',
+			selectRequired: ['', Validators.required],
+			selectDynamicRequired: '',
+			selectNonRequired: '',
+			matSelectRequired: ['', Validators.required],
+			matSelectDynamicRequired: '',
+			matSelectNonRequired: '',
+			textareaRequired: ['', Validators.required],
+			textareaDynamicRequired: '',
+			textareaNonRequired: '',
+			matChipListRequired: ['', Validators.required],
+			matChipListDynamicRequired: '',
+			matChipListNonRequired: ''
+		});
+	}
+
+	setRequired(): void {
+		this.removeRequired();
+		this.testForm.get('inputDynamicRequired').setValidators(Validators.required);
+		this.testForm.get('inputDateDynamicRequired').setValidators(Validators.required);
+		this.testForm.get('selectDynamicRequired').setValidators(Validators.required);
+		this.testForm.get('matSelectDynamicRequired').setValidators(Validators.required);
+		this.testForm.get('textareaDynamicRequired').setValidators(Validators.required);
+		this.testForm.get('matChipListDynamicRequired').setValidators(Validators.required);
+	}
+
+	removeRequired(): void {
+		this.testForm.get('inputDynamicRequired').clearValidators();
+		this.testForm.get('inputDateDynamicRequired').clearValidators();
+		this.testForm.get('selectDynamicRequired').clearValidators();
+		this.testForm.get('matSelectDynamicRequired').clearValidators();
+		this.testForm.get('textareaDynamicRequired').clearValidators();
+		this.testForm.get('matChipListDynamicRequired').clearValidators();
+	}
+}
