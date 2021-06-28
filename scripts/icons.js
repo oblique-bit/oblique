e1e0bcad104b584cb2356fb3a1adf53c4438f309
René Giller <rene.giller@bit.admin.ch>
2021-06-28 21:00:55 +0200

    refactor(toolchain): improve `icons`'s script performances

--- scripts/icons.js
+++ scripts/icons.js
@@ -5,17 +5,22 @@ const fs = require('fs'),
 
 const iconsPath = path.join('projects', 'oblique', 'icons');
 const iconSetPath = path.join('projects', 'oblique', 'src', 'assets', 'obliqueIcons.svg');
+const SVGs = getSVGs();
+const iconSet = ['<svg>', '\t<defs>', ...SVGs.map(svg => `\t\t${svg}`), '\t</defs>', '</svg>'];
 
-fs.writeFileSync(iconSetPath, '<svg>\n\t<defs>');
-fs.readdirSync(iconsPath)
-	.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
-	.forEach(file => {
-		const id = file.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase().replace('.svg', '');
-		const icon = fs.readFileSync(path.join(iconsPath, file)).toString()
-			.replace(/\n*/g, '')
-			.replace(/#171717/g, 'currentColor')
-			.replace('<svg ', `<svg id="${id}" `)
-			.replace(/<title>.+?<\/title>/g, '');
-		fs.appendFileSync(iconSetPath, `\n\t\t${icon}`);
-	});
-fs.appendFileSync(iconSetPath, '\t</defs>\n</svg>');
+fs.writeFileSync(iconSetPath, iconSet.join('\n'));
+
+
+function getSVGs() {
+	return fs.readdirSync(iconsPath)
+		.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+		.map(file => readFile(file));
+}
+
+function readFile(file) {
+	const id = file.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase().replace('.svg', '');
+	return fs.readFileSync(path.join(iconsPath, file)).toString().replace(/\n*/g, '')
+		.replace(/#171717/g, 'currentColor')
+		.replace('<svg ', `<svg id="${id}" `)
+		.replace(/<title>.+?<\/title>/g, '');
+}
