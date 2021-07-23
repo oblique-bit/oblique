46a01ed73220bebe29f15e57d6210eddad239e40
René Giller <rene.giller@bit.admin.ch>
2021-07-23 11:30:44 +0200

    fix(toolchain): check for `package-lock.json` existence before modifying it

--- scripts/sanitize-package-lock.js
+++ scripts/sanitize-package-lock.js
@@ -2,10 +2,9 @@
 
 const fs = require('fs');
 
-fs.writeFileSync(
-	'package-lock.json',
-	fs.readFileSync('package-lock.json').toString().replace(/repo\.bit\.admin\.ch\/repository\/npm-group/g, 'registry.npmjs.org')
-);
-
-
-
+if (fs.existsSync('package-lock.json')) {
+	fs.writeFileSync(
+		'package-lock.json',
+		fs.readFileSync('package-lock.json').toString().replace(/repo\.bit\.admin\.ch\/repository\/npm-group/g, 'registry.npmjs.org')
+	);
+}
