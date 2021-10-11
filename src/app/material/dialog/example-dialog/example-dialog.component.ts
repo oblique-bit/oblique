b95fb15aab336be18a54b19ea0e166415b48229a
René Giller <rene.giller@bit.admin.ch>
2021-10-11 15:14:19 +0200

    showcase(material): activate the spinner when the dialog is opened
    OUI-1402

--- src/app/material/dialog/example-dialog/example-dialog.component.ts
+++ src/app/material/dialog/example-dialog/example-dialog.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {ObSpinnerService} from '@oblique/oblique';
 import {ObIDialogData} from '../dialog.model';
 
 @Component({
@@ -7,7 +8,10 @@ import {ObIDialogData} from '../dialog.model';
 	templateUrl: './example-dialog.component.html'
 })
 export class ObExampleDialogComponent {
-	constructor(public dialogRef: MatDialogRef<ObExampleDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: ObIDialogData) {}
+	constructor(public dialogRef: MatDialogRef<ObExampleDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: ObIDialogData, spinner: ObSpinnerService) {
+		spinner.activate();
+		setTimeout(() => spinner.deactivate(), 2000);
+	}
 
 	onCancel(): void {
 		this.dialogRef.close();
