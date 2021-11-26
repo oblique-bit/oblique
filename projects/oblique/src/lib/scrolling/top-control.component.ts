5fc5580bbcee53ced0b98896d11911b9d3b99182
René Giller <rene.giller@bit.admin.ch>
2021-11-27 00:04:23 +0100

    refactor(scrolling): simplify code for the `ObTopControl` component

--- projects/oblique/src/lib/scrolling/top-control.component.ts
+++ projects/oblique/src/lib/scrolling/top-control.component.ts
@@ -1,5 +1,4 @@
 import {Component, HostBinding, Inject, Optional, ViewEncapsulation} from '@angular/core';
-import {ObMasterLayoutConfig} from '../master-layout/master-layout.config';
 import {WINDOW} from '../utilities';
 import {ObUseObliqueIcons} from '../icon/icon.model';
 
@@ -12,23 +11,15 @@ import {ObUseObliqueIcons} from '../icon/icon.model';
 	host: {class: 'ob-top-control'}
 })
 export class ObTopControlComponent {
-	private readonly window: Window;
 	@HostBinding('class.ob-font-awesome') useFontAwesomeIcon: boolean;
 
-	constructor(private readonly config: ObMasterLayoutConfig, @Inject(WINDOW) window, @Optional() @Inject(ObUseObliqueIcons) useObliqueIcon) {
-		this.window = window; // because AoT don't accept interfaces as DI
+	constructor(@Inject(WINDOW) private readonly window, @Optional() @Inject(ObUseObliqueIcons) useObliqueIcon) {
 		this.useFontAwesomeIcon = !useObliqueIcon;
 	}
 
 	public scrollTop(): void {
-		const scrollStep = this.window.scrollY / (this.config.scrollToTopDuration / 15);
-		this.scrollToTop(scrollStep);
-	}
-
-	private scrollToTop(scrollStep: number): void {
 		if (this.window.scrollY) {
-			this.window.scrollBy(0, -scrollStep);
-			this.window.setTimeout(() => this.scrollToTop(scrollStep), 15);
+			this.window.scrollTo({top: 0, behavior: 'smooth'});
 		}
 	}
 }
