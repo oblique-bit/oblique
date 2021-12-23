e02eb01695411c6e031a79967c6b00aa01bb82d7
René Giller <rene.giller@bit.admin.ch>
2021-12-23 18:04:07 +0100

    fix(master-layout): toggle `aria-expanded` on an outside click
    OUI-1538

--- projects/oblique/src/lib/master-layout/aria-menu-button.directive.ts
+++ projects/oblique/src/lib/master-layout/aria-menu-button.directive.ts
@@ -1,13 +1,21 @@
-import {Directive, HostBinding, HostListener, Input} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit} from '@angular/core';
+import {ObGlobalEventsService} from "../global-events/global-events.service";
+import {obOutsideFilter} from "../global-events/outsideFilter";
 
 @Directive({
 	selector: '[obAriaMenuButton]',
 	exportAs: 'obAriaMenuButton'
 })
-export class ObAriaMenuButtonDirective {
+export class ObAriaMenuButtonDirective implements OnInit {
 	@Input('obAriaMenuButton') @HostBinding('attr.aria-owns') @HostBinding('attr.aria-controls') target: string;
 	@HostBinding('attr.aria-expanded') active = undefined;
 	@HostBinding('attr.aria-haspopup') popup = true;
+
+	constructor(private readonly globalEvents: ObGlobalEventsService, private readonly element: ElementRef) {}
+
+	ngOnInit(): void {
+		this.globalEvents.click$.pipe(obOutsideFilter(this.element.nativeElement)).subscribe(() => (this.active = undefined));
+	}
 
 	@HostListener('click')
 	@HostListener('keyup.enter')
