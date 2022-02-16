b31401253f5953281b968924bdcdb0f18d641e6f
René Giller <rene.giller@bit.admin.ch>
2022-02-16 12:44:31 +0100

    feat(multi-translate-loader): provide a useful error message in case of missing translations
    OUI-1602

--- projects/oblique/src/lib/multi-translate-loader/multi-translate-loader.ts
+++ projects/oblique/src/lib/multi-translate-loader/multi-translate-loader.ts
@@ -36,7 +36,13 @@ export class ObMultiTranslateLoader implements TranslateLoader {
 	}
 
 	private static handleError(url: string): Observable<Record<string, string>> {
-		console.error('Could not find translation file:', url);
+		const language: string = /(?<language>\w\w)\.json$/.exec(url)?.groups?.language;
+		const text = `The "${language.toUpperCase()}" language has been selected but ${
+			/oblique-\w\w\.json$/.test(url)
+				? `Oblique doesn't provide a translation file for that language. The file "oblique-${language}.json" needs to be created in the project's "assets/i18n" directory. Each project is responsible for providing the files to enable Oblique's translation of additional languages.`
+				: `the project does not provide a translation file for that language. Please make sure that the "${language}.json" file exists in the project's "assets/i18n" directory. Each project is responsible for its own translations`
+		}`;
+		console.warn(text);
 		return of({});
 	}
 }
