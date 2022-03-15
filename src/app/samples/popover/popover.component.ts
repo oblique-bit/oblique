527a40a11547bc6def885144b52b8ac8147bcf8b
Ciril Saner <Ciril.Saner@bit.admin.ch>
2022-03-15 11:09:02 +0100

    showcase(popover): open popover on mouseover
    OUI-1492

--- src/app/samples/popover/popover.component.ts
+++ src/app/samples/popover/popover.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {ObEToggleType} from '@oblique/oblique';
 import {Placement} from '@popperjs/core';
 
 @Component({
@@ -10,4 +11,8 @@ export class PopoverComponent {
 	param = 'This is a param.';
 	placement: Placement = 'top';
 	openPopoverPosition = 'center';
+	toggleHandle: ObEToggleType;
+
+	toggleTypeClick = ObEToggleType.CLICK;
+	toggleTypeHover = ObEToggleType.HOVER;
 }
