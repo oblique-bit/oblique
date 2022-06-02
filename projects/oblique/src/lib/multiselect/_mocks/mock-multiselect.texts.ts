f3d6561fb96447bfec8bd3ee7447cac5c5b402e1
Ceronio Justin BIT <justin.ceronio@bit.admin.ch>
2022-06-02 09:19:51 +0200

    feat(multiselect): remove unused `checkedPlural` text
    OUI-1675
    BREAKING CHANGE:
    * `checkedPlural` text has been removed

--- projects/oblique/src/lib/multiselect/_mocks/mock-multiselect.texts.ts
+++ projects/oblique/src/lib/multiselect/_mocks/mock-multiselect.texts.ts
@@ -5,7 +5,6 @@ export class ObMockMultiselectTexts {
 	checkAll = 'i18n.oblique.multiselect.checkAll';
 	uncheckAll = 'i18n.oblique.multiselect.uncheckAll';
 	checked = 'i18n.oblique.multiselect.checked';
-	checkedPlural = 'i18n.oblique.multiselect.checkedPlural';
 	searchPlaceholder = 'i18n.oblique.multiselect.searchPlaceholder';
 	defaultTitle = 'i18n.oblique.multiselect.defaultTitle';
 	allSelected = 'i18n.oblique.multiselect.allSelected';
--- projects/oblique/src/lib/multiselect/multiselect.texts.ts
+++ projects/oblique/src/lib/multiselect/multiselect.texts.ts
@@ -5,8 +5,6 @@ export class ObMultiselectTexts {
 	checkAll = 'i18n.oblique.multiselect.checkAll';
 	uncheckAll = 'i18n.oblique.multiselect.uncheckAll';
 	checked = 'i18n.oblique.multiselect.checked';
-	// TODO: This is currently not used!
-	checkedPlural = 'i18n.oblique.multiselect.checkedPlural';
 	searchPlaceholder = 'i18n.oblique.multiselect.searchPlaceholder';
 	defaultTitle = 'i18n.oblique.multiselect.defaultTitle';
 	allSelected = 'i18n.oblique.multiselect.allSelected';
