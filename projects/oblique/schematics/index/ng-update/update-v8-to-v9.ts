d39cbe3082a35cf60fa76cf056372d3b14da7360
René Giller <rene.giller@bit.admin.ch>
2022-06-15 21:54:06 +0200

    feat(telemetry): extend `ng-update` to include telemetry information in the project
    OUI-1604

--- projects/oblique/schematics/index/ng-update/update-v8-to-v9.ts
+++ projects/oblique/schematics/index/ng-update/update-v8-to-v9.ts
@@ -1,6 +1,20 @@
 import {Rule, SchematicContext, Tree, chain} from '@angular-devkit/schematics';
 import {ObIMigrations} from './ng-update.model';
-import {applyInTree, createSafeRule, getIndexPaths, infoMigration, overwriteIndexFile, readFile, replaceInFile} from '../utils';
+import {
+	PathPerProject,
+	addTsCompilerOption,
+	applyInTree,
+	createSafeRule,
+	getFilePathPerProject,
+	getIndexPaths,
+	getPackageJsonPath,
+	getRootModulePathPerProject,
+	infoMigration,
+	overwriteIndexFile,
+	packageJsonConfigPath,
+	readFile,
+	replaceInFile
+} from '../utils';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface IUpdateV8Schema {}
@@ -19,7 +33,7 @@ export class UpdateV8toV9 implements ObIMigrations {
 				this.renameJumpLinks(),
 				this.useKebabCaseForMixins(),
 				this.renameOpened(),
-				this.removeTelemetryDisableToken()
+				this.addTelemetryInfo()
 			])(tree, _context);
 		};
 	}
@@ -114,13 +128,96 @@ export class UpdateV8toV9 implements ObIMigrations {
 		});
 	}
 
-	private removeTelemetryDisableToken(): Rule {
+	private addTelemetryInfo(): Rule {
 		return createSafeRule((tree: Tree, _context: SchematicContext) => {
-			infoMigration(_context, 'Remove TELEMETRY_DISABLE injection token');
-			const apply = (filePath: string): void => {
-				replaceInFile(tree, filePath, /{\s*provide\s*:\s*TELEMETRY_DISABLE\s*,\s*useValue\s*:\s*(?:true|false)}\s*,?/, '');
-			};
-			return applyInTree(tree, apply, '*.ts');
+			infoMigration(_context, 'Update Telemetry info');
+			const mainTsPathPerProject = getFilePathPerProject(tree, ['architect', 'build', 'options', 'main']);
+			const rootModulesPathPerProject = getRootModulePathPerProject(tree, mainTsPathPerProject);
+			this.addTelemetry(tree, _context, mainTsPathPerProject, rootModulesPathPerProject);
+			this.removeTelemetryDisable(
+				tree,
+				_context,
+				rootModulesPathPerProject.map(file => file.path)
+			);
+
+			return tree;
 		});
+	}
+
+	private addTelemetry(
+		tree: Tree,
+		_context: SchematicContext,
+		mainTsPathPerProject: PathPerProject[],
+		rootModulesPathPerProject: PathPerProject[]
+	): void {
+		infoMigration(_context, "Configure Telemetry for projects that didn't disable it.");
+		const tsConfigPathsPerProject = getFilePathPerProject(tree, ['architect', 'build', 'options', 'tsConfig']);
+		rootModulesPathPerProject
+			.map(file => ({...file, content: readFile(tree, file.path)}))
+			.map(file => ({...file, isDisabled: this.extractIsTelemetryDisabled(file.content)}))
+			.filter(file => !file.isDisabled)
+			.map(file => file.project)
+			.map(project => ({
+				project,
+				mainTsPath: this.getProjectPath(mainTsPathPerProject, project),
+				tsConfigPath: this.getProjectPath(tsConfigPathsPerProject, project)
+			}))
+			.filter(paths => !!paths.mainTsPath && !!paths.tsConfigPath)
+			.forEach(paths => {
+				this.updateMainTs(tree, paths.mainTsPath, paths.project);
+				this.updateTsConfig(tree, paths.tsConfigPath);
+				this.updatePackageJson(tree);
+			});
+	}
+
+	private extractIsTelemetryDisabled(fileContent: string): boolean {
+		return /{\s*provide\s*:\s*TELEMETRY_DISABLE\s*,\s*useValue\s*:\s*(?<isDisabled>\w*)}/.exec(fileContent)?.groups?.isDisabled === 'true';
+	}
+
+	private getProjectPath(target: PathPerProject[], project: string): string {
+		return target.find(config => config.project === project)?.path ?? '';
+	}
+
+	private removeTelemetryDisable(tree: Tree, _context: SchematicContext, rootModulePaths: string[]): void {
+		infoMigration(_context, 'Remove TELEMETRY_DISABLE injection token');
+		rootModulePaths.forEach(path =>
+			replaceInFile(tree, path, /{\s*provide\s*:\s*TELEMETRY_DISABLE\s*,\s*useValue\s*:\s*(?:true|false)}\s*,?/, '')
+		);
+	}
+
+	private updateMainTs(tree: Tree, mainTsPath: string, project: string): void {
+		const content = readFile(tree, mainTsPath);
+		if (!/{\s*provide\s*:\s*OB_PROJECT_INFO\s*,\s*useValue/.test(content)) {
+			tree.overwrite(
+				mainTsPath,
+				[
+					"import {OB_PROJECT_INFO} from '@oblique/oblique';",
+					`import packageInfo from '${getPackageJsonPath(tree, project)}';`,
+					content
+						.replace(
+							/(?<=platformBrowserDynamic\(\s*)\[?\s*(?<providers>{\s*provide.*})?\s*]?\s*(?=\))/s,
+							'[$<providers>,\n{provide: OB_PROJECT_INFO, useValue: {name: packageInfo.name, version: packageInfo.version, title: packageInfo.title}}\n]'
+						)
+						.replace(/\[,/, '[')
+				].join('\n')
+			);
+		}
+	}
+
+	private updateTsConfig(tree: Tree, tsConfigPath: string): void {
+		const content = readFile(tree, tsConfigPath);
+		let replacement = addTsCompilerOption(content, 'resolveJsonModule');
+		replacement = addTsCompilerOption(replacement, 'allowSyntheticDefaultImports');
+		tree.overwrite(tsConfigPath, replacement);
+	}
+
+	private updatePackageJson(tree: Tree): void {
+		const content = readFile(tree, packageJsonConfigPath);
+		if (!/^\s*"title"/.test(content)) {
+			tree.overwrite(
+				packageJsonConfigPath,
+				content.replace(/^(?<tabs>\s*)"name"\s*:\s*"(?<name>.*)"/m, '$<tabs>"title": "$<name>",\n$<tabs>"name": "$<name>"')
+			);
+		}
 	}
 }
