777b4073541aff93614f3d19dc74dc0024c08b32
Ciril Saner <Ciril.Saner@bit.admin.ch>
2022-07-29 15:15:30 +0200

    showcase: remove usage of `directory` for api & description and refactor accordingly
    OUI-1834

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -7,17 +7,15 @@ import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 })
 export class DescriptionComponent implements OnInit {
 	@Input() component: string;
-	@Input() directory: string;
 	description: SafeHtml;
 
 	constructor(private readonly sanitizer: DomSanitizer) {}
 
 	ngOnInit(): void {
-		if (this.component && this.directory) {
+		if (this.component) {
 			this.description = this.sanitizer.bypassSecurityTrustHtml(
 				// eslint-disable-next-line @typescript-eslint/no-var-requires
-				require(`!!raw-loader!../../../projects/oblique/src/lib/${this.directory || this.component}/${this.component}.description.html`)
-					.default
+				require(`!!raw-loader!../../../projects/oblique/src/lib//${this.component}/${this.component}.description.html`).default
 			);
 		}
 	}
