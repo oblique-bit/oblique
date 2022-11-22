b97d3ebe685131e01138d8ef5382f4383b09fff6
Percebois-Garve Olivier BIT <olivier.percebois-garve@bit.admin.ch>
2022-11-22 10:17:43 +0100

    fix(toolchain): generate `oblique-components.scss` before transpiling
    OUI-1445

--- projects/oblique/scripts/post-dist/run-post-dist-steps.ts
+++ projects/oblique/scripts/post-dist/run-post-dist-steps.ts
@@ -1,7 +1,6 @@
 import {CopyDistFiles} from './steps/copy-dist-files';
 import {RenameDistribution} from './steps/rename-distribution';
 import {AdaptPackageJson} from './steps/adapt-package-json';
-import {GenerateComponentStyles} from './steps/generate-component-styles';
 import {UpdatePaths} from './steps/update-paths';
 import {AddBanner} from './steps/add-banner';
 import {DistributeObFeatures} from './steps/distribute-ob-features';
@@ -9,7 +8,6 @@ import {DistributeObFeatures} from './steps/distribute-ob-features';
 CopyDistFiles.perform();
 RenameDistribution.perform();
 AdaptPackageJson.perform();
-GenerateComponentStyles.perform();
 UpdatePaths.perform();
 AddBanner.perform();
 DistributeObFeatures.perform();
