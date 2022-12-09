20ac15bde4f02d7987a09d1a067d453a92b08d0f
Olivier Percebois-Garve <olivier.percebois-garve@estv.admin.ch>
2022-12-09 10:22:10 +0100

    fix(icon): fix typos in icon names
    OUI-2065
    BREAKING CHANGES:
    * `attachement` icon has been renamed into `attachment` (Solved with Schematics)
    * `mail-attachement` icon has been renamed into `mail-attachment` (Solved with Schematics)
    * `ObEIcon.ATTACHEMENT` has been renamed into `ObEIcon.ATTACHMENT` (Solved with Schematics)
    * `ObEIcon.MAIL_ATTACHEMENT` has been renamed into `ObEIcon.MAIL_ATTACHMENT`
    (Solved with Schematics)

--- projects/oblique/schematics/index/ng-update/update-v9-to-v10.ts
+++ projects/oblique/schematics/index/ng-update/update-v9-to-v10.ts
@@ -1,5 +1,6 @@
 import {Rule, SchematicContext, Tree, chain} from '@angular-devkit/schematics';
 import {ObIMigrations} from './ng-update.model';
+import {applyInTree, createSafeRule, infoMigration, replaceInFile} from '../utils';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface IUpdateV9Schema {}
@@ -9,6 +10,19 @@ export class UpdateV9toV10 implements ObIMigrations {
 
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	applyMigrations(_options: IUpdateV9Schema): Rule {
-		return (tree: Tree, _context: SchematicContext) => chain([])(tree, _context);
+		return (tree: Tree, _context: SchematicContext) => chain([this.renameIcons()])(tree, _context);
+	}
+
+	private renameIcons(): Rule {
+		return createSafeRule((tree: Tree, _context: SchematicContext) => {
+			infoMigration(_context, 'Rename attachement into attachment for icon names');
+			const toApply = (filePath: string): void => {
+				replaceInFile(tree, filePath, /svgIcon="attachement/g, 'svgIcon="attachment');
+				replaceInFile(tree, filePath, /svgIcon="mail-attachement/g, 'svgIcon="mail-attachment');
+				replaceInFile(tree, filePath, /ObEIcon.ATTACHEMENT/g, 'ObEIcon.ATTACHMENT');
+				replaceInFile(tree, filePath, /ObEIcon.MAIL_ATTACHEMENT/g, 'ObEIcon.MAIL_ATTACHMENT');
+			};
+			return applyInTree(tree, toApply, '*.{ts,html}');
+		});
 	}
 }
