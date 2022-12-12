856d9d7c3d527acadc19fa4de947bc6f893e64ec
Nicole Widmer <nicole.widmer@bit.admin.ch>
2022-12-12 22:00:40 +0100

    chore: postpone `ObliqueTestingModule` removal to Oblique 11
    OUI-2076

--- projects/oblique/src/lib/oblique-testing.module.ts
+++ projects/oblique/src/lib/oblique-testing.module.ts
@@ -172,7 +172,7 @@ const MOCK_OBLIQUE_MODULES = [
 ];
 
 /**
- * @deprecated since version 9.0.0. It will be removed with Oblique 10. Real implementation of Oblique Modules should be used instead.
+ * @deprecated since version 9.0.0. It will be removed with Oblique 11. Real implementation of Oblique Modules should be used instead.
  */
 @NgModule({
 	imports: MOCK_OBLIQUE_MODULES,
