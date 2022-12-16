260117c748260d1dcce44e0f2b2f0f95581b78d9
Olivier Percebois-Garve <olivier.percebois-garve@estv.admin.ch>
2022-12-16 21:13:43 +0100

    showcase(telemetry): remove telemetry information
    OUI-2040

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,9 @@ import {enableProdMode} from '@angular/core';
 import {environment} from './environments/environment';
 // Application resources:
 import {AppModule} from './app/app.module';
-import {OB_PROJECT_INFO} from '../projects/oblique/src/lib/telemetry/telemetry.service';
-import packageInfo from '../package.json';
 
 if (environment.production) {
 	enableProdMode();
 }
 
-void platformBrowserDynamic([
-	{
-		provide: OB_PROJECT_INFO,
-		useValue: {name: packageInfo.name, version: packageInfo.version, title: packageInfo.title}
-	}
-]).bootstrapModule(AppModule);
+void platformBrowserDynamic().bootstrapModule(AppModule);
