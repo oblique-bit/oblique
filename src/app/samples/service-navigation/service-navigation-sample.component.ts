8a0a328ab8eabb336e903caa0a495ee603740b44
Nicole Widmer <nicole.widmer@bit.admin.ch>
2023-03-21 00:00:54 +0100

    showcase(service-navigation): add `returnUrl` parameter
    OUI-1847

--- src/app/samples/service-navigation/service-navigation-sample.component.ts
+++ src/app/samples/service-navigation/service-navigation-sample.component.ts
@@ -1,11 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {WINDOW} from '@oblique/oblique';
 import {environment} from '../../../environments/environment';
 
 @Component({
 	selector: 'sc-service-navigation',
 	templateUrl: './service-navigation-sample.component.html'
 })
-export class ServiceNavigationSampleComponent {
+export class ServiceNavigationSampleComponent implements OnInit {
+	returnUrl: string;
 	readonly rootUrl = environment.pams.rootUrl;
 	readonly environment = environment.pams.environment;
+
+	constructor(@Inject(WINDOW) private readonly window: Window) {}
+
+	ngOnInit(): void {
+		this.returnUrl = this.window.location.href;
+	}
 }
