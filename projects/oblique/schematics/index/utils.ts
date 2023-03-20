b905542bf3eb2f360b51e2bd25ee3f21ca6cb274
René Giller <rene.giller@bit.admin.ch>
2023-03-20 11:47:52 +0100

    chore(schematics): add `getAngularProjectsWithConfigs` function that don't filter out empty configs
    OUI-2198

--- projects/oblique/schematics/index/utils.ts
+++ projects/oblique/schematics/index/utils.ts
@@ -96,19 +96,21 @@ export function getJson(tree: any, path: string): any {
 }
 
 export function getAngularConfigs(tree: Tree, path: string[]): {project: string; config: any}[] {
+	return getAngularProjectsWithConfigs(tree, path).filter(project => project.config);
+}
+
+export function getAngularProjectsWithConfigs(tree: Tree, path: string[]): {project: string; config: any}[] {
 	const json = getJson(tree, angularJsonConfigPath);
-	return Object.keys(getJsonProperty(json, 'projects'))
-		.reduce(
-			(config, project) => [
-				...config,
-				{
-					project,
-					config: getJsonProperty(json, ['projects', project, ...path].join(';'))
-				}
-			],
-			[]
-		)
-		.filter(project => project.config);
+	return Object.keys(getJsonProperty(json, 'projects')).reduce(
+		(config, project) => [
+			...config,
+			{
+				project,
+				config: getJsonProperty(json, ['projects', project, ...path].join(';'))
+			}
+		],
+		[]
+	);
 }
 
 export function checkIfAngularConfigExists(tree: Tree, path: string[], config: string): boolean {
