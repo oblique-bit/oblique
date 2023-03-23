5aa7c027af5711e1454124163da25aff107f4967
Nicole Widmer <nicole.widmer@bit.admin.ch>
2023-03-24 00:51:42 +0100

    showcase(service-navigation): add `profileLinks` to  profile feature
    OUI-1848

--- src/app/samples/service-navigation/service-navigation-sample.component.ts
+++ src/app/samples/service-navigation/service-navigation-sample.component.ts
@@ -1,13 +1,37 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {WINDOW} from '@oblique/oblique';
+import {ObIServiceNavigationLink, WINDOW} from '@oblique/oblique';
 import {environment} from '../../../environments/environment';
 
 @Component({
 	selector: 'sc-service-navigation',
-	templateUrl: './service-navigation-sample.component.html'
+	templateUrl: './service-navigation-sample.component.html',
+	styleUrls: ['./service-navigation-sample.component.scss']
 })
 export class ServiceNavigationSampleComponent implements OnInit {
 	returnUrl: string;
+	profileLinks: ObIServiceNavigationLink[] = [
+		{
+			url: 'i18n.service-navigation.profile.link.unicorn.url',
+			label: 'i18n.service-navigation.profile.link.unicorn.label'
+		},
+		{
+			url: 'i18n.service-navigation.profile.link.dragon.url',
+			label: 'i18n.service-navigation.profile.link.dragon.label'
+		},
+		{
+			url: 'i18n.service-navigation.profile.link.phoenix.url',
+			label: 'i18n.service-navigation.profile.link.phoenix.label'
+		},
+		{
+			url: 'i18n.service-navigation.profile.link.ork.url',
+			label: 'i18n.service-navigation.profile.link.ork.label'
+		},
+		{
+			url: 'i18n.service-navigation.profile.link.kappa.url',
+			label: 'i18n.service-navigation.profile.link.kappa.label'
+		}
+	];
+	hasProfileLinks = true;
 	readonly rootUrl = environment.pams.rootUrl;
 	readonly environment = environment.pams.environment;
 
