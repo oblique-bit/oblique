2ebb3a84f4bf793964cf89f5641725f141c485c4
René Giller <rene.giller@bit.admin.ch>
2023-04-14 14:45:08 +0200

    chore(toolchain): ensure scripted commits have an issue number in footer

--- projects/oblique/scripts/release/commit.ts
+++ projects/oblique/scripts/release/commit.ts
@@ -1,7 +1,9 @@
 import {execSync} from 'child_process';
 
 export class Commit {
-	static perform(message: string): void {
+	static perform(header: string): void {
+		const issueNumber = (/OUI-\d+/.exec(execSync('git branch --show-current').toString()) ?? [])[0];
+		const message = issueNumber ? `${header}\n\n${issueNumber}` : header;
 		execSync(`git commit -am "${message}"`);
 	}
 }
