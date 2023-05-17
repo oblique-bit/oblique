8d9021abc3ef4dc55f0906fd5bc8982ed25cc74b
René Giller <rene.giller@bit.admin.ch>
2023-05-17 15:34:57 +0200

    chore(toolchain): add `update-error-messages` script to oblique
    OUI-2326

--- projects/oblique/scripts/pre-build/run-pre-build-steps.ts
+++
@@ -1,7 +0,0 @@
-import {CopyErrorMessages} from './steps/copy-error-messages';
-import {execSync} from 'child_process';
-
-CopyErrorMessages.perform();
-
-const files = ['projects/oblique/src/lib/error-messages/error-messages.description.html'].join(',');
-execSync(`prettier "{${files}}" --loglevel warn --write`, {stdio: 'inherit'});
