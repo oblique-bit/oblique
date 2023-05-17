913e59002f3a0530fbce24d1f19b4532b987ca53
René Giller <rene.giller@bit.admin.ch>
2023-05-17 16:05:47 +0200

    chore(toolchain): simplify `lint` scripts
    OUI-2326

--- scripts/lint/steps/eslint.ts
+++
@@ -1,7 +0,0 @@
-import {execSync} from 'child_process';
-
-export class Eslint {
-	static perform(param = ''): void {
-		execSync(`eslint "{projects,scripts,tests}/**/*.{ts,js,html}" --cache ${param}`, {stdio: 'inherit'});
-	}
-}
