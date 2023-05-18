0aabf9bdb971716156adab854cf205098a8580b8
René Giller <rene.giller@bit.admin.ch>
2023-05-18 03:51:04 +0200

    refactor(toolchain): simplify `adapt-package-json.ts` and `adapt-schematics-package-json.ts`
    OUI-2326

--- projects/oblique/scripts/post-dist/steps/adapt-package-json.ts
+++ projects/oblique/scripts/post-dist/steps/adapt-package-json.ts
@@ -1,34 +1,12 @@
-import {readFileSync, writeFileSync} from 'fs';
 import path from 'path';
+import {PackageJson} from './package-json';
 
-type Json = Record<string, any>;
 export class AdaptPackageJson {
 	static perform(): void {
 		const filePath = path.join('..', '..', 'dist', 'oblique', 'package.json');
-		const distPackage = AdaptPackageJson.getDistPackage(filePath);
-		let adaptedDistPackage = AdaptPackageJson.addGlobalExports(distPackage);
-		adaptedDistPackage = AdaptPackageJson.addProperties(adaptedDistPackage);
-
-		writeFileSync(filePath, JSON.stringify(adaptedDistPackage, null, 2));
-	}
-
-	private static getDistPackage(filePath: string): Json {
-		return JSON.parse(readFileSync(filePath).toString());
-	}
-
-	private static addGlobalExports(distPackage: Json): Json {
-		distPackage.exports['./*'] = {
-			default: './*'
-		};
-		return distPackage;
-	}
-
-	private static addProperties(distPackage: Json): Json {
-		const rootPackage = JSON.parse(readFileSync(path.join('..', '..', 'package.json')).toString());
-		['version', 'description', 'keywords', 'author', 'contributors', 'homepage', 'repository', 'license', 'bugs'].forEach(
-			field => (distPackage[field] = rootPackage[field])
-		);
-
-		return distPackage;
+		const fields = ['version', 'description', 'keywords', 'author', 'contributors', 'homepage', 'repository', 'license', 'bugs'];
+		const distPackage = PackageJson.getData(filePath);
+		const rootPackage = PackageJson.filterIn(path.join('..', '..', 'package.json'), fields);
+		PackageJson.write(filePath, {...distPackage, ...rootPackage, exports: {...distPackage.exports, './*': {default: './*'}}});
 	}
 }
--- projects/oblique/scripts/post-dist/steps/adapt-schematics-package-json.ts
+++ projects/oblique/scripts/post-dist/steps/adapt-schematics-package-json.ts
@@ -1,27 +1,11 @@
-import {readFileSync, writeFileSync} from 'fs';
 import path from 'path';
+import {PackageJson} from './package-json';
 
-type Json = Record<string, any>;
 export class AdaptSchematicsPackageJson {
 	static perform(): void {
 		const filePath = path.join('..', '..', 'dist', 'oblique', 'schematics', 'package.json');
-		const distPackage = AdaptSchematicsPackageJson.filter(filePath, ([key]) => key !== 'scripts');
-		const rootPackage = AdaptSchematicsPackageJson.getAdaptedRootPackage();
-
-		writeFileSync(filePath, JSON.stringify({...distPackage, ...rootPackage}, null, 2));
-	}
-
-	private static getAdaptedRootPackage(): Json {
-		const filePath = path.join('..', '..', 'package.json');
-		const fields = ['version', 'author', 'contributors', 'license'];
-		return AdaptSchematicsPackageJson.filter(filePath, ([key]) => fields.includes(key));
-	}
-
-	private static filter(filePath: string, func: (string) => boolean): Json {
-		return Object.fromEntries(Object.entries(AdaptSchematicsPackageJson.getData(filePath)).filter(func));
-	}
-
-	private static getData(filePath: string): Json {
-		return JSON.parse(readFileSync(filePath).toString());
+		const distPackage = PackageJson.filterOut(filePath, ['scripts']);
+		const rootPackage = PackageJson.filterIn(path.join('..', '..', 'package.json'), ['version', 'author', 'contributors', 'license']);
+		PackageJson.write(filePath, {...distPackage, ...rootPackage});
 	}
 }
