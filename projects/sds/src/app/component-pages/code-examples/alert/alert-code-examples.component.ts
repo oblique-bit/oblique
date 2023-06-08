71d5adff37bb460633a62cf7aa52ebc856dad4be
René Giller <rene.giller@bit.admin.ch>
2023-06-08 17:02:08 +0200

    refactor(sds/component-pages): use a function to `require` the snippet
    OUI-2343

--- projects/sds/src/app/component-pages/code-examples/alert/alert-code-examples.component.ts
+++ projects/sds/src/app/component-pages/code-examples/alert/alert-code-examples.component.ts
@@ -1,27 +1,12 @@
 import {Component} from '@angular/core';
-import {SourceCode} from '../../code-example/source-code.model';
 import {CodeExamples} from '../../code-examples.model';
 
 @Component({
 	selector: 'app-code-example-alert',
 	templateUrl: './alert-code-examples.component.html'
 })
-export class AlertCodeExamplesComponent implements CodeExamples {
+export class AlertCodeExamplesComponent extends CodeExamples {
 	readonly componentId = 'alert-examples';
-
-	readonly codeSnippetsInfo = [
-		new SourceCode(
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-			require('!!raw-loader!./previews/info/alert-example-info-preview.component.html').default as string,
-			'HTML'
-		)
-	];
-
-	readonly codeSnippetsSuccess = [
-		new SourceCode(
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-			require('!!raw-loader!./previews/success/alert-example-success-preview.component.html').default as string,
-			'HTML'
-		)
-	];
+	readonly codeSnippetsInfo = [this.getSnippet('alert', 'info/alert-example-info-preview.component.html', 'HTML')];
+	readonly codeSnippetsSuccess = [this.getSnippet('alert', 'success/alert-example-success-preview.component.html', 'HTML')];
 }
--- projects/sds/src/app/component-pages/code-examples/button/button-code-examples.component.ts
+++ projects/sds/src/app/component-pages/code-examples/button/button-code-examples.component.ts
@@ -1,5 +1,4 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {SourceCode} from '../../code-example/source-code.model';
 import {CodeExamples} from '../../code-examples.model';
 
 @Component({
@@ -7,55 +6,30 @@ import {CodeExamples} from '../../code-examples.model';
 	templateUrl: './button-code-examples.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ButtonCodeExamplesComponent implements CodeExamples {
+export class ButtonCodeExamplesComponent extends CodeExamples {
 	readonly componentId = 'button-examples';
 
 	readonly codeSnippetsPrimaryLinkFrown = [
-		new SourceCode(
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-			require('!!raw-loader!./previews/primary-link-frown/button-example-primary-link-frown.component.html').default as string,
-			'HTML'
-		)
+		this.getSnippet('button', 'primary-link-frown/button-example-primary-link-frown.component.html', 'HTML')
 	];
 
 	readonly codeSnippetsPrimaryLoginDisabled = [
-		new SourceCode(
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-			require('!!raw-loader!./previews/primary-login-disabled/button-example-primary-login-disabled.component.html').default as string,
-			'HTML'
-		)
+		this.getSnippet('button', 'primary-login-disabled/button-example-primary-login-disabled.component.html', 'HTML')
 	];
 
 	readonly codeSnippetsSecondaryNoIcon = [
-		new SourceCode(
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-			require('!!raw-loader!./previews/secondary-link-no-icon/button-example-secondary-link-no-icon.component.html').default as string,
-			'HTML'
-		)
+		this.getSnippet('button', 'secondary-link-no-icon/button-example-secondary-link-no-icon.component.html', 'HTML')
 	];
 
 	readonly codeSnippetsSecondaryWheelchair = [
-		new SourceCode(
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-			require('!!raw-loader!./previews/secondary-wheelchair/button-example-secondary-wheelchair.component.html').default as string,
-			'HTML'
-		)
+		this.getSnippet('button', 'secondary-wheelchair/button-example-secondary-wheelchair.component.html', 'HTML')
 	];
 
 	readonly codeSnippetsTertiaryLinkNoIconDisabled = [
-		new SourceCode(
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-			require('!!raw-loader!./previews/tertiary-link-no-icon-disabled/button-example-tertiary-link-no-icon-disabled.component.html')
-				.default as string,
-			'HTML'
-		)
+		this.getSnippet('button', 'tertiary-link-no-icon-disabled/button-example-tertiary-link-no-icon-disabled.component.html', 'HTML')
 	];
 
 	readonly codeSnippetsTertiaryRepeatNoText = [
-		new SourceCode(
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-			require('!!raw-loader!./previews/tertiary-repeat-no-text/button-example-tertiary-repeat-no-text.component.html').default as string,
-			'HTML'
-		)
+		this.getSnippet('button', 'tertiary-repeat-no-text/button-example-tertiary-repeat-no-text.component.html', 'HTML')
 	];
 }
