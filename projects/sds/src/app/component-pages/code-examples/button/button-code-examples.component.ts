81d97c78134fb9d3e44fbdc02c781ac88e992e94
René Giller <rene.giller@bit.admin.ch>
2023-06-12 16:35:17 +0200

    feat(sds/component-pages): add a title to button previews
    OUI-2350

--- projects/sds/src/app/component-pages/code-examples/button/button-code-examples.component.ts
+++ projects/sds/src/app/component-pages/code-examples/button/button-code-examples.component.ts
@@ -18,26 +18,31 @@ export class ButtonCodeExamplesComponent extends CodeExamples {
 		{
 			component: ButtonExamplePrimaryLinkFrownComponent,
 			idParts: ['primary', 'link', 'frown'],
+			title: 'Primary link with icon',
 			snippets: [this.getSnippet('button', 'primary-link-frown/button-example-primary-link-frown.component.html', 'HTML')]
 		},
 		{
 			component: ButtonExamplePrimaryLoginDisabledComponent,
 			idParts: ['primary', 'login', 'disabled'],
+			title: 'Primary disabled button with icon',
 			snippets: [this.getSnippet('button', 'primary-login-disabled/button-example-primary-login-disabled.component.html', 'HTML')]
 		},
 		{
 			component: ButtonExampleSecondaryLinkNoIconComponent,
 			idParts: ['secondary', 'no-icon'],
+			title: 'Secondary link without icon',
 			snippets: [this.getSnippet('button', 'secondary-link-no-icon/button-example-secondary-link-no-icon.component.html', 'HTML')]
 		},
 		{
 			component: ButtonExampleSecondaryWheelchairComponent,
 			idParts: ['secondary', 'wheelchair'],
+			title: 'Secondary button with icon',
 			snippets: [this.getSnippet('button', 'secondary-wheelchair/button-example-secondary-wheelchair.component.html', 'HTML')]
 		},
 		{
 			component: ButtonExampleTertiaryLinkNoIconDisabledComponent,
 			idParts: ['tertiary', 'link', 'no-icon', 'disabled'],
+			title: 'Tertiary link without icon',
 			snippets: [
 				this.getSnippet('button', 'tertiary-link-no-icon-disabled/button-example-tertiary-link-no-icon-disabled.component.html', 'HTML')
 			]
@@ -45,6 +50,7 @@ export class ButtonCodeExamplesComponent extends CodeExamples {
 		{
 			component: ButtonExampleTertiaryRepeatNoTextComponent,
 			idParts: ['tertiary', 'repeat', 'no-text'],
+			title: 'Tertiary icon link',
 			snippets: [this.getSnippet('button', 'tertiary-repeat-no-text/button-example-tertiary-repeat-no-text.component.html', 'HTML')]
 		}
 	];
