ebc476695643c95c66c1177b499e23a212aecc38
Ciril Saner <Ciril.Saner@bit.admin.ch>
2023-08-03 14:22:30 +0200

    feat(sds/button): add TS files to the previews

--- projects/sds/src/app/component-pages/code-examples/button/button-code-examples.component.ts
+++ projects/sds/src/app/component-pages/code-examples/button/button-code-examples.component.ts
@@ -24,39 +24,55 @@ export class ButtonCodeExamplesComponent extends CodeExamples {
 			component: ButtonExamplePrimaryLinkFrownComponent,
 			idParts: ['primary', 'link', 'frown'],
 			title: 'Primary link with icon',
-			snippets: [this.getSnippet('button', 'primary-link-frown/button-example-primary-link-frown.component.html', 'HTML')]
+			snippets: [
+				this.getSnippet('button', 'primary-link-frown/button-example-primary-link-frown.component.html', 'HTML'),
+				this.getSnippet('button', 'primary-link-frown/button-example-primary-link-frown.component.ts', 'TS')
+			]
 		},
 		{
 			component: ButtonExamplePrimaryLoginDisabledComponent,
 			idParts: ['primary', 'login', 'disabled'],
 			title: 'Primary disabled button with icon',
-			snippets: [this.getSnippet('button', 'primary-login-disabled/button-example-primary-login-disabled.component.html', 'HTML')]
+			snippets: [
+				this.getSnippet('button', 'primary-login-disabled/button-example-primary-login-disabled.component.html', 'HTML'),
+				this.getSnippet('button', 'primary-login-disabled/button-example-primary-login-disabled.component.ts', 'TS')
+			]
 		},
 		{
 			component: ButtonExampleSecondaryLinkNoIconComponent,
 			idParts: ['secondary', 'no-icon'],
 			title: 'Secondary link without icon',
-			snippets: [this.getSnippet('button', 'secondary-link-no-icon/button-example-secondary-link-no-icon.component.html', 'HTML')]
+			snippets: [
+				this.getSnippet('button', 'secondary-link-no-icon/button-example-secondary-link-no-icon.component.html', 'HTML'),
+				this.getSnippet('button', 'secondary-link-no-icon/button-example-secondary-link-no-icon.component.ts', 'TS')
+			]
 		},
 		{
 			component: ButtonExampleSecondaryWheelchairComponent,
 			idParts: ['secondary', 'wheelchair'],
 			title: 'Secondary button with icon',
-			snippets: [this.getSnippet('button', 'secondary-wheelchair/button-example-secondary-wheelchair.component.html', 'HTML')]
+			snippets: [
+				this.getSnippet('button', 'secondary-wheelchair/button-example-secondary-wheelchair.component.html', 'HTML'),
+				this.getSnippet('button', 'secondary-wheelchair/button-example-secondary-wheelchair.component.ts', 'TS')
+			]
 		},
 		{
 			component: ButtonExampleTertiaryLinkNoIconDisabledComponent,
 			idParts: ['tertiary', 'link', 'no-icon', 'disabled'],
 			title: 'Tertiary link without icon',
 			snippets: [
-				this.getSnippet('button', 'tertiary-link-no-icon-disabled/button-example-tertiary-link-no-icon-disabled.component.html', 'HTML')
+				this.getSnippet('button', 'tertiary-link-no-icon-disabled/button-example-tertiary-link-no-icon-disabled.component.html', 'HTML'),
+				this.getSnippet('button', 'tertiary-link-no-icon-disabled/button-example-tertiary-link-no-icon-disabled.component.ts', 'TS')
 			]
 		},
 		{
 			component: ButtonExampleTertiaryRepeatNoTextComponent,
 			idParts: ['tertiary', 'repeat', 'no-text'],
 			title: 'Tertiary icon link',
-			snippets: [this.getSnippet('button', 'tertiary-repeat-no-text/button-example-tertiary-repeat-no-text.component.html', 'HTML')]
+			snippets: [
+				this.getSnippet('button', 'tertiary-repeat-no-text/button-example-tertiary-repeat-no-text.component.html', 'HTML'),
+				this.getSnippet('button', 'tertiary-repeat-no-text/button-example-tertiary-repeat-no-text.component.ts', 'TS')
+			]
 		}
 	];
 }
