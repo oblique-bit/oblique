8bb007448f83603b70baf55c901648569b425410
Albert Marsnik <albert.marsnik@bit.admin.ch>
2023-08-15 15:21:15 +0200

    feat(sandbox/material): add slide toggle page
    - includes addition of link to slide toggle page within material sub menu
    
    OUI-2275 / OUI-2391

--- projects/sandbox/src/app/app.component.ts
+++ projects/sandbox/src/app/app.component.ts
@@ -169,6 +169,7 @@ export class AppComponent implements OnDestroy {
 				{url: 'list', label: 'List'},
 				{url: 'mandatory', label: 'Mandatory'},
 				{url: 'progress-bar', label: 'Progress bar'},
+				{url: 'slide-toggle', label: 'Slide Toggle'},
 				{url: 'slider', label: 'Slider'},
 				{url: 'stepper-horizontal', label: 'Stepper horizontal'},
 				{url: 'stepper-vertical', label: 'Stepper vertical'},
