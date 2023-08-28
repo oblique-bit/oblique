d7b51b1910edb24c4ba31d0a5047d242a213de46
Albert Marsnik <albert.marsnik@bit.admin.ch>
2023-08-28 10:41:59 +0200

    fix(sds/component-pages): `list-group` examples cleanup
    - remove `togglePosition="before"` from `list-group` checkbox examples
    - remove `mat-divider` from all `list-group` examples
    - replace icons with avatars in 1 example (a different example is still using icons)
    - add subheader to avatar example
    - add image example (including svg in assets for this example)
    - adjust redundant `list-group` code examples paths
    - make small adjustments to `title`s & `idParts` of examples
    
    OUI-2356

--- projects/sds/src/app/component-pages/code-examples/list-group/list-group-code-examples.component.ts
+++ projects/sds/src/app/component-pages/code-examples/list-group/list-group-code-examples.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {CodeExample, CodeExamples} from '../../code-examples.model';
-import {ListGroupExampleDefaultComponent} from './previews/list-group-example-default/list-group-example-default.component';
-import {ListGroupExampleCheckBoxComponent} from './previews/list-group-example-checkbox/list-group-example-checkbox.component';
-import {ListGroupExampleIconComponent} from './previews/list-group-example-icon/list-group-example-icon.component';
-import {ListGroupExampleIconCheckboxComponent} from './previews/list-group-example-icon-checkbox/list-group-example-icon-checkbox.component';
+import {ListGroupExampleDefaultComponent} from './previews/default/list-group-example-default.component';
+import {ListGroupExampleCheckBoxComponent} from './previews/checkbox/list-group-example-checkbox.component';
+import {ListGroupExampleAvatarSubheaderComponent} from './previews/avatar-subheader/list-group-example-avatar-subheader.component';
+import {ListGroupExampleIconCheckboxComponent} from './previews/icon-checkbox/list-group-example-icon-checkbox.component';
 import {IdPipe} from '../../../shared/id/id.pipe';
 import {CodeExampleComponent} from '../../code-example/code-example.component';
 import {CommonModule} from '@angular/common';
+import {ListGroupExampleImagePreviewComponent} from '../list-group/previews/image/list-group-example-image-preview.component';
 
 @Component({
 	selector: 'app-list-group-code-examples',
@@ -20,38 +21,47 @@ export class ListGroupCodeExamplesComponent extends CodeExamples {
 	readonly previews: CodeExample[] = [
 		{
 			component: ListGroupExampleDefaultComponent,
-			idParts: ['default'],
-			title: 'Default list group',
+			idParts: ['list', 'group', 'default'],
+			title: 'Default',
 			snippets: [
-				this.getSnippet('list-group', 'list-group-example-default/list-group-example-default.component.html', 'HTML'),
-				this.getSnippet('list-group', 'list-group-example-default/list-group-example-default.component.ts', 'TS')
+				this.getSnippet('list-group', 'default/list-group-example-default.component.html', 'HTML'),
+				this.getSnippet('list-group', 'default/list-group-example-default.component.ts', 'TS')
 			]
 		},
 		{
 			component: ListGroupExampleCheckBoxComponent,
-			idParts: ['list group', 'checkbox'],
-			title: 'List group with checkbox',
+			idParts: ['list', 'group', 'checkbox'],
+			title: 'Checkbox',
 			snippets: [
-				this.getSnippet('list-group', 'list-group-example-checkbox/list-group-example-checkbox.component.html', 'HTML'),
-				this.getSnippet('list-group', 'list-group-example-checkbox/list-group-example-checkbox.component.ts', 'TS')
+				this.getSnippet('list-group', 'checkbox/list-group-example-checkbox.component.html', 'HTML'),
+				this.getSnippet('list-group', 'checkbox/list-group-example-checkbox.component.ts', 'TS')
 			]
 		},
 		{
-			component: ListGroupExampleIconComponent,
-			idParts: ['list group', 'icon'],
-			title: 'List group with icons',
+			component: ListGroupExampleAvatarSubheaderComponent,
+			idParts: ['list', 'group', 'avatar', 'subheader'],
+			title: 'Avatar & subheader',
 			snippets: [
-				this.getSnippet('list-group', 'list-group-example-icon/list-group-example-icon.component.html', 'HTML'),
-				this.getSnippet('list-group', 'list-group-example-icon/list-group-example-icon.component.ts', 'TS')
+				this.getSnippet('list-group', 'avatar-subheader/list-group-example-avatar-subheader.component.html', 'HTML'),
+				this.getSnippet('list-group', 'avatar-subheader/list-group-example-avatar-subheader.component.ts', 'TS')
 			]
 		},
 		{
 			component: ListGroupExampleIconCheckboxComponent,
-			idParts: ['list group', 'icon', 'checkbox'],
-			title: 'List group with icons and checkbox',
+			idParts: ['list', 'group', 'icon', 'checkbox'],
+			title: 'Icons & checkbox',
 			snippets: [
-				this.getSnippet('list-group', 'list-group-example-icon-checkbox/list-group-example-icon-checkbox.component.html', 'HTML'),
-				this.getSnippet('list-group', 'list-group-example-icon-checkbox/list-group-example-icon-checkbox.component.ts', 'TS')
+				this.getSnippet('list-group', 'icon-checkbox/list-group-example-icon-checkbox.component.html', 'HTML'),
+				this.getSnippet('list-group', 'icon-checkbox/list-group-example-icon-checkbox.component.ts', 'TS')
+			]
+		},
+		{
+			component: ListGroupExampleImagePreviewComponent,
+			idParts: ['image'],
+			title: 'Image',
+			snippets: [
+				this.getSnippet('list-group', 'image/list-group-example-image-preview.component.html', 'HTML'),
+				this.getSnippet('list-group', 'image/list-group-example-image-preview.component.ts', 'TS')
 			]
 		}
 	];
--- projects/sds/src/app/component-pages/code-examples/list-group/previews/image/list-group-example-image-preview.component.ts
+++ projects/sds/src/app/component-pages/code-examples/list-group/previews/image/list-group-example-image-preview.component.ts
@@ -1,41 +1,38 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {PreviewComponent} from '../../../../code-examples.model';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
 import {CommonModule} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
+	selector: 'app-list-group-example-image-preview',
+	templateUrl: './list-group-example-image-preview.component.html',
 	standalone: true,
-	selector: 'app-list-group-example-icon',
-	templateUrl: './list-group-example-icon.component.html',
 	imports: [MatListModule, MatIconModule, CommonModule],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ListGroupExampleIconComponent implements PreviewComponent {
+export class ListGroupExampleImagePreviewComponent implements PreviewComponent {
 	readonly contentForList = [
 		{
 			id: 'list-item-1',
 			label: 'List item 1',
 			line1: 'Cadrozzi AG',
 			line2: 'CHE-123.456.789 (active)',
-			line3: 'Musterstrasse 99, 3000 Bern',
-			icon: 'user-checkmark'
+			line3: 'Musterstrasse 99, 3000 Bern'
 		},
 		{
 			id: 'list-item-2',
 			label: 'List item 1',
 			line1: 'Bananopa AG',
 			line2: 'CHE-123.633.789',
-			line3: 'Laupenfuss 59, 3002 Bern',
-			icon: 'user'
+			line3: 'Laupenfuss 59, 3002 Bern'
 		},
 		{
 			id: 'list-item-3',
 			label: 'List item 1',
 			line1: 'Lavani GmbH',
 			line2: 'CHE-123.456.408 (active)',
-			line3: 'Taubendorfstrasse 45, 3004 Bern',
-			icon: 'user-checkmark'
+			line3: 'Taubendorfstrasse 45, 3004 Bern'
 		}
 	];
 }
