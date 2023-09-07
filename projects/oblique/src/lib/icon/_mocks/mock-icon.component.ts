659bc8797abd8a6d3d55e12da5455e456763bc3b
Nina Egger <nina.egger@bit.admin.ch>
2023-09-07 11:38:29 +0200

    feat(oblique/icon): update to standalone
    OUI-2258

--- projects/oblique/src/lib/icon/_mocks/mock-icon.component.ts
+++ projects/oblique/src/lib/icon/_mocks/mock-icon.component.ts
@@ -2,7 +2,8 @@ import {Component, Input} from '@angular/core';
 
 @Component({
 	selector: 'ob-icon',
-	template: ''
+	template: '',
+	standalone: true
 })
 export class ObMockIconComponent {
 	@Input() icon: string;
