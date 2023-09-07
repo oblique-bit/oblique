e6e9b49b57d3621bf007a612edd107b58f4ed2d8
Nina Egger <nina.egger@bit.admin.ch>
2023-09-07 11:17:48 +0200

    feat(oblique): update mocks to standalone
    OUI-2258

--- projects/oblique/src/lib/_mocks/mock-mat-element.ts
+++ projects/oblique/src/lib/_mocks/mock-mat-element.ts
@@ -4,7 +4,8 @@ import {Component} from '@angular/core';
 @Component({
 	// eslint-disable-next-line @angular-eslint/component-selector, @angular-eslint/component-class-suffix
 	selector: 'mat-select, mat-slide-toggle, mat-slider',
-	template: ``
+	template: ``,
+	standalone: true
 })
 // eslint-disable-next-line @angular-eslint/component-class-suffix
 export class ObMockMatElement implements ControlValueAccessor {
