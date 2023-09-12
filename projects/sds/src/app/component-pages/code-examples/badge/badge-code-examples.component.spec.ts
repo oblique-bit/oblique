1fa46096a2628b62b2aeb35c2706ca282f8d8c76
Albert Marsnik <albert.marsnik@bit.admin.ch>
2023-09-12 15:49:16 +0200

    test(sds/component-pages): improve `badge-code-examples` tests
    - replace `'BadgeCodeExamplesComponent'` in favor of `BadgeCodeExamplesComponent.name`
    - adjust 1st test to use `test` in favor of `it`
    - add additional tests confirming content of badge examples
    
    OUI-2362 / OUI-2446

--- projects/sds/src/app/component-pages/code-examples/badge/badge-code-examples.component.spec.ts
+++ projects/sds/src/app/component-pages/code-examples/badge/badge-code-examples.component.spec.ts
@@ -1,8 +1,13 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {BadgeCodeExamplesComponent} from './badge-code-examples.component';
 import {IdPipe} from '../../../shared/id/id.pipe';
+import {CodeExampleComponent} from '../../code-example/code-example.component';
+import {By} from '@angular/platform-browser';
+import {BadgeExampleColorsComponent} from './previews/colors/badge-example-colors.component';
+import {BadgeExampleOtherOptionsComponent} from './previews/other-options/badge-example-other-options.component';
+import {MatBadge} from '@angular/material/badge';
 
-describe('BadgeCodeExamplesComponent', () => {
+describe(BadgeCodeExamplesComponent.name, () => {
 	let component: BadgeCodeExamplesComponent;
 	let fixture: ComponentFixture<BadgeCodeExamplesComponent>;
 
@@ -16,7 +21,23 @@ describe('BadgeCodeExamplesComponent', () => {
 		fixture.detectChanges();
 	});
 
-	it('should create', () => {
+	test('that creation works', () => {
 		expect(component).toBeTruthy();
+	});
+
+	test(`that there are 2 ${CodeExampleComponent.name}s`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(CodeExampleComponent)).length).toBe(2);
+	});
+
+	test(`that there is 1 ${BadgeExampleColorsComponent.name}`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(BadgeExampleColorsComponent)).length).toBe(1);
+	});
+
+	test(`that there is 1 ${BadgeExampleOtherOptionsComponent.name}`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(BadgeExampleOtherOptionsComponent)).length).toBe(1);
+	});
+
+	test(`that there are 11 ${MatBadge.name}s`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(MatBadge)).length).toBe(11);
 	});
 });
