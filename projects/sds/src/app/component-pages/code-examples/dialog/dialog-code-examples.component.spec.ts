5eaf795a66b3f8cc125ec76e6c19a19843a40a29
Albert Marsnik <albert.marsnik@bit.admin.ch>
2023-09-12 11:54:27 +0200

    test(sds/component-pages): add dialog tests
    - adjust 1st test to use `test` in favor of `it`
    - add additional tests confirming content of dialog examples
    
    OUI-2362 / OUI2445

--- projects/sds/src/app/component-pages/code-examples/dialog/dialog-code-examples.component.spec.ts
+++ projects/sds/src/app/component-pages/code-examples/dialog/dialog-code-examples.component.spec.ts
@@ -1,5 +1,10 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {DialogCodeExamplesComponent} from './dialog-code-examples.component';
+import {CodeExampleComponent} from '../../code-example/code-example.component';
+import {By} from '@angular/platform-browser';
+import {DialogExampleSpinnerComponent} from './previews/spinner/dialog-example-spinner.component';
+import {DialogExampleDefaultComponent} from './previews/default/dialog-example-default.component';
+import {ObButtonDirective} from '@oblique/oblique';
 
 describe(DialogCodeExamplesComponent.name, () => {
 	let component: DialogCodeExamplesComponent;
@@ -15,7 +20,29 @@ describe(DialogCodeExamplesComponent.name, () => {
 		fixture.detectChanges();
 	});
 
-	test('should create', () => {
+	test('that creation works', () => {
 		expect(component).toBeTruthy();
+	});
+
+	test(`that there are 2 ${CodeExampleComponent.name}s`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(CodeExampleComponent)).length).toBe(2);
+	});
+
+	test(`that there is 1 ${DialogExampleDefaultComponent.name}`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(DialogExampleDefaultComponent)).length).toBe(1);
+	});
+
+	test(`that there is 1 ${DialogExampleSpinnerComponent.name}`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(DialogExampleSpinnerComponent)).length).toBe(1);
+	});
+
+	test(`that there are 2 ${ObButtonDirective.name}s`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(ObButtonDirective)).length).toBe(2);
+	});
+
+	test.each<{index: number}>([{index: 0}, {index: 1}])(`that all ${ObButtonDirective.name}s contain text "Open dialog"`, ({index}) => {
+		expect(
+			(fixture.debugElement.queryAll(By.directive(ObButtonDirective))[index].nativeElement as {textContent: string}).textContent
+		).toContain('Open dialog');
 	});
 });
