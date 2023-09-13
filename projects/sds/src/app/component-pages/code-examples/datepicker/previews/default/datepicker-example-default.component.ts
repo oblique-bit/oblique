fdf6230d26b125da7adf242937845b45b953aa33
Albert Marsnik <albert.marsnik@bit.admin.ch>
2023-09-13 09:08:05 +0200

    feat(sds/component-pages): make datepicker adhere to sds component documentation guidelines
    - add `datepicker-example-locale` preview
    - add `datepicker-example-other-options` preview
    - make small improvements to `datepicker-example-default` preview
    	- change label to "Default"
    	- correct format hint
    	- add `ObFormFieldModule`
    
    OUI-2362 / OUI-2447
    
    feat(sds/component-pages): remove locale example

--- projects/sds/src/app/component-pages/code-examples/datepicker/previews/default/datepicker-example-default.component.ts
+++ projects/sds/src/app/component-pages/code-examples/datepicker/previews/default/datepicker-example-default.component.ts
@@ -3,10 +3,11 @@ import {Component} from '@angular/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {ObFormFieldModule} from '@oblique/oblique';
 
 @Component({
 	standalone: true,
-	imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
+	imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, ObFormFieldModule],
 	selector: 'app-datepicker-example-default',
 	templateUrl: './datepicker-example-default.component.html'
 })
---
+++ projects/sds/src/app/component-pages/code-examples/datepicker/previews/other-options/datepicker-example-other-options-preview.component.ts
@@ -0,0 +1,46 @@
+import {Component} from '@angular/core';
+import {PreviewComponent} from '../../../../code-examples.model';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerInputEvent, MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {DatePipe, JsonPipe, NgIf} from '@angular/common';
+import {ObFormFieldModule} from '@oblique/oblique';
+
+@Component({
+	selector: 'app-datepicker-example-other-options-preview',
+	templateUrl: './datepicker-example-other-options-preview.component.html',
+	styleUrls: ['../../../../code-example-flex-layout.scss'],
+	standalone: true,
+	imports: [
+		DatePipe,
+		JsonPipe,
+		MatFormFieldModule,
+		MatInputModule,
+		MatDatepickerModule,
+		MatNativeDateModule,
+		NgIf,
+		ObFormFieldModule,
+		ReactiveFormsModule
+	]
+})
+export class DatepickerExampleOtherOptionsPreviewComponent implements PreviewComponent {
+	readonly august1st1891 = new Date(1891, 7, 1);
+	readonly errorControl = new FormControl(new Date(), [
+		function (control: FormControl) {
+			return new Date((control.value as Date).toDateString()) < new Date(new Date().toDateString()) ? {pastDate: true} : null;
+		}
+	]);
+	lastDateChange: MatDatepickerInputEvent<any>;
+	lastDateInput: MatDatepickerInputEvent<any>;
+	readonly today = new Date();
+
+	onDateChange(event: MatDatepickerInputEvent<any>): void {
+		this.lastDateChange = event;
+	}
+
+	onDateInput(event: MatDatepickerInputEvent<any>): void {
+		this.lastDateInput = event;
+	}
+}
