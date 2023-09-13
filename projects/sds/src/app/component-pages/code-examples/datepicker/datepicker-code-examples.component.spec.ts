5a88368a560460dae60bc12a7f926162659637f4
Albert Marsnik <albert.marsnik@bit.admin.ch>
2023-09-13 09:14:36 +0200

    test(sds/component-pages): improve`datepicker-code-examples` tests
    - cleanup wording in 1st test
    - add tests confirming content in examples
    
    OUI-2362 / OUI-2447

--- projects/sds/src/app/component-pages/code-examples/datepicker/datepicker-code-examples.component.spec.ts
+++ projects/sds/src/app/component-pages/code-examples/datepicker/datepicker-code-examples.component.spec.ts
@@ -3,6 +3,9 @@ import {By} from '@angular/platform-browser';
 import {DatepickerCodeExamplesComponent} from './datepicker-code-examples.component';
 import {CodeExampleComponent} from '../../code-example/code-example.component';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {DatepickerExampleDefaultComponent} from './previews/default/datepicker-example-default.component';
+import {MatDatepicker} from '@angular/material/datepicker';
+import {DatepickerExampleOtherOptionsPreviewComponent} from './previews/other-options/datepicker-example-other-options-preview.component';
 
 describe(DatepickerCodeExamplesComponent.name, () => {
 	let component: DatepickerCodeExamplesComponent;
@@ -18,11 +21,23 @@ describe(DatepickerCodeExamplesComponent.name, () => {
 		fixture.detectChanges();
 	});
 
-	test('should create', () => {
+	test('that creation works', () => {
 		expect(component).toBeTruthy();
 	});
 
-	test('should have 1 CodeExampleComponent', () => {
-		expect(fixture.debugElement.queryAll(By.directive(CodeExampleComponent)).length).toBe(1);
+	test(`that there are 2 ${CodeExampleComponent.name}s`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(CodeExampleComponent)).length).toBe(2);
+	});
+
+	test(`that there is 1 ${DatepickerExampleDefaultComponent.name}`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(DatepickerExampleDefaultComponent)).length).toBe(1);
+	});
+
+	test(`that there is 1 ${DatepickerExampleOtherOptionsPreviewComponent.name}`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(DatepickerExampleOtherOptionsPreviewComponent)).length).toBe(1);
+	});
+
+	test(`that there are 8 ${MatDatepicker.name}s`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(MatDatepicker)).length).toBe(8);
 	});
 });
