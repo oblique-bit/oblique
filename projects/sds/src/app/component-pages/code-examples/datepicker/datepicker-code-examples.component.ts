e8f454427af91f887002d5e6b44dd8e3594dc579
Albert Marsnik <albert.marsnik@bit.admin.ch>
2023-09-13 09:08:05 +0200

    feat(sds/component-pages): make datepicker adhere to sds component documentation guidelines
    - add `datepicker-example-locale` preview
    - add `datepicker-example-other-options` preview
    - make small improvements to `datepicker-example-default` preview
    	- change label to "Default"
    	- correct format hint
    	- add `ObFormFieldModule`
    
    OUI-2362 / OUI-2447
    
    feat(sds/component-pages): remove locale example

--- projects/sds/src/app/component-pages/code-examples/datepicker/datepicker-code-examples.component.ts
+++ projects/sds/src/app/component-pages/code-examples/datepicker/datepicker-code-examples.component.ts
@@ -4,6 +4,7 @@ import {CodeExample, CodeExamples} from './../../code-examples.model';
 import {CodeExampleComponent} from '../../code-example/code-example.component';
 import {IdPipe} from '../../../shared/id/id.pipe';
 import {CommonModule} from '@angular/common';
+import {DatepickerExampleOtherOptionsPreviewComponent} from '../datepicker/previews/other-options/datepicker-example-other-options-preview.component';
 
 @Component({
 	selector: 'app-datepicker-code-examples',
@@ -20,6 +21,15 @@ export class DatepickerCodeExamplesComponent extends CodeExamples {
 			snippets: [
 				this.getSnippet('datepicker', 'default/datepicker-example-default.component.html', 'HTML'),
 				this.getSnippet('datepicker', 'default/datepicker-example-default.component.ts', 'TS')
+			]
+		},
+		{
+			component: DatepickerExampleOtherOptionsPreviewComponent,
+			idParts: ['other', 'options'],
+			title: 'Other options',
+			snippets: [
+				this.getSnippet('datepicker', 'other-options/datepicker-example-other-options-preview.component.html', 'HTML'),
+				this.getSnippet('datepicker', 'other-options/datepicker-example-other-options-preview.component.ts', 'TS')
 			]
 		}
 	];
