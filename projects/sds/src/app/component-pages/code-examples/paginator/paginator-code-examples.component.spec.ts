c7d05c5cc1d5d461efd46613d2ce9f38e0f759d7
Albert Marsnik <albert.marsnik@bit.admin.ch>
2023-09-19 09:26:11 +0200

    test(sds/component-pages): improve `paginator-code-examples` tests
    - remove brackets around component name in describe
    - add test confirming that there are 2 `CodeExampleComponent`s
    - add test confirming that there are 13 `MatPaginator`s
    
    OUI-2362 / OUI-2449

--- projects/sds/src/app/component-pages/code-examples/paginator/paginator-code-examples.component.spec.ts
+++ projects/sds/src/app/component-pages/code-examples/paginator/paginator-code-examples.component.spec.ts
@@ -1,8 +1,11 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {PaginatorCodeExamplesComponent} from './paginator-code-examples.component';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {CodeExampleComponent} from '../../code-example/code-example.component';
+import {By} from '@angular/platform-browser';
+import {MatPaginator} from '@angular/material/paginator';
 
-describe(`${PaginatorCodeExamplesComponent.name}`, () => {
+describe(PaginatorCodeExamplesComponent.name, () => {
 	let fixture: ComponentFixture<PaginatorCodeExamplesComponent>;
 	let component: PaginatorCodeExamplesComponent;
 
@@ -16,7 +19,15 @@ describe(`${PaginatorCodeExamplesComponent.name}`, () => {
 		fixture.detectChanges();
 	});
 
-	test('create', () => {
+	test('that creation works', () => {
 		expect(component).toBeTruthy();
+	});
+
+	test(`that there are 2 ${CodeExampleComponent.name}s`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(CodeExampleComponent)).length).toBe(2);
+	});
+
+	test(`that there are 13 ${MatPaginator.name}s`, () => {
+		expect(fixture.debugElement.queryAll(By.directive(MatPaginator)).length).toBe(13);
 	});
 });
