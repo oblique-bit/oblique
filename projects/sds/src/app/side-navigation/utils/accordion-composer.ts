76b16bddc744d7f9d4e9fb7421615f7edfdbc084
Ciril Saner <Ciril.Saner@bit.admin.ch>
2023-10-27 14:35:02 +0200

    refactor(sds): create new model for CMS pages info used in the side-nav and refactor accordingly
    OUI-2552

--- projects/sds/src/app/side-navigation/utils/accordion-composer.ts
+++ projects/sds/src/app/side-navigation/utils/accordion-composer.ts
@@ -1,20 +1,19 @@
 import {CategoryCms} from '../../cms/models/category.model';
-import {TabbedPageShortCms} from '../../cms/models/tabbed-page.model';
-import {DocumentationPageShortCms} from '../../cms/models/documentation-page.model';
+import {CMSPageShortList} from '../../cms/models/cms-page.model';
 import {Accordion} from '../accordion-links/accordion-links.model';
 import {AccordionMapper} from './accordion-mapper';
 
 export class AccordionComposer {
 	static composeAccordions(value: {
 		categories: CategoryCms;
-		componentAccordions: TabbedPageShortCms;
-		documentationAccordions: DocumentationPageShortCms;
+		componentAccordions: CMSPageShortList;
+		documentationAccordions: CMSPageShortList;
 	}): Accordion[] {
 		const accordions: Accordion[] = [];
 
 		value.categories.data
 			.sort((c1, c2) => c2.name.localeCompare(c1.name)) // This is a small hack to have the categories Introductions & Guidelines in the correct order. This maight not scale if other categories are added. This must be deleted when the sorting will done through the CMS.
-			.forEach(category => accordions.push(AccordionMapper.mapDocumentationPageShortToAccordion(value.documentationAccordions, category)));
+			.forEach(category => accordions.push(AccordionMapper.mapCMSPageShortToAccordion(value.documentationAccordions, category)));
 
 		accordions.push(AccordionMapper.mapTabbedPageShortToAccordion(value.componentAccordions));
 
