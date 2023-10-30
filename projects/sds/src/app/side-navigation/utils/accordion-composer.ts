54217b40fafe6969cef5d31ae5a328dfcae4d9ac
Ciril Saner <Ciril.Saner@bit.admin.ch>
2023-10-30 09:09:50 +0100

    refactor(sds/side-navigation): rename `documentation-pages` to `text-pages`
    OUI-2552

--- projects/sds/src/app/side-navigation/utils/accordion-composer.ts
+++ projects/sds/src/app/side-navigation/utils/accordion-composer.ts
@@ -4,14 +4,10 @@ import {Accordion} from '../accordion-links/accordion-links.model';
 import {AccordionMapper} from './accordion-mapper';
 
 export class AccordionComposer {
-	static composeAccordions(value: {
-		categories: CategoryCms;
-		tabbedPages: CMSPageShortList;
-		documentationPages: CMSPageShortList;
-	}): Accordion[] {
+	static composeAccordions(value: {categories: CategoryCms; tabbedPages: CMSPageShortList; textPages: CMSPageShortList}): Accordion[] {
 		const accordions: Accordion[] = [];
 
-		const allPages: CMSPage[] = [...value.tabbedPages.data, ...value.documentationPages.data];
+		const allPages: CMSPage[] = [...value.tabbedPages.data, ...value.textPages.data];
 
 		value.categories.data
 			.sort((c1, c2) => c2.name.localeCompare(c1.name)) // This is a small hack to have the categories Introductions & Guidelines in the correct order. This maight not scale if other categories are added. This must be deleted when the sorting will done through the CMS.
