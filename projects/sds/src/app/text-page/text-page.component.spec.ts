fb94c7cfc2808a23327e03804643e294a8d0736b
Ciril Saner <Ciril.Saner@bit.admin.ch>
2023-10-30 09:02:55 +0100

    refactor(sds): rename `documentation-pages` to `text-page` and refactor accordingly
    OUI-2552

--- projects/sds/src/app/text-page/text-page.component.spec.ts
+++ projects/sds/src/app/text-page/text-page.component.spec.ts
@@ -1,19 +1,19 @@
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
-import {DocumentationPagesComponent} from './documentation-pages.component';
+import {TextPageComponent} from './text-page.component';
 import {IdPipe} from '../shared/id/id.pipe';
 
-describe('DocumentationPagesComponent', () => {
-	let component: DocumentationPagesComponent;
-	let fixture: ComponentFixture<DocumentationPagesComponent>;
+describe(TextPageComponent.name, () => {
+	let component: TextPageComponent;
+	let fixture: ComponentFixture<TextPageComponent>;
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [HttpClientTestingModule, IdPipe, RouterTestingModule, DocumentationPagesComponent]
+			imports: [HttpClientTestingModule, IdPipe, RouterTestingModule, TextPageComponent]
 		}).compileComponents();
 
-		fixture = TestBed.createComponent(DocumentationPagesComponent);
+		fixture = TestBed.createComponent(TextPageComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
---
+++ projects/sds/src/app/text-page/text-pages.routes.ts
@@ -0,0 +1,4 @@
+import {TextPageComponent} from './text-page.component';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+export default [{path: '', component: TextPageComponent}];
