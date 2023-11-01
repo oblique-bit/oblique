dfc49786a9ed7737d4921737086419ddee0c19fe
Olivier Percebois-Garve <olivier.percebois-garve@estv.admin.ch>
2023-11-01 16:06:26 +0100

    feat(sds/master-layout): add stackblitz code example
    OUI-2456

--- projects/sds/src/app/code-examples/code-examples/banner/banner-code-examples.component.ts
+++ projects/sds/src/app/code-examples/code-examples/banner/banner-code-examples.component.ts
@@ -1,12 +1,25 @@
-import {Component} from '@angular/core';
-import {CodeExamples} from '../../code-examples.model';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CodeExample, CodeExamples} from '../../code-examples.model';
+import {IdPipe} from '../../../shared/id/id.pipe';
 import {CommonModule} from '@angular/common';
 import {CodeExampleComponent} from '../../code-example/code-example.component';
+import {BannerExampleDefaultPreviewComponent} from './previews/default/banner-example-default-preview.component';
 
 @Component({
 	selector: 'app-code-example-banner',
-	templateUrl: './banner-code-examples.component.html',
+	templateUrl: '../../code-examples.component.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
-	imports: [CommonModule, CodeExampleComponent]
+	imports: [CommonModule, IdPipe, CodeExampleComponent]
 })
-export class BannerCodeExamplesComponent extends CodeExamples {}
+export class BannerCodeExamplesComponent extends CodeExamples {
+	readonly componentId = 'banner-examples';
+	readonly previews: CodeExample[] = [
+		{
+			component: BannerExampleDefaultPreviewComponent,
+			idParts: ['default'],
+			title: 'Link to Stackblitz Example',
+			snippets: []
+		}
+	];
+}
