a062bcba8e56940194d326086dbecc7d8a40746a
René Giller <rene.giller@bit.admin.ch>
2023-11-22 12:14:43 +0100

    refactor(sds/toolchain): use a script to copy `nginx` configuration files after a build
    Using `assets` properties on Angular.json is not possible anymore with Angular 16
    
    OUI-2554 / OUI-2609

---
+++ projects/sds/scripts/post-dist/copy-nginx.ts
@@ -0,0 +1,19 @@
+import {copyFileSync, mkdirSync} from 'fs';
+import path from 'path';
+
+export class CopyNginx {
+	private static readonly source = path.join('src', 'nginx');
+	private static readonly rootDestination = path.join('..', '..', 'dist', 'sds', 'nginx');
+	private static readonly confDestination = path.join(CopyNginx.rootDestination, 'conf', 'includes');
+
+	static perform(): void {
+		CopyNginx.copyFile('Staticfile', CopyNginx.source, CopyNginx.rootDestination);
+		CopyNginx.copyFile('custom_headers.conf', CopyNginx.source, CopyNginx.confDestination);
+		CopyNginx.copyFile('security_headers.conf', CopyNginx.source, CopyNginx.confDestination);
+	}
+
+	private static copyFile(filename: string, source: string, destination: string): void {
+		mkdirSync(destination, {recursive: true});
+		copyFileSync(path.join(source, filename), path.join(destination, filename));
+	}
+}
