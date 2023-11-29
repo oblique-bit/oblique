e80149f0695659c9623a6af709df42fe34fc36b3
Simao Dias Almeida <simao.diasalmeida@bit.admin.ch>
2023-11-29 13:53:24 +0100

    test(oblique/column-layout): enforce testing conventions in component tests
    OUI-2120

--- projects/oblique/src/lib/column-layout/column-layout.component.spec.ts
+++ projects/oblique/src/lib/column-layout/column-layout.component.spec.ts
@@ -61,11 +61,11 @@ describe(ObColumnLayoutComponent.name, () => {
 				.map(element => element.injector.get(ObColumnPanelDirective));
 		});
 
-		it('should create', () => {
+		test('that it is created', () => {
 			expect(component).toBeTruthy();
 		});
 
-		it('should contain columnLayout class', () => {
+		test('that it contains columnLayout class', () => {
 			expect(fixture.debugElement.nativeElement.classList).toContain('ob-column-layout');
 		});
 
@@ -73,17 +73,17 @@ describe(ObColumnLayoutComponent.name, () => {
 			{property: 'wider', className: 'ob-wider-columns'},
 			{property: 'noLayout', className: 'ob-no-layout'}
 		])('property $property', ({property, className}) => {
-			it('should be initialized to false', () => {
+			test('that it initializes to false', () => {
 				expect(component[property]).toBe(false);
 			});
 
-			it(`should add "${className}" class when true`, () => {
+			test(`that "${className}" class is added when true`, () => {
 				component[property] = true;
 				fixture.detectChanges();
 				expect(fixture.debugElement.nativeElement.classList).toContain(className);
 			});
 
-			it(`should remove "${className}" class when false`, () => {
+			test(`that "${className}" class is removed when false`, () => {
 				component[property] = false;
 				fixture.detectChanges();
 				expect(fixture.debugElement.nativeElement.classList).not.toContain(className);
@@ -94,18 +94,18 @@ describe(ObColumnLayoutComponent.name, () => {
 			{property: 'toggleLeftIcon$', initialValue: 'left', toggledValue: 'right', index: 0},
 			{property: 'toggleRightIcon$', initialValue: 'right', toggledValue: 'left', index: 1}
 		])('property $property', ({property, initialValue, toggledValue, index}) => {
-			it('should be an observable', () => {
+			test('that it is an observable', () => {
 				expect(component[property] instanceof Observable).toBe(true);
 			});
 
-			it(`should initially emit "${initialValue}"`, done => {
+			test(`that it initially emits "${initialValue}"`, done => {
 				component[property].subscribe(value => {
 					expect(value).toBe(initialValue);
 					done();
 				});
 			});
 
-			it(`should emit "${toggledValue}" after toggle has been toggled`, done => {
+			test(`that it emits "${toggledValue}" after toggle has been toggled`, done => {
 				component[property].pipe(skip(1)).subscribe(value => {
 					expect(value).toBe(toggledValue);
 					done();
@@ -118,13 +118,13 @@ describe(ObColumnLayoutComponent.name, () => {
 			{method: 'toggleLeft', index: 0, panel: 'left'},
 			{method: 'toggleRight', index: 1, panel: 'right'}
 		])('method $method', ({method, index, panel}) => {
-			it('should toggle the panel', () => {
+			test('that it toggles the panel', () => {
 				jest.spyOn(panels[index], 'toggle');
 				component[method]();
 				expect(panels[index].toggle).toHaveBeenCalled();
 			});
 
-			it('should not toggle the panel when panel is removed', () => {
+			test('that it does not toggle the panel when panel is removed', () => {
 				component[panel] = false;
 				fixture.detectChanges();
 				jest.spyOn(panels[index], 'toggle');
@@ -144,7 +144,7 @@ describe(ObColumnLayoutComponent.name, () => {
 				});
 			});
 
-			it('should be empty per default', () => {
+			test('that it is empty per default', () => {
 				expect(element.nativeElement.style.top).toBe('');
 			});
 
@@ -155,7 +155,7 @@ describe(ObColumnLayoutComponent.name, () => {
 					{description: 'half the height with a height less than window height', height: 767, top: '383.5px'},
 					{description: 'half the height with a height equal to window height', height: 768, top: '384px'},
 					{description: 'half the window height with a height bigger than window height', height: 769, top: '384px'}
-				])('should be $description', ({height, top}) => {
+				])('that it is $description', ({height, top}) => {
 					jest.spyOn(fixture.debugElement.nativeElement, 'getBoundingClientRect').mockReturnValue({top: 0, height, windowHeight: 768});
 					component.ngDoCheck();
 					fixture.detectChanges();
@@ -164,13 +164,13 @@ describe(ObColumnLayoutComponent.name, () => {
 			});
 
 			describe('with a top value of 10', () => {
-				it.each([
+				test.each([
 					{description: 'empty with a height of 0px', height: 0, top: ''},
 					{description: 'half the height with a height less than window height - top', height: 1, top: '0.5px'},
 					{description: 'half the height with a height less than window height - top', height: 757, top: '378.5px'},
 					{description: 'half the height with a height equal to window height - top', height: 758, top: '379px'},
 					{description: 'half the (window height - top) with a height bigger than window height - top', height: 759, top: '379px'}
-				])('should be $description', ({height, top}) => {
+				])('that it is $description', ({height, top}) => {
 					jest.spyOn(fixture.debugElement.nativeElement, 'getBoundingClientRect').mockReturnValue({top: 10, height, windowHeight: 768});
 					component.ngDoCheck();
 					fixture.detectChanges();
@@ -197,16 +197,16 @@ describe(ObColumnLayoutComponent.name, () => {
 			fixture.detectChanges();
 		});
 
-		it('should create the test component', () => {
+		test('that the test component is created', () => {
 			expect(testComponent).toBeTruthy();
 		});
 
-		it('should create the column layout component', () => {
+		test('that the column layout component is created', () => {
 			expect(component).toBeTruthy();
 		});
 
 		describe.each(['toggleLeftIcon$', 'toggleRightIcon$'])('property %s', property => {
-			it('should not be defined', () => {
+			test('that it is not defined', () => {
 				expect(component[property]).toBeUndefined();
 			});
 		});
@@ -243,11 +243,11 @@ describe(ObColumnLayoutComponent.name, () => {
 				});
 			});
 
-			it.each([
+			test.each([
 				{description: 'half the sum of the element and header heights with a smaller header height', height: 49, top: '49.5px'},
 				{description: 'empty with the header height equal to the element height', height: 50, top: ''},
 				{description: 'empty with the header height greater than the element height', height: 51, top: ''}
-			])('should be $description', ({height, top}) => {
+			])('that it is $description', ({height, top}) => {
 				resizerCallback([{contentRect: {height}}]);
 				jest
 					.spyOn(fixture.debugElement.query(By.css('ob-column-layout')).nativeElement, 'getBoundingClientRect')
