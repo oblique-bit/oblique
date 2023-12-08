18faa2fb0e6ecdb4da3986eb481fd5f530aa3a40
René Giller <rene.giller@bit.admin.ch>
2023-12-08 17:39:28 +0100

    feat(sds/code-examples): remove table examples with `ob-table-cicd` class
    OUI-2727

--- projects/sds/src/app/code-examples/code-examples/html-table/previews/styles/html-table-example-styles-preview.component.ts
+++ projects/sds/src/app/code-examples/code-examples/html-table/previews/styles/html-table-example-styles-preview.component.ts
@@ -3,7 +3,6 @@ import {Component} from '@angular/core';
 @Component({
 	selector: 'app-html-table-example-styles-preview',
 	templateUrl: './html-table-example-styles-preview.component.html',
-	styleUrls: ['./html-table-example-styles-preview.component.scss'],
 	standalone: true
 })
 export class HtmlTableExampleStylesPreviewComponent {}
