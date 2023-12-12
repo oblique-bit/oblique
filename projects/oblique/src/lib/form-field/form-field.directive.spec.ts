30fb692e44009643ca6faa270605dc8b4e758bbe
Albert Marsnik <albert.marsnik@bit.admin.ch>
2023-12-12 14:15:57 +0100

    fix(oblique/material): add has prefix & suffix classes to `ObFormFieldDirective`
    - `.ob-form-field-has-text-prefix`
    - `.ob-form-field-has-text-suffix`
    
    OUI-2616

--- projects/oblique/src/lib/form-field/form-field.directive.spec.ts
+++ projects/oblique/src/lib/form-field/form-field.directive.spec.ts
@@ -1,88 +1,121 @@
-import {ElementRef} from '@angular/core';
+import {Component, DebugElement, Type} from '@angular/core';
 import {ObFormFieldDirective} from './form-field.directive';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {ObFormFieldModule} from './form-field.module';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
-describe(`${ObFormFieldDirective.name}`, () => {
-	const createElRef = (innerHTML: string): ElementRef => ({
-		nativeElement: {
-			innerHTML
-		}
-	});
+const imports: (readonly any[] | Type<any>)[] = [FormsModule, MatFormFieldModule, MatInputModule, ObFormFieldModule];
 
-	it('should correctly identify a non textarea form field', () => {
-		const directive = new ObFormFieldDirective(
-			createElRef('<input type="text" matinput="" placeholder="Multiline" class="mat-mdc-input-element">')
-		);
-		directive.ngOnInit();
-		expect(directive.containsTextarea).toBeFalsy();
-	});
+@Component({
+	template: `<mat-form-field><input type="text" matInput placeholder="Default" class="mat-mdc-input-element" ngModel /></mat-form-field>`,
+	standalone: true,
+	imports
+})
+class DefaultTestComponent {}
 
-	it('should correctly identify a textarea form field', () => {
-		const directive = new ObFormFieldDirective(createElRef('<textarea placeholder="Mandatory" class="mat-mdc-input-element"></textarea>'));
-		directive.ngOnInit();
-		expect(directive.containsTextarea).toBeTruthy();
-	});
+@Component({
+	template: `<mat-form-field><textarea matInput placeholder="Textarea" class="mat-mdc-input-element" ngModel></textarea></mat-form-field>`,
+	standalone: true,
+	imports
+})
+class ContainsTextareaTestComponent {}
 
-	it('should correctly identify a non mat-input-server form field', () => {
-		const directive = new ObFormFieldDirective(
-			createElRef('<input type="text" matinput="" placeholder="Multiline" class="mat-mdc-input-element">')
-		);
-		directive.ngOnInit();
-		expect(directive.hasMatInputServer).toBeFalsy();
-	});
+@Component({
+	template: `<mat-form-field
+		><input type="text" matInput placeholder="Mat input server" class="mat-input-server" ngModel
+	/></mat-form-field>`,
+	standalone: true,
+	imports
+})
+class HasMatInputServerTestComponent {}
 
-	it('should correctly identify a mat-input-server form field', () => {
-		const directive = new ObFormFieldDirective(
-			createElRef('<input type="text" matinput="" placeholder="Multiline" class="mat-input-server">')
-		);
-		directive.ngOnInit();
-		expect(directive.hasMatInputServer).toBeTruthy();
-	});
+@Component({
+	template: `<mat-form-field>
+		<mat-label>Text prefix</mat-label>
+		<input type="text" matInput placeholder="Mat text prefix" ngModel />
+		<span matTextPrefix>Mat text prefix</span>
+	</mat-form-field>`,
+	standalone: true,
+	imports
+})
+class HasTextPrefixTestComponent {}
 
-	it('should correctly identify a non required-marker form field', () => {
-		const directive = new ObFormFieldDirective(
-			createElRef('<input type="text" matinput="" placeholder="Multiline" class="mat-mdc-input-element">')
-		);
-		directive.ngOnInit();
-		expect(directive.hasRequiredMarker).toBeFalsy();
-	});
+@Component({
+	template: `<mat-form-field>
+		<mat-label>Text suffix</mat-label>
+		<input type="text" matInput placeholder="Mat text suffix" ngModel />
+		<span matTextSuffix>Mat text Suffix</span>
+	</mat-form-field>`,
+	standalone: true,
+	imports
+})
+class HasTextSuffixTestComponent {}
 
-	it('should correctly identify a required-marker form field', () => {
-		const directive = new ObFormFieldDirective(
-			createElRef('<input type="text" matinput="" placeholder="Multiline" class="mat-mdc-form-field-required-marker">')
-		);
-		directive.ngOnInit();
-		expect(directive.hasRequiredMarker).toBeTruthy();
-	});
+@Component({
+	template: `<mat-form-field
+		><input type="text" matInput readonly placeholder="Readonly" class="mat-mdc-input-element" ngModel
+	/></mat-form-field>`,
+	standalone: true,
+	imports
+})
+class IsReadonlyTestComponent {}
 
-	it('should correctly identify a non readonly form field', () => {
-		const directive = new ObFormFieldDirective(
-			createElRef('<input type="text" matinput="" placeholder="Multiline" class="mat-mdc-input-element">')
-		);
-		directive.ngOnInit();
-		expect(directive.isReadonly).toBeFalsy();
-	});
+@Component({
+	template: `<mat-form-field
+		><input type="text" matInput placeholder="Multiline" class="mat-mdc-form-field-required-marker" ngModel
+	/></mat-form-field>`,
+	standalone: true,
+	imports
+})
+class IsRequiredTestComponent {}
 
-	it('should correctly identify a readonly form field', () => {
-		const directive = new ObFormFieldDirective(
-			createElRef('<input type="text" matinput="" readonly placeholder="Multiline" class="mat-mdc-input-element">')
-		);
-		directive.ngOnInit();
-		expect(directive.isReadonly).toBeTruthy();
-	});
+describe(ObFormFieldDirective.name, () => {
+	let fixture: ComponentFixture<Component>;
+	let debugElement: DebugElement;
+	let directive: ObFormFieldDirective;
 
-	it('should correctly identify a non required form field', () => {
-		const directive = new ObFormFieldDirective(
-			createElRef('<input type="text" matinput="" placeholder="Multiline" class="mat-mdc-input-element">')
-		);
-		directive.ngOnInit();
-		expect(directive.isRequired).toBeFalsy();
-	});
+	describe.each<{propertyToCheck: keyof ObFormFieldDirective; testComponentType: Type<Component>}>([
+		{propertyToCheck: 'containsTextarea', testComponentType: ContainsTextareaTestComponent},
+		{propertyToCheck: 'hasMatInputServer', testComponentType: HasMatInputServerTestComponent},
+		{propertyToCheck: 'hasRequiredMarker', testComponentType: IsRequiredTestComponent},
+		{propertyToCheck: 'hasTextPrefix', testComponentType: HasTextPrefixTestComponent},
+		{propertyToCheck: 'hasTextSuffix', testComponentType: HasTextSuffixTestComponent},
+		{propertyToCheck: 'isReadonly', testComponentType: IsReadonlyTestComponent},
+		{propertyToCheck: 'isRequired', testComponentType: IsRequiredTestComponent}
+	])('', ({propertyToCheck, testComponentType}) => {
+		const readablePropertyName = propertyToCheck
+			.replace(/(?<upperCaseLetter>[A-Z])/g, ' $1')
+			.replace(/(?<containsHasOrIs>contains|has|is) /, '')
+			.toLowerCase();
 
-	it('should correctly identify a required form field', () => {
-		const directive = new ObFormFieldDirective(
-			createElRef('<input type="text" matinput="" required placeholder="Multiline" class="mat-mdc-input-element">')
-		);
-		directive.ngOnInit();
-		expect(directive.isRequired).toBeTruthy();
+		test(`that it correctly identifies a non ${readablePropertyName} form field`, async () => {
+			await TestBed.configureTestingModule({
+				imports: [DefaultTestComponent, NoopAnimationsModule]
+			}).compileComponents();
+
+			fixture = TestBed.createComponent(DefaultTestComponent);
+			fixture.detectChanges();
+			debugElement = fixture.debugElement.query(By.directive(ObFormFieldDirective));
+			directive = debugElement.injector.get(ObFormFieldDirective);
+
+			expect(directive[propertyToCheck]).toBeFalsy();
+		});
+
+		test(`that it correctly identifies a ${readablePropertyName} form field`, async () => {
+			await TestBed.configureTestingModule({
+				imports: [testComponentType, NoopAnimationsModule]
+			}).compileComponents();
+
+			fixture = TestBed.createComponent(testComponentType);
+			fixture.detectChanges();
+			debugElement = fixture.debugElement.query(By.directive(ObFormFieldDirective));
+			directive = debugElement.injector.get(ObFormFieldDirective);
+
+			expect(directive[propertyToCheck]).toBeTruthy();
+		});
 	});
 });
--- projects/oblique/src/lib/form-field/form-field.directive.ts
+++ projects/oblique/src/lib/form-field/form-field.directive.ts
@@ -1,4 +1,5 @@
-import {Directive, ElementRef, HostBinding, OnInit} from '@angular/core';
+import {AfterContentInit, Directive, HostBinding, OnInit, inject} from '@angular/core';
+import {MatFormField} from '@angular/material/form-field';
 
 @Directive({
 	// eslint-disable-next-line @angular-eslint/directive-selector
@@ -6,18 +7,18 @@ import {Directive, ElementRef, HostBinding, OnInit} from '@angular/core';
 	host: {class: 'ob-form-field'},
 	standalone: true
 })
-export class ObFormFieldDirective implements OnInit {
+export class ObFormFieldDirective implements OnInit, AfterContentInit {
 	@HostBinding('class.ob-form-field-textarea') containsTextarea = false;
 	@HostBinding('class.ob-form-field-has-mat-input-server') hasMatInputServer = false;
 	@HostBinding('class.ob-form-field-has-required-marker') hasRequiredMarker = false;
+	@HostBinding('class.ob-form-field-has-text-prefix') hasTextPrefix = false;
+	@HostBinding('class.ob-form-field-has-text-suffix') hasTextSuffix = false;
 	@HostBinding('class.ob-form-field-readonly') isReadonly = false;
 	@HostBinding('class.ob-form-field-required') isRequired = false;
 
-	private readonly host: HTMLAnchorElement;
-
-	constructor(elRef: ElementRef) {
-		this.host = elRef.nativeElement;
-	}
+	private readonly matFormField = inject(MatFormField);
+	// eslint-disable-next-line no-underscore-dangle
+	private readonly host: HTMLElement = this.matFormField._elementRef.nativeElement;
 
 	ngOnInit(): void {
 		this.containsTextarea = this.hostContainsTextArea();
@@ -25,6 +26,11 @@ export class ObFormFieldDirective implements OnInit {
 		this.hasRequiredMarker = this.hasRequiredMarkerClass();
 		this.isReadonly = this.hasReadonlyAttribute();
 		this.isRequired = this.hasRequiredAttribute();
+	}
+
+	ngAfterContentInit(): void {
+		this.hasTextPrefix = this.hasTextPrefixClass();
+		this.hasTextSuffix = this.hasTextSuffixClass();
 	}
 
 	private getHostInnerHTML(): string {
@@ -37,6 +43,16 @@ export class ObFormFieldDirective implements OnInit {
 
 	private hasRequiredMarkerClass(): boolean {
 		return this.hostInnerHTMLIncludes('mat-mdc-form-field-required-marker');
+	}
+
+	private hasTextPrefixClass(): boolean {
+		// eslint-disable-next-line no-underscore-dangle
+		return this.matFormField._hasTextPrefix;
+	}
+
+	private hasTextSuffixClass(): boolean {
+		// eslint-disable-next-line no-underscore-dangle
+		return this.matFormField._hasTextSuffix;
 	}
 
 	private hasReadonlyAttribute(): boolean {
