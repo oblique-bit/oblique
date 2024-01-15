a7fd9cf8403459df83ff787c521e0b0caf20df63
René Giller <rene.giller@bit.admin.ch>
2024-01-15 13:38:50 +0100

    feat(oblique/nested-form): export `ObFormFieldModule` and `ObCheckboxModule`
    OUI-2786

--- projects/oblique/src/lib/nested-form/nested-form.module.ts
+++ projects/oblique/src/lib/nested-form/nested-form.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {ObParentFormDirective} from './parent-form.directive';
 import {ObNestedFormComponent} from './nested-form.component';
-import {obliqueProviders} from '../utilities';
+import {obliqueExports, obliqueProviders} from '../utilities';
 
 export {ObParentFormDirective} from './parent-form.directive';
 export {ObNestedFormComponent} from './nested-form.component';
@@ -9,6 +9,6 @@ export {ObNestedFormComponent} from './nested-form.component';
 @NgModule({
 	imports: [ObNestedFormComponent, ObParentFormDirective],
 	providers: obliqueProviders(),
-	exports: [ObNestedFormComponent, ObParentFormDirective]
+	exports: [ObNestedFormComponent, ObParentFormDirective, ...obliqueExports]
 })
 export class ObNestedFormModule {}
