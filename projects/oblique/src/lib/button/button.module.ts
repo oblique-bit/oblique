94921b882d1d73adfcbcf071edabf25e5c1c657b
René Giller <rene.giller@bit.admin.ch>
2024-01-15 13:33:50 +0100

    feat(oblique/button): export `ObFormFieldModule` and `ObCheckboxModule`
    OUI-2786

--- projects/oblique/src/lib/button/button.module.ts
+++ projects/oblique/src/lib/button/button.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {ObButtonDirective} from './button.directive';
-import {obliqueProviders} from '../utilities';
+import {obliqueExports, obliqueProviders} from '../utilities';
 
 export {ObButtonDirective} from './button.directive';
 
 @NgModule({
 	imports: [ObButtonDirective],
 	providers: obliqueProviders(),
-	exports: [ObButtonDirective]
+	exports: [ObButtonDirective, ...obliqueExports]
 })
 export class ObButtonModule {}
