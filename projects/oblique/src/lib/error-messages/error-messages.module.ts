1d1a3b24b0f5775fafe6f9c5fa3cd6e5c10b3796
René Giller <rene.giller@bit.admin.ch>
2024-01-15 13:35:50 +0100

    feat(oblique/error-messages): export `ObFormFieldModule` and `ObCheckboxModule`
    OUI-2786

--- projects/oblique/src/lib/error-messages/error-messages.module.ts
+++ projects/oblique/src/lib/error-messages/error-messages.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 
 import {ObMatErrorDirective} from './mat-error.directive';
 import {ObErrorMessagesDirective} from './error-messages.directive';
-import {obliqueProviders} from '../utilities';
+import {obliqueExports, obliqueProviders} from '../utilities';
 import {ObTranslateParamsPipe} from '../translate-params/translate-params.pipe';
 
 export {ObErrorMessagesService} from './error-messages.service';
@@ -12,6 +12,6 @@ export {ObErrorMessagesDirective} from './error-messages.directive';
 @NgModule({
 	imports: [ObErrorMessagesDirective, ObMatErrorDirective, ObTranslateParamsPipe],
 	providers: obliqueProviders(),
-	exports: [ObErrorMessagesDirective, ObMatErrorDirective, ObTranslateParamsPipe]
+	exports: [ObErrorMessagesDirective, ObMatErrorDirective, ObTranslateParamsPipe, ...obliqueExports]
 })
 export class ObErrorMessagesModule {}
