9eac2463204e2371ad44084ffba66db69f2994b8
René Giller <rene.giller@bit.admin.ch>
2024-01-15 13:32:05 +0100

    feat(oblique/alert): export `ObFormFieldModule` and `ObCheckboxModule`
    OUI-2786

--- projects/oblique/src/lib/alert/alert.module.ts
+++ projects/oblique/src/lib/alert/alert.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 
-import {obliqueProviders} from '../utilities';
+import {obliqueExports, obliqueProviders} from '../utilities';
 import {ObAlertComponent} from './alert.component';
 
 export {ObAlertComponent, OBLIQUE_HAS_ROLE_ALERT} from './alert.component';
@@ -9,6 +9,6 @@ export {ObIAlertType} from './alert.model';
 @NgModule({
 	imports: [ObAlertComponent],
 	providers: obliqueProviders(),
-	exports: [ObAlertComponent]
+	exports: [ObAlertComponent, ...obliqueExports]
 })
 export class ObAlertModule {}
