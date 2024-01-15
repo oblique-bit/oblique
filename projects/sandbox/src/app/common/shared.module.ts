1debe827f3b2d2b9f8b218aeaac68739ceedae95
René Giller <rene.giller@bit.admin.ch>
2024-01-15 09:39:49 +0100

    feat(sandbox): remove `ApiComponent` & `DescriptionComponent`
    OUI-2799

--- projects/sandbox/src/app/common/shared.module.ts
+++ projects/sandbox/src/app/common/shared.module.ts
@@ -1,12 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {DescriptionComponent} from '../description/description.component';
-import {ApiComponent} from '../api/api.component';
-import {ApiElementComponent} from '../api/api-element.component';
 
 @NgModule({
-	declarations: [ApiComponent, ApiElementComponent, DescriptionComponent],
-	imports: [CommonModule],
-	exports: [ApiComponent, ApiElementComponent, DescriptionComponent]
+	imports: [CommonModule]
 })
 export class SharedModule {}
