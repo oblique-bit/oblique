4a0505890fdda049738768cf64372b0c0a2fc603
René Giller <rene.giller@bit.admin.ch>
2024-01-15 13:40:31 +0100

    feat(oblique/paginator): export `ObFormFieldModule` and `ObCheckboxModule`
    OUI-2786

--- projects/oblique/src/lib/paginator/ob-paginator.module.ts
+++ projects/oblique/src/lib/paginator/ob-paginator.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {MatPaginatorIntl, MatPaginatorModule} from '@angular/material/paginator';
 
 import {ObPaginatorService} from './ob-paginator.service';
-import {obliqueProviders} from '../utilities';
+import {obliqueExports, obliqueProviders} from '../utilities';
 import {ObPaginatorDirective} from './paginator.directive';
 
 export {ObPaginatorDirective} from './paginator.directive';
@@ -10,7 +10,7 @@ export {ObPaginatorService} from './ob-paginator.service';
 
 @NgModule({
 	imports: [MatPaginatorModule, ObPaginatorDirective],
-	exports: [MatPaginatorModule, ObPaginatorDirective],
+	exports: [MatPaginatorModule, ObPaginatorDirective, ...obliqueExports],
 	providers: [ObPaginatorService, {provide: MatPaginatorIntl, useClass: ObPaginatorService}, ...obliqueProviders()]
 })
 export class ObPaginatorModule {}
