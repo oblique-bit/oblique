0d89e2b7bfe425395e5e7026805e3033a3ba587c
Etienne FRANK <etienne.frank@bit.admin.ch>
2024-01-26 15:28:47 +0100

    feat(service-navigation): add environment and basic inputs
    TPEFD-7656

--- projects/service-navigation-web-component/src/app/service-navigation-web-component.component.ts
+++ projects/service-navigation-web-component/src/app/service-navigation-web-component.component.ts
@@ -1,5 +1,7 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation, numberAttribute} from '@angular/core';
+import {NgIf} from '@angular/common';
 import {ObServiceNavigationModule} from '../../../oblique/src/lib/service-navigation/service-navigation.module';
+import {ObEPamsEnvironment} from '../../../oblique/src/lib/service-navigation/service-navigation.model';
 
 @Component({
 	standalone: true,
@@ -7,6 +9,28 @@ import {ObServiceNavigationModule} from '../../../oblique/src/lib/service-naviga
 	templateUrl: './service-navigation-web-component.component.html',
 	styleUrl: '../../../oblique/src/styles/scss/oblique-core.scss',
 	encapsulation: ViewEncapsulation.None,
-	imports: [ObServiceNavigationModule]
+	imports: [ObServiceNavigationModule, NgIf]
 })
-export class ObServiceNavigationWebComponentComponent {}
+export class ObServiceNavigationWebComponentComponent implements OnInit {
+	@Input() environment: 'DEV' | 'REF' | 'TEST' | 'ABN' | 'PROD';
+	@Input({transform: numberAttribute}) maxLastUsedApplications: number;
+	@Input({transform: numberAttribute}) maxFavoriteApplications: number;
+	@Input() rootUrl: string;
+	@Input() returnUrl: string;
+
+	environmentParsed: ObEPamsEnvironment;
+
+	ngOnInit(): void {
+		this.environmentParsed = this.parseEnvironment(this.environment);
+	}
+
+	private parseEnvironment(environmentName: string): ObEPamsEnvironment {
+		const environmentNames = Object.keys(ObEPamsEnvironment);
+		if (!environmentNames.includes(environmentName)) {
+			throw new Error(
+				`An environment has to be provided, valid values are: ${environmentNames.join(', ')}, but "${environmentName}" has been provided`
+			);
+		}
+		return ObEPamsEnvironment[environmentName];
+	}
+}
