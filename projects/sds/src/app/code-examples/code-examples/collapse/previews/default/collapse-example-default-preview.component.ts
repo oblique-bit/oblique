4682f43c89d2d0de1523cbde81a0ec6fa00fc38d
Nicole Widmer <nicole.widmer@bit.admin.ch>
2024-01-30 13:27:44 +0100

    feat(sds/code-examples): add collapse examples
    OUI-2742

---
+++ projects/sds/src/app/code-examples/code-examples/collapse/previews/default/collapse-example-default-preview.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {ObCollapseModule} from '@oblique/oblique';
+
+@Component({
+	selector: 'app-collapse-example-default-preview',
+	templateUrl: './collapse-example-default-preview.component.html',
+	imports: [ObCollapseModule],
+	standalone: true
+})
+export class CollapseExampleDefaultPreviewComponent {}
---
+++ projects/sds/src/app/code-examples/code-examples/collapse/previews/icon-position/collapse-example-icon-position-preview.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {ObCollapseModule} from '@oblique/oblique';
+
+@Component({
+	selector: 'app-collapse-example-icon-position-preview',
+	templateUrl: './collapse-example-icon-position-preview.component.html',
+	imports: [ObCollapseModule],
+	standalone: true
+})
+export class CollapseExampleIconPositionPreviewComponent {}
