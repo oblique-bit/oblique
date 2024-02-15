b2ddff4e61ffc04db8b4a94718bfd7373f2c1445
René Giller <rene.giller@bit.admin.ch>
2024-02-15 19:18:07 +0100

    feat(sds/code-examples): remove `layout` example
    OUI-2887

--- projects/sds/src/app/code-examples/code-examples/spacing-and-layout/previews/layout/spacing-and-layout-example-layout-preview.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-	selector: 'app-spacing-and-layout-example-layout-preview',
-	templateUrl: './spacing-and-layout-example-layout-preview.component.html',
-	styleUrls: ['../../../../code-example-flex-layout.scss', './spacing-and-layout-example-layout-preview.component.scss'],
-	standalone: true
-})
-export class SpacingAndLayoutExampleLayoutPreviewComponent {}
---
+++ projects/sds/src/app/code-examples/code-examples/spacing/previews/spacing/spacing-example-spacing-preview.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app-spacing-example-spacing-preview',
+	templateUrl: './spacing-example-spacing-preview.component.html',
+	styleUrls: ['../../../../code-example-flex-layout.scss', './spacing-example-spacing-preview.component.scss'],
+	standalone: true
+})
+export class SpacingExampleSpacingPreviewComponent {}
