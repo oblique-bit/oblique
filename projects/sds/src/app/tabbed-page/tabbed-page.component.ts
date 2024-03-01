fadf2aebc7d83380c542fa9b4556f0513826beaf
René Giller <rene.giller@bit.admin.ch>
2024-03-01 14:40:17 +0100

    refactor(sds/tabbed-page): avoid subscription in the component
    OUI-2504

--- projects/sds/src/app/tabbed-page/tabbed-page.component.ts
+++ projects/sds/src/app/tabbed-page/tabbed-page.component.ts
@@ -1,23 +1,10 @@
-import {Component, OnDestroy, OnInit, Type, ViewChild, inject} from '@angular/core';
+import {Component, OnInit, Type, ViewChild, inject} from '@angular/core';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {CmsDataService} from '../cms/cms-data.service';
 import {CodeExampleDirective} from '../code-examples/code-example.directive';
 import {CodeExamplesMapper} from '../code-examples/code-examples.mapper';
 import {CodeExamples} from '../code-examples/code-examples.model';
-import {
-	Observable,
-	ReplaySubject,
-	Subject,
-	debounceTime,
-	distinctUntilChanged,
-	filter,
-	map,
-	mergeWith,
-	share,
-	switchMap,
-	takeUntil,
-	tap
-} from 'rxjs';
+import {Observable, ReplaySubject, distinctUntilChanged, filter, map, mergeWith, share, switchMap, tap} from 'rxjs';
 import {SlugToIdService} from '../shared/slug-to-id/slug-to-id.service';
 import {URL_CONST} from '../shared/url/url.const';
 import {IdPipe} from '../shared/id/id.pipe';
@@ -35,14 +22,11 @@ import {TabNameMapper} from './utils/tab-name-mapper';
 	standalone: true,
 	imports: [TabsComponent, TabComponent, CodeExampleDirective, CommonModule, IdPipe, SafeHtmlPipe]
 })
-export class TabbedPageComponent implements OnInit, OnDestroy {
+export class TabbedPageComponent implements OnInit {
 	@ViewChild(CodeExampleDirective, {static: false}) codeExample!: CodeExampleDirective;
 	@ViewChild('tabs') tabs: TabsComponent;
 	readonly componentId = 'tabbed-page';
 	public cmsData$: Observable<CmsData>;
-
-	private readonly unsubscribe = new Subject<void>();
-
 	private readonly activatedRoute = inject(ActivatedRoute);
 	private readonly cmsDataService = inject(CmsDataService);
 	private readonly router = inject(Router);
@@ -51,12 +35,6 @@ export class TabbedPageComponent implements OnInit, OnDestroy {
 
 	ngOnInit(): void {
 		this.monitorForSlugToIdChanges();
-		this.monitorForPageChanges();
-	}
-
-	ngOnDestroy(): void {
-		this.unsubscribe.next();
-		this.unsubscribe.complete();
 	}
 
 	handleTabChanged(tabName: string): void {
@@ -67,15 +45,6 @@ export class TabbedPageComponent implements OnInit, OnDestroy {
 			: `${this.router.url}/${urlParamForTab}`;
 
 		this.location.replaceState(newUrl);
-	}
-
-	private monitorForPageChanges(): void {
-		this.cmsData$.pipe(takeUntil(this.unsubscribe), debounceTime(1)).subscribe(cmsData => {
-			if (cmsData.api || cmsData.source || cmsData.uiUx) {
-				const tabToSelect: string = this.activatedRoute.snapshot.paramMap.get(URL_CONST.urlParams.selectedTab) ?? '';
-				this.tabs.selectTabWithName(TabNameMapper.getTabNameFromUrlParam(tabToSelect));
-			}
-		});
 	}
 
 	private loadCodeExample(codeExampleComponent: Type<CodeExamples> | undefined): void {
@@ -96,6 +65,7 @@ export class TabbedPageComponent implements OnInit, OnDestroy {
 			switchMap(id => this.cmsDataService.getTabbedPageComplete(id)),
 			map(cmsData => this.buildCmsData(cmsData.data)),
 			tap(cmsData => this.loadCodeExample(cmsData.source)),
+			tap(cmsData => this.activateTab(cmsData)),
 			share({connector: () => new ReplaySubject(1)})
 		);
 	}
@@ -107,5 +77,13 @@ export class TabbedPageComponent implements OnInit, OnDestroy {
 			uiUx: cmsData.ui_ux,
 			source: CodeExamplesMapper.getCodeExampleComponent(cmsData.slug)
 		};
+	}
+
+	private activateTab(cmsData: CmsData): void {
+		if (cmsData.api || cmsData.source || cmsData.uiUx) {
+			const tabToSelect: string = this.activatedRoute.snapshot.paramMap.get(URL_CONST.urlParams.selectedTab) ?? '';
+			// setTimeout delays the tab selection until the view is initialized and the tabs are available
+			setTimeout(() => this.tabs.selectTabWithName(TabNameMapper.getTabNameFromUrlParam(tabToSelect)));
+		}
 	}
 }
