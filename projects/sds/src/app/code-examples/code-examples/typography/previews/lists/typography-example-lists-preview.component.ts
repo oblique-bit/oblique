e48a2c0e1892e7352b14c910756c683021d1aba0
Nina Egger <nina.egger@bit.admin.ch>
2024-03-19 08:45:56 +0100

    feat(sds/code-examples): add list examples
    OUI-2761

---
+++ projects/sds/src/app/code-examples/code-examples/typography/previews/lists/typography-example-lists-preview.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app-typography-example-lists-preview',
+	templateUrl: './typography-example-lists-preview.component.html',
+	styleUrls: ['./typography-example-lists-preview.component.scss'],
+	standalone: true
+})
+export class TypographyExampleListsPreviewComponent {}
