2d81c61a8178dd3174514b14c4bdda075e520404
René Giller <rene.giller@bit.admin.ch>
2024-04-04 15:33:58 +0200

    chore(oblique/toolchain): use the branch name to define the version number to release
    Before, the version was computed from the git history
    
    OUI-2419

--- projects/oblique/scripts/release/release.ts
+++ projects/oblique/scripts/release/release.ts
@@ -2,53 +2,22 @@ import {execSync} from 'child_process';
 import {writeFileSync} from 'fs';
 import path from 'path';
 import {Changelog} from './changelog';
-import {version as packageVersion} from '../../../../package.json';
-
-interface Version {
-	version: string;
-	preVersionType: string;
-	preVersionNumber: number;
-}
 
 class Release {
-	static perform(preVersion?: string): void {
-		const nextVersion = Release.computeVersion(Release.splitVersion(packageVersion), preVersion);
+	static perform(): void {
+		const nextVersion = Release.getVersionFromBranchName();
 		Release.bumpVersion(nextVersion);
 		Changelog.perform();
 	}
 
-	private static splitVersion(version): Version {
-		const {groups} = version.match(/(?<version>\d+\.\d+\.\d+)(?:-(?<type>[^.]+)\.(?<typeNbr>\d+))?/);
-		return {
-			version: groups?.version,
-			preVersionType: groups?.type,
-			preVersionNumber: +groups?.typeNbr
-		};
-	}
-
-	private static computeVersion(version: Version, preVersion: string): string {
-		if (!version.preVersionType) {
-			const newVersion = Release.getVersionFromGit(version.version);
-			return preVersion ? `${newVersion}-${preVersion}.1` : newVersion;
+	private static getVersionFromBranchName(): string {
+		const branchName = execSync('git rev-parse --abbrev-ref HEAD').toString();
+		const regexp = /\d+\.\d+\.\d+(?:-(?:alpha|beta|RC)\.\d+)?/;
+		if (!regexp.test(branchName)) {
+			console.error('The branch MUST contain the version number to release');
+			throw new Error('Version number not found');
 		}
-		if (!preVersion) {
-			return version.version;
-		}
-		if (version.preVersionType !== preVersion) {
-			return `${version.version}-${preVersion}.1`;
-		}
-		return `${version.version}-${version.preVersionType}.${version.preVersionNumber + 1}`;
-	}
-
-	private static getVersionFromGit(versionNbr: string): string {
-		const current = /(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)/.exec(versionNbr).groups;
-		const commits = execSync(`git log ${versionNbr}..HEAD --abbrev-commit`).toString();
-		if (commits.includes('BREAKING CHANGE')) {
-			return `${+current?.major + 1}.0.0`;
-		}
-		return commits.includes('feat:') || commits.includes('feat(')
-			? `${current?.major}.${+current?.minor + 1}.0`
-			: `${current?.major}.${current?.minor}.${+current?.patch + 1}`;
+		return regexp.exec(branchName).toString();
 	}
 
 	private static bumpVersion(version: string): void {
@@ -58,4 +27,4 @@ class Release {
 	}
 }
 
-Release.perform(process.argv[2]);
+Release.perform();
