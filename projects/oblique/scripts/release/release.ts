c8a9d1d039ab129ff3b2f4c75a167acf86e4fddb
René Giller <rene.giller@bit.admin.ch>
2024-04-05 15:26:13 +0200

    chore(oblique/toolchain): make sure the release script includes both issue and version numbers
    OUI-2419

--- projects/oblique/scripts/release/release.ts
+++ projects/oblique/scripts/release/release.ts
@@ -1,31 +1,34 @@
 import {execSync} from 'child_process';
 import {writeFileSync} from 'fs';
 import path from 'path';
-import {Commit} from '../../../../scripts/commit';
 import {Changelog} from './changelog';
 
 class Release {
 	static perform(): void {
-		const nextVersion = Release.getVersionFromBranchName();
-		Release.bumpVersion(nextVersion);
+		const {version, issue} = Release.parseBranchName();
+		Release.bumpVersion(version);
 		Changelog.perform();
-		Commit.perform(`chore(toolchain): release version ${process.env.npm_package_version}`);
+		Release.commit(version, issue);
 	}
 
-	private static getVersionFromBranchName(): string {
+	private static parseBranchName(): {version: string; issue: string} {
 		const branchName = execSync('git rev-parse --abbrev-ref HEAD').toString();
-		const regexp = /\d+\.\d+\.\d+(?:-(?:alpha|beta|RC)\.\d+)?/;
+		const regexp = /(?<issue>OUI-\d+).*?(?<version>\d+\.\d+\.\d+(?:-(?:alpha|beta|RC)\.\d+)?)/;
 		if (!regexp.test(branchName)) {
-			console.error('The branch MUST contain the version number to release');
-			throw new Error('Version number not found');
+			console.error('The branch MUST contain the version number to release and the Jira issue number');
+			throw new Error('Version or Issue number not found');
 		}
-		return regexp.exec(branchName).toString();
+		return regexp.exec(branchName).groups as {version: string; issue: string};
 	}
 
 	private static bumpVersion(version: string): void {
 		process.chdir('../..'); // so that the release is made with the info of the root package.json
 		execSync(`npm version ${version}`);
 		writeFileSync(path.join('projects', 'oblique', 'src', 'lib', 'version.ts'), `export const appVersion = '${version}';\n`, {flag: 'w'});
+	}
+
+	private static commit(version: string, issue: string): void {
+		execSync(`git commit -am "chore(toolchain): release version ${version}" -m "${issue}"`);
 	}
 }
 
