2e5d9cfa5354ea6ac3c651c516e85c1d3682a7f1
René Giller <rene.giller@bit.admin.ch>
2024-04-05 17:44:42 +0200

    chore(toolchain): ensure `update-dependencies.ts` don't update the `peerDependencies`
    OUI-2419

--- scripts/update-dependencies.ts
+++ scripts/update-dependencies.ts
@@ -1,12 +1,32 @@
 import {execSync} from 'child_process';
+import {readFileSync, writeFileSync} from 'fs';
+import path from 'path';
 
 class UpdateDependencies {
+	private static readonly packageJsonPath = path.join('projects', 'oblique', 'package.json');
 	static perform(): void {
+		const peerDependencies = UpdateDependencies.savePeerDependencies();
 		UpdateDependencies.execute('npm update --save --audit false');
 		UpdateDependencies.execute('npm audit fix --audit-level=none');
 		UpdateDependencies.execute('npm dedupe --audit false');
 		UpdateDependencies.execute('npm prune --audit false');
+		UpdateDependencies.restorePeerDependencies(peerDependencies);
 		UpdateDependencies.commit('chore(toolchain): update dependencies and refactor accordingly');
+	}
+
+	private static savePeerDependencies(): Record<string, string> {
+		return UpdateDependencies.readPackageJson().peerDependencies;
+	}
+
+	private static restorePeerDependencies(peerDependencies: Record<string, string>): void {
+		writeFileSync(
+			UpdateDependencies.packageJsonPath,
+			JSON.stringify({...UpdateDependencies.readPackageJson(), peerDependencies}, null, '  ')
+		);
+	}
+
+	private static readPackageJson(): {peerDependencies: Record<string, string>} {
+		return JSON.parse(readFileSync(UpdateDependencies.packageJsonPath).toString());
 	}
 
 	private static execute(command: string): void {
