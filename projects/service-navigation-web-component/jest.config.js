a0d58008493eeb8a73d726db95c9daa69b217496
René Giller <rene.giller@bit.admin.ch>
2024-04-30 00:03:11 +0200

    refactor(service-navigation): create a "default" test configuration
    The "default" configuration replaces the previous unnamed one. It matches the structure of the
    Oblique test configuration and is called through the `test` script. It also makes sure the coverage
    is collected into a dedicated repository.
    
    OUI-3062

--- projects/service-navigation-web-component/jest.config.js
+++ projects/service-navigation-web-component/jest.config.js
@@ -2,17 +2,13 @@
 
 module.exports = require('../../tests/jest.config');
 module.exports.roots = ['<rootDir>/projects/service-navigation-web-component'];
-module.exports.moduleNameMapper = {
-	'@oblique/oblique': '<rootDir>/projects/oblique/src/public_api.ts',
-	'@oblique/version': '<rootDir>/projects/oblique/src/lib/version.ts'
-};
 module.exports.displayName = {
 	name: 'Service Navigation Web Component',
 	color: 'magenta'
 };
-module.exports.coveragePathIgnorePatterns = [
-	'<rootDir>/node_modules/',
-	'<rootDir>/projects/oblique',
-	'<rootDir>/projects/stylesBuilder',
-	'<rootDir>/tests/'
-];
+module.exports.moduleNameMapper = {
+	'@oblique/oblique': '<rootDir>/projects/oblique/src/public_api.ts',
+	'@oblique/version': '<rootDir>/projects/oblique/src/lib/version.ts'
+};
+module.exports.coverageDirectory = '<rootDir>/coverage/service-navigation';
+module.exports.coveragePathIgnorePatterns = ['<rootDir>/projects/oblique'];
