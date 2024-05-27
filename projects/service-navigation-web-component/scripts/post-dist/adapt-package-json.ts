d6ca432f1453ef0d520cda6903fd0c3feac55ad3
René Giller <rene.giller@bit.admin.ch>
2024-05-27 14:42:39 +0200

    feat(service-navigation): remove `@angular/elements` from the distributed `package.json`
    OUI-3113

--- projects/service-navigation-web-component/scripts/post-dist/adapt-package-json.ts
+++ projects/service-navigation-web-component/scripts/post-dist/adapt-package-json.ts
@@ -8,10 +8,22 @@ export class AdaptPackageJson {
 		const filePath = path.join(AdaptPackageJson.DIST_PATH, 'package.json');
 		const fields = ['version', 'author', 'contributors', 'license'];
 		const distPackage = PackageJson.getData(filePath);
+		this.removeDependencies(distPackage, 'devDependencies', '@angular/elements');
 		const rootPackage = PackageJson.filterIn(path.join('..', '..', 'package.json'), fields);
 		PackageJson.write(filePath, {
 			...distPackage,
 			...rootPackage
 		});
+	}
+
+	private static removeDependencies(
+		packageJson: object,
+		dependencyType: 'devDependencies' | 'dependencies',
+		...dependencyNames: string[]
+	): void {
+		dependencyNames.forEach(dependencyName => delete packageJson[dependencyType][dependencyName]);
+		if (!Object.keys(packageJson[dependencyType]).length) {
+			delete packageJson[dependencyType];
+		}
 	}
 }
