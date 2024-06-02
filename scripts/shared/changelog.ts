def7082680e9d908dfb8bcdab5b18c7da7ce92e4
René Giller <rene.giller@bit.admin.ch>
2024-06-03 00:45:27 +0200

    fix(toolchain): skip empty version in CHANGELOG generation
    OUI-3144

--- scripts/shared/changelog.ts
+++ scripts/shared/changelog.ts
@@ -82,16 +82,18 @@ export class Changelog {
 	}
 
 	private static writeChangelog(commits: Commits, previousVersion: string, nextVersion: string): void {
-		writeFileSync(
-			'../../CHANGELOG.md',
-			[
-				Changelog.getTitle(nextVersion, previousVersion),
-				Changelog.getSection(commits.fix, 'Bug Fixes'),
-				Changelog.getSection(commits.feat, 'Features'),
-				Changelog.getSection(commits.breakingChanges, 'BREAKING CHANGES'),
-				readFileSync('../../CHANGELOG.md').toString()
-			].join('\n\n')
-		);
+		if (commits.feat.length || commits.fix.length) {
+			writeFileSync(
+				'../../CHANGELOG.md',
+				[
+					Changelog.getTitle(nextVersion, previousVersion),
+					Changelog.getSection(commits.fix, 'Bug Fixes'),
+					Changelog.getSection(commits.feat, 'Features'),
+					Changelog.getSection(commits.breakingChanges, 'BREAKING CHANGES'),
+					readFileSync('../../CHANGELOG.md').toString()
+				].join('\n\n')
+			);
+		}
 	}
 
 	private static getTitle(nextVersion: string, previousVersion: string): string {
