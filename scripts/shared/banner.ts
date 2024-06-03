6778369ec43d44447da1d7b04f652176a68809a9
René Giller <rene.giller@bit.admin.ch>
2024-06-03 22:08:42 +0200

    refactor(toolchain): use global script to add the banner and refactor accordingly
    OUI-3098

---
+++ scripts/shared/banner.ts
@@ -0,0 +1,54 @@
+import {readFileSync, writeFileSync} from 'fs';
+import path from 'path';
+import {EOL} from 'os';
+import {version as currentVersion} from './../../package.json';
+import {executeCommand, listFiles} from './utils';
+
+export class Banner {
+	// manually set for versions with prolonged support
+	private static readonly eolDates = {
+		'10.0.0': '2024-06-30' // eslint-disable-line @typescript-eslint/naming-convention
+	};
+
+	constructor() {
+		throw new Error('"Banner" may not be instantiated.');
+	}
+
+	static addToFilesInProject(projectName: string): void {
+		const banner = Banner.prepareBanner(currentVersion);
+
+		listFiles(path.join('..', '..', 'dist', projectName))
+			.filter(filePath => /\.(?:m?js|css)$/.test(filePath))
+			.map(filePath => ({filePath, content: readFileSync(filePath).toString()}))
+			.forEach(({filePath, content}) => writeFileSync(filePath, `${banner}${EOL}${content}`));
+	}
+
+	private static prepareBanner(version: string): string {
+		const releaseDate = Banner.getTodayDate();
+		const majorVersion = `${version.split('.')[0]}.0.0`;
+		const endOfLifeDate = (Banner.eolDates[majorVersion] as string) ?? Banner.getEndOfLifeDate(majorVersion);
+		return `/**
+* @file Oblique, The front-end framework for your Swiss branded UI.
+* @copyright 2020 - ${new Date().getFullYear()} Federal Office of Information Technology, Systems and Telecommunication FOITT {@link https://www.bit.admin.ch}
+* @version ${version} (released on ${releaseDate}, supported at least until ${endOfLifeDate})
+* @author Oblique team, FOITT, BS-BSC-EN4 <oblique@bit.admin.ch>
+* @license MIT {@link https://github.com/oblique-bit/oblique/blob/master/LICENSE}
+* @see https://oblique.bit.admin.ch
+*/
+`;
+	}
+
+	private static getTodayDate(): string {
+		return new Date().toISOString().split('T')[0];
+	}
+
+	private static getEndOfLifeDate(version: string): string {
+		const endOfLifeDate = new Date(Banner.getTagDate(version));
+		endOfLifeDate.setFullYear(endOfLifeDate.getFullYear() + 1, endOfLifeDate.getMonth() + 1, 0);
+		return endOfLifeDate.toISOString().split('T')[0];
+	}
+
+	private static getTagDate(tag: string): string {
+		return executeCommand(`git tag -l '${tag}'`) ? executeCommand(`git show -s --format=%ci ${tag}`).split(' ')[0] : Banner.getTodayDate();
+	}
+}
