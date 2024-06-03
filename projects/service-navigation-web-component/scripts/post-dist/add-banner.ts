7c8324e202a137adb33db5ef7c80eec34abc2199
René Giller <rene.giller@bit.admin.ch>
2024-06-04 00:14:14 +0200

    refactor(service-navigation): simplify post-dist scripts
    OUI-3098

--- projects/service-navigation-web-component/scripts/post-dist/add-banner.ts
+++
@@ -1,7 +0,0 @@
-import {Banner} from '../../../../scripts/shared/banner';
-
-export class AddBanner {
-	static perform(): void {
-		Banner.addToFilesInProject('service-navigation-web-component');
-	}
-}
--- projects/service-navigation-web-component/scripts/post-dist/index.ts
+++
@@ -1,7 +0,0 @@
-import {AdaptPackageJson} from './adapt-package-json';
-import {Pack} from './pack';
-import {AddBanner} from './add-banner';
-
-AdaptPackageJson.perform();
-Pack.perform();
-AddBanner.perform();
