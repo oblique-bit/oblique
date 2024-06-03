0e27b16852ad506486ed6668689192e88427bef6
René Giller <rene.giller@bit.admin.ch>
2024-06-03 23:33:31 +0200

    refactor(oblique/toolchain): simplify post dist scripts
    OUI-3098

--- projects/oblique/scripts/post-dist/steps/distribute-ob-features.ts
+++
@@ -1,7 +0,0 @@
-import {executeCommand} from '../../../../../scripts/shared/utils';
-
-export class DistributeObFeatures {
-	static perform(): void {
-		executeCommand(`uglifyjs --compress --mangle --output ../../dist/oblique/ob-features.js -- src/ob-features.js`);
-	}
-}
