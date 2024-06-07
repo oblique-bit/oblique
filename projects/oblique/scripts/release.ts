b78d410d27885c8a8538e2a493a7d1717929ee8d
René Giller <rene.giller@bit.admin.ch>
2024-06-07 12:29:03 +0200

    feat(oblique/toolchain): update `package.json` version on release
    This allows the workspace name to be printed when executing a command
    
    OUI-3144

--- projects/oblique/scripts/release.ts
+++ projects/oblique/scripts/release.ts
@@ -2,11 +2,13 @@ import {writeFileSync} from 'fs';
 import path from 'path';
 import {Changelog} from '../../../scripts/shared/changelog';
 import {version} from '../../../package.json';
+import {updatePackageJsonVersion} from '../../../scripts/shared/utils';
 
 class Release {
 	static perform(): void {
 		Changelog.update(version, 'oblique');
 		writeFileSync(path.join('src', 'lib', 'version.ts'), `export const appVersion = '${version}';\n`);
+		updatePackageJsonVersion(version);
 	}
 }
 
