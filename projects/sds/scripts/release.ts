d636054986257a1b32d0b85d8c125ac17fee464e
René Giller <rene.giller@bit.admin.ch>
2024-06-13 00:38:08 +0200

    feat(toolchain): make sure the latest version is provided to Sonar
    OUI-3147

--- projects/sds/scripts/release.ts
+++ projects/sds/scripts/release.ts
@@ -1,8 +1,9 @@
 import {Changelog} from '../../../scripts/shared/changelog';
-import {updatePackageJsonVersion} from '../../../scripts/shared/utils';
+import {updatePackageJsonVersion, updateSonarPropertiesVersion} from '../../../scripts/shared/utils';
 import {version} from '../../../package.json';
 import {ObliqueVersion} from './oblique-version';
 
 ObliqueVersion.updateObliqueVersion();
 Changelog.addRelease(version, 'sds');
 updatePackageJsonVersion(version);
+updateSonarPropertiesVersion(version);
--- projects/service-navigation-web-component/scripts/release.ts
+++ projects/service-navigation-web-component/scripts/release.ts
@@ -1,6 +1,7 @@
 import {Changelog} from '../../../scripts/shared/changelog';
-import {updatePackageJsonVersion} from '../../../scripts/shared/utils';
+import {updatePackageJsonVersion, updateSonarPropertiesVersion} from '../../../scripts/shared/utils';
 import {version} from '../../../package.json';
 
 Changelog.addRelease(version, 'service-navigation');
 updatePackageJsonVersion(version);
+updateSonarPropertiesVersion(version);
