2e8df2183b336c9351e28499ed3224cc7b08934a
René Giller <rene.giller@bit.admin.ch>
2024-06-29 16:20:21 +0200

    feat(toolchain): let `troubleshooting` script deploy SDS
    OUI-3167

--- scripts/troubleshoot.ts
+++ scripts/troubleshoot.ts
@@ -1,13 +1,31 @@
 import {executeCommand} from './shared/utils';
+import {readFileSync, writeFileSync} from 'fs';
 
 class Troubleshoot {
 	static perform(): void {
 		Troubleshoot.createTroubleshootBranch();
+		Troubleshoot.adaptJenkinsFile('Jenkinsfile');
+		executeCommand('git commit -am "chore(toolchain): troubleshoot release" --no-verify');
 	}
 
 	private static createTroubleshootBranch(): void {
 		executeCommand('git fetch --all');
 		executeCommand('git checkout -b troubleshoot --no-track origin/master');
+	}
+
+	private static adaptJenkinsFile(jenkinsFilePath: string): void {
+		writeFileSync(
+			jenkinsFilePath,
+			readFileSync(jenkinsFilePath)
+				.toString()
+				.replace(
+					/(?<=branches = \[\s\t\t).*(?=\s\t])/ms,
+					`troubleshoot: [
+			build: 'npm run build -w projects/sds',
+			cloudFoundry: [[project: 'sds', space: 'prod']]
+		]`
+				)
+		);
 	}
 }
 
