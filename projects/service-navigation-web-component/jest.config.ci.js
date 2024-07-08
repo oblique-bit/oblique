eb860435f7287e8d33900e4d9ddb65d82b857b70
René Giller <rene.giller@bit.admin.ch>
2024-07-09 01:58:51 +0200

    test(service-navigation/toolchain): use full Sonar configuration for `test-ci`
    Don't rely on the root configuration anymore
    
    OUI-3201

--- projects/service-navigation-web-component/jest.config.ci.js
+++ projects/service-navigation-web-component/jest.config.ci.js
@@ -1,4 +1,13 @@
 'use strict';
 
 module.exports = require('./jest.config');
-module.exports.reporters[1][1].outputDirectory = '<rootDir>/coverage/service-navigation-web-component';
+module.exports.reporters = [
+	'default',
+	[
+		'jest-sonar',
+		{
+			outputDirectory: '<rootDir>/coverage/service-navigation-web-component',
+			outputName: 'sqr.xml'
+		}
+	]
+];
