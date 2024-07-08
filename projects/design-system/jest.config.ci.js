88ab4e1b11fba3936ed5c75360a34c0bea1e1c67
René Giller <rene.giller@bit.admin.ch>
2024-07-09 01:57:48 +0200

    test(design-system/toolchain): use full Sonar configuration for `test-ci`
    Don't rely on the root configuration anymore
    
    OUI-3201

--- projects/design-system/jest.config.ci.js
+++ projects/design-system/jest.config.ci.js
@@ -1,4 +1,13 @@
 'use strict';
 
 module.exports = require('./jest.config');
-module.exports.reporters[1][1].outputDirectory = '<rootDir>/coverage/design-system';
+module.exports.reporters = [
+	'default',
+	[
+		'jest-sonar',
+		{
+			outputDirectory: '<rootDir>/coverage/design-system',
+			outputName: 'sqr.xml'
+		}
+	]
+];
