03cbd1f3ef2f9f528d9567258ef229f2ea36ec24
René Giller <rene.giller@bit.admin.ch>
2024-07-09 02:00:02 +0200

    test(toolchain): remove Sonar configuration from root configuration
    This allows the Sonar report to be only created on the CI pipeline
    
    OUI-3201

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -5,15 +5,5 @@ globalThis.ngJest = {
 module.exports = {
 	preset: 'jest-preset-angular',
 	setupFilesAfterEnv: ['<rootDir>/tests/setupJest.ts'],
-	collectCoverage: true,
-	reporters: [
-		'default',
-		[
-			'jest-sonar',
-			{
-				outputDirectory: '<rootDir>/coverage/oblique',
-				outputName: 'sqr.xml'
-			}
-		]
-	]
+	collectCoverage: true
 };
