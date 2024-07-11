dc241919cd2e49b8a61a15a2625220157ed7405f
René Giller <rene.giller@bit.admin.ch>
2024-07-11 18:40:44 +0200

    feat(sds/toolchain): add logging to scripts
    OUI-3209

--- projects/sds/scripts/post-dist.ts
+++ projects/sds/scripts/post-dist.ts
@@ -1,9 +1,11 @@
 import path from 'path';
 import {CopyFiles} from '../../../scripts/shared/copy-files';
 import {StaticScript} from '../../../scripts/shared/static-script';
+import {Log} from '../../../scripts/shared/log';
 
 class PostDist extends StaticScript {
 	static perform(): void {
+		Log.start('Finalize build');
 		const source = path.join('src', 'nginx');
 		const destination = path.join('nginx', 'conf', 'includes');
 		CopyFiles.initialize('sds')
@@ -11,6 +13,7 @@ class PostDist extends StaticScript {
 			.copyFile('custom_headers.conf', source, destination)
 			.copyFile('security_headers.conf', source, destination)
 			.finalize();
+		Log.success();
 	}
 }
 
