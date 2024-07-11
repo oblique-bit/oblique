bf296fddd2e024743e217621b63665635bb348a6
René Giller <rene.giller@bit.admin.ch>
2024-07-11 18:34:53 +0200

    feat(cli/toolchain): add logging to scripts
    OUI-3209

--- projects/cli/scripts/lint.ts
+++ projects/cli/scripts/lint.ts
@@ -1,4 +1,7 @@
 import {hasFlag} from '../../../scripts/shared/utils';
 import {Lint} from '../../../scripts/shared/lint';
+import {Log} from '../../../scripts/shared/log';
 
+Log.start('Lint CLI project');
 Lint.initialize(hasFlag('fix')).esLint('**/*.{ts,js}', '.eslintrc.local.yml').prettier('**/*.{ts,js,json,yml,md}').finalize();
+Log.success();
