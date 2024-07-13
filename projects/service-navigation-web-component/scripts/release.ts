244e4c032a214be0d5d44e879f014033c796ce69
René Giller <rene.giller@bit.admin.ch>
2024-07-13 13:54:04 +0200

    feat(service-navigation/toolchain): add `oblique/service-navigation` commits to the changelog
    OUI-3182

--- projects/service-navigation-web-component/scripts/release.ts
+++ projects/service-navigation-web-component/scripts/release.ts
@@ -2,6 +2,6 @@ import {Changelog} from '../../../scripts/shared/changelog';
 import {updatePackageJsonVersion, updateSonarPropertiesVersion} from '../../../scripts/shared/utils';
 import {version} from '../../../package.json';
 
-Changelog.addRelease(version, 'service-navigation');
+Changelog.addRelease(version, 'service-navigation', 'oblique/service-navigation');
 updatePackageJsonVersion(version);
 updateSonarPropertiesVersion(version);
