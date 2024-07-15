d6be82885c6212a2f51b3b9c036cb7a8004dcf21
Nicole Widmer <nicole.widmer@bit.admin.ch>
2024-07-15 10:45:59 +0200

    feat(cli/toolchain): add help and version command
    OUI-3203

--- projects/cli/src/index.ts
+++ projects/cli/src/index.ts
@@ -1,1 +1,18 @@
-console.info('Hello CLI');
+import * as packageFile from '../package.json';
+import {Command} from 'commander';
+
+new Command()
+	.name('ob')
+	.description('Oblique CLI for managing projects')
+	.version(packageFile.version)
+	.usage(
+		'new <project-name>\t\t\tCreates a new project in current place\n' +
+			'or \n' +
+			'ob update <project-name>\t\t\tUpdates the oblique package in the project and runs migration\n'
+	)
+	.action(() => {
+		console.info('Starts Oblique CLI');
+	})
+	.showSuggestionAfterError(true)
+	.showHelpAfterError(true)
+	.parse();
