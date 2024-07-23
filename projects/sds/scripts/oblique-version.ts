71d6b836a31c6710b0ca5b0ca36cf371d1e4fa86
René Giller <rene.giller@bit.admin.ch>
2024-07-23 23:35:36 +0200

    refactor(sds/toolchain): use `Files` class for all file system related operations
    OUI-3226

--- projects/sds/scripts/oblique-version.ts
+++ projects/sds/scripts/oblique-version.ts
@@ -1,17 +1,16 @@
-import {readFileSync, writeFileSync} from 'fs';
-import path from 'path';
 import {version as currentVersion} from '../../../package.json';
 import {StaticScript} from '../../../scripts/shared/static-script';
+import {Files} from '../../../scripts/shared/files';
 
 export class ObliqueVersion extends StaticScript {
 	static updateObliqueVersion(): void {
 		const latest = ObliqueVersion.getLatestNonPreReleaseVersion();
-		writeFileSync(path.join('src', 'obliqueVersion.ts'), `export const latest = '${latest}';\n`, {flag: 'w'});
+		Files.write('src/obliqueVersion.ts', `export const latest = '${latest}';\n`);
 	}
 
 	private static getLatestNonPreReleaseVersion(): string {
 		return /^\d+\.\d+.\d+$/.test(currentVersion)
 			? currentVersion
-			: /# \[(?<latest>\d+\.\d+.\d+)]/.exec(readFileSync('../oblique/CHANGELOG.md').toString()).groups.latest;
+			: (/# \[(?<latest>\d+\.\d+.\d+)]/.exec(Files.read('../oblique/CHANGELOG.md'))?.groups?.latest ?? '');
 	}
 }
