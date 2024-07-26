5aa3d094ab1ecb784c3c74cec328bded19fad299
Nicole Widmer <nicole.widmer@bit.admin.ch>
2024-07-26 22:27:01 +0200

    feat(cli/toolchain): make `ob` command runnable globally
    OUI-3242

--- projects/cli/src/index.spec.ts
+++ projects/cli/src/index.spec.ts
@@ -1,4 +1,4 @@
-import {spawnSync} from 'child_process';
+import {SpawnSyncOptions, spawnSync} from 'child_process';
 import * as path from 'path';
 import * as packageFile from './../package.json';
 
@@ -6,22 +6,22 @@ describe('CLI Tests with ts-node', () => {
 	let cliPath: string;
 	const expectedHelpText = `Usage: ob new <project-name> Creates a new project in current place or ob update <project-name> Updates the oblique package in the project and runs migration Oblique CLI for managing projects Options: -V, --version output the version number -h, --help display help for command
     `;
+	const options = {
+		encoding: 'utf-8',
+		shell: true
+	} as SpawnSyncOptions;
 	beforeAll(() => {
 		cliPath = path.resolve(__dirname, './index.ts');
 	});
 
 	test.each(['-V', '--version'])(`with %s set the version from package.json`, flag => {
-		const result = spawnSync('ts-node', [cliPath, flag], {
-			encoding: 'utf-8'
-		});
-		expect(cleanOutput(result.stdout)).toBe(cleanOutput(packageFile.version));
+		const result = spawnSync('ts-node', [cliPath, flag], options);
+		expect(cleanOutput(result.stdout.toString())).toBe(cleanOutput(packageFile.version));
 	});
 
 	test.each(['-h', '--help'])(`with %s show help text`, flag => {
-		const result = spawnSync('ts-node', [cliPath, flag], {
-			encoding: 'utf-8'
-		});
-		expect(cleanOutput(result.stdout)).toContain(cleanOutput(expectedHelpText));
+		const result = spawnSync('ts-node', [cliPath, flag], options);
+		expect(cleanOutput(result.stdout.toString())).toContain(cleanOutput(expectedHelpText));
 	});
 
 	describe.each([
@@ -29,10 +29,8 @@ describe('CLI Tests with ts-node', () => {
 		{correctOption: '--version', wrongOption: '--vorsion'}
 	])(`$wrongOption instead of $correctOption`, ({wrongOption, correctOption}) => {
 		test(`show suggestion "(Did you mean ${correctOption}?)"`, () => {
-			const result = spawnSync('ts-node', [cliPath, wrongOption], {
-				encoding: 'utf-8'
-			});
-			expect(cleanOutput(result.stderr)).toContain(`(Did you mean ${correctOption}?)`);
+			const result = spawnSync('ts-node', [cliPath, wrongOption], options);
+			expect(cleanOutput(result.stderr.toString())).toContain(`(Did you mean ${correctOption}?)`);
 		});
 	});
 
@@ -42,17 +40,13 @@ describe('CLI Tests with ts-node', () => {
 		{correctOption: '--version', wrongOption: '--wersion'}
 	])('$wrongOption instead of $correctOption', ({wrongOption}) => {
 		test(`show  "error: unknown option '${wrongOption}'"`, () => {
-			const result = spawnSync('ts-node', [cliPath, wrongOption], {
-				encoding: 'utf-8'
-			});
-			expect(cleanOutput(result.stderr)).toContain(`error: unknown option '${wrongOption}'`);
+			const result = spawnSync('ts-node', [cliPath, wrongOption], options);
+			expect(cleanOutput(result.stderr.toString())).toContain(`error: unknown option '${wrongOption}'`);
 		});
 
 		test(`show help after error`, () => {
-			const result = spawnSync('ts-node', [cliPath, wrongOption], {
-				encoding: 'utf-8'
-			});
-			expect(cleanOutput(result.stderr)).toContain(cleanOutput(expectedHelpText));
+			const result = spawnSync('ts-node', [cliPath, wrongOption], options);
+			expect(cleanOutput(result.stderr.toString())).toContain(cleanOutput(expectedHelpText));
 		});
 	});
 
