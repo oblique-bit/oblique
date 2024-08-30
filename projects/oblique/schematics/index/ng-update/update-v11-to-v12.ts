c16f95fa63b35b3a8d147a70a61f196b58bb2782
Olivier Percebois-Garve <olivier.percebois-garve@bit.admin.ch>
2024-08-30 14:45:56 +0200

    feat(oblique/language): remove `ObLanguageService`
    OUI-3215
    BREAKING CHANGE:
    * `ObLanguageService` has been removed without replacement (Solved with Schematics)

--- projects/oblique/schematics/index/ng-update/update-v11-to-v12.ts
+++ projects/oblique/schematics/index/ng-update/update-v11-to-v12.ts
@@ -1,6 +1,15 @@
 import {Rule, SchematicContext, Tree, chain} from '@angular-devkit/schematics';
 import {ObIMigrations} from './ng-update.model';
-import {checkForSSR, checkForStandalone} from '../utils';
+import {
+	applyInTree,
+	checkForSSR,
+	checkForStandalone,
+	createSafeRule,
+	infoMigration,
+	readFile,
+	removeInjectionInClass,
+	removeServiceMethodCall
+} from '../utils';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface IUpdateV11Schema {}
@@ -10,6 +19,21 @@ export class UpdateV11toV12 implements ObIMigrations {
 
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	applyMigrations(_options: IUpdateV11Schema): Rule {
-		return (tree: Tree, _context: SchematicContext) => chain([checkForStandalone(), checkForSSR()])(tree, _context);
+		return (tree: Tree, _context: SchematicContext) =>
+			chain([checkForStandalone(), checkForSSR(), this.removeObLanguageService()])(tree, _context);
+	}
+
+	private removeObLanguageService(): Rule {
+		return createSafeRule((tree: Tree, _context: SchematicContext) => {
+			infoMigration(_context, 'Remove ObLanguageService');
+			const apply = (filePath: string): void => {
+				const content = readFile(tree, filePath);
+				if (content.includes('ObLanguageService')) {
+					removeInjectionInClass(tree, filePath, 'ObLanguageService', '@oblique/oblique');
+					removeServiceMethodCall(tree, filePath, 'setLocaleOnAdapter');
+				}
+			};
+			return applyInTree(tree, apply, '*.ts');
+		});
 	}
 }
