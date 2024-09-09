e25e3ef6e05edae8088f82f0878d62aff1f5215b
René Giller <rene.giller@gmail.com>
2024-09-09 11:30:09 +0200

    refactor(sds/toolchain): improve script readability
    OUI-3226

--- projects/sds/scripts/post-build.ts
+++ projects/sds/scripts/post-build.ts
@@ -1,19 +1,12 @@
 import {CopyFiles} from '../../../scripts/shared/copy-files';
-import {StaticScript} from '../../../scripts/shared/static-script';
 import {Log} from '../../../scripts/shared/log';
 
-class PostBuild extends StaticScript {
-	static perform(): void {
-		Log.start('Finalize build');
-		const source = 'src/nginx';
-		const destination = 'nginx/conf/includes';
-		CopyFiles.initialize('sds')
-			.copyFile('Staticfile', source, '')
-			.copyFile('custom_headers.conf', source, destination)
-			.copyFile('security_headers.conf', source, destination)
-			.finalize();
-		Log.success();
-	}
-}
-
-PostBuild.perform();
+Log.start('Finalize build');
+const source = 'src/nginx';
+const destination = 'nginx/conf/includes';
+CopyFiles.initialize('sds')
+	.copyFile('Staticfile', source, '')
+	.copyFile('custom_headers.conf', source, destination)
+	.copyFile('security_headers.conf', source, destination)
+	.finalize();
+Log.success();
