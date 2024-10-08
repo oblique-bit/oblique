360603f37b7be34211344843577d04a1781dbe28
René Giller <rene.giller@gmail.com>
2024-10-08 02:31:59 +0200

    feat(sds/cms): add a version service to store the version data
    OUI-3260

---
+++ projects/sds/src/app/shared/version/version.service.spec.ts
@@ -0,0 +1,45 @@
+import {TestBed} from '@angular/core/testing';
+
+import {VersionService} from './version.service';
+
+describe(VersionService.name, () => {
+	let service: VersionService;
+	/* eslint-disable @typescript-eslint/naming-convention, camelcase */
+	const cmsData = [
+		{id: 0, base_url: 'base_url_42', version_number: 42},
+		{id: 1, base_url: 'base_url_43', version_number: 43}
+	];
+	/* eslint-enable @typescript-eslint/naming-convention, camelcase */
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({});
+		service = TestBed.inject(VersionService);
+	});
+
+	test('creation', () => {
+		expect(service).toBeTruthy();
+	});
+
+	describe('getBaseUrl method', () => {
+		test('returns an empty string if there is neither data nor version', () => {
+			expect(service.getBaseUrl()).toBe('');
+		});
+
+		test('returns an empty string if there is no data', () => {
+			service.setCurrentVersion(12);
+			expect(service.getBaseUrl()).toBe('');
+		});
+
+		test('returns an empty string if there is no current version', () => {
+			service.setCurrentVersion(undefined);
+			service.setCmsData(cmsData);
+			expect(service.getBaseUrl()).toBe('');
+		});
+
+		test('returns the base url corresponding to the current version', () => {
+			service.setCurrentVersion(42);
+			service.setCmsData(cmsData);
+			expect(service.getBaseUrl()).toBe('base_url_42');
+		});
+	});
+});
---
+++ projects/sds/src/app/shared/version/version.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {Version} from '../../cms/models/version.model';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class VersionService {
+	private cmsVersion: Version[];
+	private currentVersion: number;
+
+	setCmsData(data: Version[]): void {
+		this.cmsVersion = data;
+	}
+
+	setCurrentVersion(version: number): void {
+		this.currentVersion = version;
+	}
+
+	getBaseUrl(): string {
+		return this.cmsVersion?.find(item => item.version_number === this.currentVersion)?.base_url ?? '';
+	}
+}
