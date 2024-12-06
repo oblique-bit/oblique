c89c123eb46e29fb396c15970328d25c48782c5d
Nina <nina.egger@bit.admin.ch>
2024-12-06 13:29:08 +0100

    fix(oblique/language): remove export of `ObLanguageService`
    OUI-3390

--- projects/oblique/src/lib/language/language.module.ts
+++ projects/oblique/src/lib/language/language.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {ObDatePipe} from './date.pipe';
 
 export {ObDatePipe} from './date.pipe';
-export {ObLanguageService} from './language.service';
 
 @NgModule({
 	exports: [ObDatePipe],
