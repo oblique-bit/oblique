3e83eaad25229988edcff87a29001115b7095b57
René Giller <rene.giller@gmail.com>
2025-01-03 18:22:08 +0100

    fix(cli/new): make sure there is no dependency on `tslib`
    Reading json files through `import` did introduce a dependency on `tslib` which was not wanted.
    The new solution only reads the json files at build time without introducing any major refactoring.
    
    OUI-3491

--- projects/cli/scripts/pre-build.ts
+++ projects/cli/scripts/pre-build.ts
@@ -1,14 +1,35 @@
 import fs from 'fs';
 import path from 'path';
 import {Log} from '../../../scripts/shared/log';
+import {PackageJson} from '../../../scripts/shared/package-json';
+import {Files} from '../../../scripts/shared/files';
 
 const workingDir: string = process.cwd();
 const cliDistDir: string = path.resolve(workingDir, '../../dist/cli');
 
-Log.start('Deleting dist/cli folder');
+Log.start('Initialize build');
 try {
+	Log.info('Deleting dist/cli folder');
 	fs.rmSync(cliDistDir, {recursive: true, force: true});
-	Log.success();
 } catch (err) {
 	Log.error((err as Error).message);
+}
+addVersionNumber();
+addSchemaData();
+Log.success();
+
+function addVersionNumber(): void {
+	Log.info('Add Oblique version to "ob-new.model.ts"');
+	const version = PackageJson.readVersion();
+	Files.overwrite('./src/new/ob-new.model.ts', content =>
+		content.replace(/(?<=const version = ')\d+\.\d+\.\d+(?:-(?:alpha|beta|RC)\.\d+)?/, version)
+	);
+}
+
+function addSchemaData(): void {
+	Log.info('Add schema data to "ob-new.model.ts"');
+	const schema = Files.readJson<{properties: object}>('src/new/schema.json');
+	Files.overwrite('./src/new/ob-new.model.ts', content =>
+		content.replace(/(?<=const schema = ).*(?= as \{properties: ObNewOptions<ObNewSchemaOption>})/, JSON.stringify(schema))
+	);
 }
