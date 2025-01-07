927f6cdbcf9a12d77ad1aaa09165ccd7bf164a9e
Nina <nina.egger@bit.admin.ch>
2025-01-07 07:06:41 +0100

    fix(sds/banner): remove `bypassSecurityTrustHtml`
    OUI-3455

--- projects/sds/src/app/banner/banner.component.ts
+++ projects/sds/src/app/banner/banner.component.ts
@@ -1,12 +1,11 @@
 import {Component, Input, ViewEncapsulation} from '@angular/core';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {ObExternalLinkModule} from '@oblique/oblique';
-import {SafeHtmlPipe} from '../shared/safeHtml/safeHtml.pipe';
 
 @Component({
 	selector: 'app-banner',
 	standalone: true,
-	imports: [MatTooltipModule, ObExternalLinkModule, SafeHtmlPipe],
+	imports: [MatTooltipModule, ObExternalLinkModule],
 	templateUrl: './banner.component.html',
 	styleUrl: './banner.component.scss',
 	encapsulation: ViewEncapsulation.None
