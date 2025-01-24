3179135bf53b23bbe475a77563190c7df5d9914c
René Giller <rene.giller@gmail.com>
2025-01-24 17:10:21 +0100

    feat(oblique/http-interceptor): deprecate `ObHttpApiInterceptorModule`
    The module is empty after removing the providers
    
    OUI-3514

--- projects/oblique/src/lib/http-api-interceptor/http-api-interceptor.module.ts
+++ projects/oblique/src/lib/http-api-interceptor/http-api-interceptor.module.ts
@@ -5,5 +5,8 @@ export {ObHttpApiInterceptorEvents} from './http-api-interceptor.events';
 export {ObHttpApiInterceptor} from './http-api-interceptor';
 export {ObIHttpApiRequest} from './http-api-interceptor.model';
 
+/**
+ * Deprecated since Oblique 13.0.0. As this module is now empty and does absolutely nothing, it can safely be removed
+ */
 @NgModule()
 export class ObHttpApiInterceptorModule {}
