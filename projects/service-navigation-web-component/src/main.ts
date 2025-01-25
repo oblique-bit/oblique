2ea96d6d8b3ef13ff3a31ad7e2df5247f4e05add
René Giller <rene.giller@gmail.com>
2025-01-25 12:46:04 +0100

    refactor(service-navigation): use `provideObliqueConfiguration` instead of directly providing tokens
    OUI-3514

--- projects/service-navigation-web-component/src/main.ts
+++ projects/service-navigation-web-component/src/main.ts
@@ -5,28 +5,16 @@ import {importProvidersFrom} from '@angular/core';
 import {createCustomElement} from '@angular/elements';
 import {provideTranslateService} from '@ngx-translate/core';
 import {ObIconModule} from '../../oblique/src/lib/icon/icon.module';
-import {WINDOW, multiTranslateLoader} from '../../oblique/src/lib/utilities';
+import {multiTranslateLoader, provideObliqueConfiguration} from '../../oblique/src/lib/utilities';
 import {ObServiceNavigationWebComponentComponent} from './app/service-navigation-web-component.component';
-import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
-import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
-import {MAT_CHECKBOX_DEFAULT_OPTIONS} from '@angular/material/checkbox';
-import {MAT_RADIO_DEFAULT_OPTIONS} from '@angular/material/radio';
-import {MAT_SLIDE_TOGGLE_DEFAULT_OPTIONS} from '@angular/material/slide-toggle';
-import {MAT_TABS_CONFIG} from '@angular/material/tabs';
 
 createApplication({
 	providers: [
-		{provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}},
-		{provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}},
-		{provide: MAT_CHECKBOX_DEFAULT_OPTIONS, useValue: {color: 'primary'}},
-		{provide: MAT_RADIO_DEFAULT_OPTIONS, useValue: {color: 'primary'}},
-		{provide: MAT_SLIDE_TOGGLE_DEFAULT_OPTIONS, useValue: {color: 'primary'}},
-		{provide: MAT_TABS_CONFIG, useValue: {stretchTabs: false}},
-		{provide: WINDOW, useValue: window},
 		provideHttpClient(),
 		importProvidersFrom(ObIconModule.forRoot()),
 		provideTranslateService(multiTranslateLoader()),
-		provideAnimations()
+		provideAnimations(),
+		provideObliqueConfiguration()
 	]
 })
 	.then(appRef => {
