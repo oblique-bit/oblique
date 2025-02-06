d0416e5b0b0f8a990c524056bd494063c69e2815
René Giller <rene.giller@gmail.com>
2025-02-06 12:34:11 +0100

    feat(oblique/accessibility-statement): add accessibility statement component
    OUI-3468

---
+++ projects/oblique/src/lib/accessibility-statement/accessibility-statement.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {AccessibilityStatementComponent} from './accessibility-statement.component';
+
+export {AccessibilityStatementComponent} from './accessibility-statement.component';
+
+@NgModule({
+	imports: [AccessibilityStatementComponent],
+	exports: [AccessibilityStatementComponent]
+})
+export class AccessibilityStatementModule {}
