9960147d7a2d02473fa8cdcd73d2604114d13b0f
René Giller <rene.giller@gmail.com>
2025-02-17 21:12:34 +0100

    fix(toolchain): avoid git operations on publish script
    This avoid the `Fatal: Dubious ownership` error
    
    OUI-3603

--- scripts/shared/publish.ts
+++ scripts/shared/publish.ts
@@ -1,22 +1,23 @@
 import {version as currentVersion} from '../../package.json';
 import {executeCommandWithLog, getResultFromCommand} from './utils';
 import {StaticScript} from './static-script';
+import {Files} from './files';
 import {Log} from './log';
-import {Git} from './git';
 
 export class Publish extends StaticScript {
 	static perform(packageName: string): void {
 		Log.start(`Publish ${packageName}`);
 		const tag = /^\d+\.\d+\.\d+$/.test(currentVersion) ? 'latest' : 'next';
-		executeCommandWithLog(`npm publish ../../dist/${packageName} --access public --tag ${tag}`, 'Publish');
-		Publish.deprecatePreReleaseVersions(`@oblique/${packageName}`, currentVersion);
+		const distPath = `../../dist/${packageName}`;
+		executeCommandWithLog(`npm publish ${distPath} --access public --tag ${tag}`, 'Publish');
+		Publish.deprecatePreReleaseVersions(`@oblique/${packageName}`, distPath, currentVersion);
 		Log.success();
 	}
 
-	private static deprecatePreReleaseVersions(packageName: string, version: string): void {
+	private static deprecatePreReleaseVersions(packageName: string, distPath: string, version: string): void {
 		if (Publish.getTagOnNext(packageName).startsWith(currentVersion)) {
 			executeCommandWithLog(
-				`npm deprecate ${packageName}@">${version}-0 <${version}" "Oblique ${version} has been released on ${Git.getTagDate(version)}"`,
+				`npm deprecate ${packageName}@">${version}-0 <${version}" "Oblique ${version} has been released on ${Publish.getTagDateFromChangelog(version, distPath)}"`,
 				`Deprecate all pre-release versions of ${version}`
 			);
 			executeCommandWithLog(`npm dist-tag rm ${packageName} next`, 'Remove "next" tag');
@@ -26,5 +27,12 @@ export class Publish extends StaticScript {
 	private static getTagOnNext(packageName: string): string {
 		const distTags = getResultFromCommand(`npm dist-tags ls ${packageName}`);
 		return /(?<=next: )(?<next>.*)$/m.exec(distTags)?.groups?.next ?? '';
+	}
+
+	private static getTagDateFromChangelog(version: string, distPath: string): string {
+		return new RegExp(`(?<=#\\s+\\[${version}]\\([\\w:/.-]*\\)\\s*\\()\\d{4}-\\d{2}-\\d{2}(?=\\))`)
+			.exec(Files.read(`${distPath}/CHANGELOG.md`))
+			.toString()
+			.trim();
 	}
 }
