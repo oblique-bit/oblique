580ea9a928dc41c1003668cf8af7c6616363e0a1
Nina Egger <nina.egger@bit.admin.ch>
2025-03-28 10:25:32 +0100

    feat(sds/material): use `ObDatepickerModule` instead of `MatDatepickerModule`
    OUI-3570

--- projects/sds/src/app/code-examples/code-examples/form/previews/sizes/form-example-sizes-preview.component.ts
+++ projects/sds/src/app/code-examples/code-examples/form/previews/sizes/form-example-sizes-preview.component.ts
@@ -1,15 +1,15 @@
 import {Component} from '@angular/core';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
+import {ObDatepickerModule} from '@oblique/oblique';
 
 @Component({
 	selector: 'app-form-example-sizes-preview',
 	templateUrl: './form-example-sizes-preview.component.html',
 	styleUrls: ['./form-example-sizes-preview.component.scss'],
-	imports: [MatCheckboxModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatSelectModule, MatDatepickerModule]
+	imports: [MatCheckboxModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatSelectModule, ObDatepickerModule]
 })
 export class FormExampleSizesPreviewComponent {}
