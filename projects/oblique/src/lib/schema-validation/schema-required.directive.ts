a9d921eb8947c62840571ba29e5547f3a9bbdedd
Giller René <rene.giller@bit.admin.ch>
2025-04-04 01:29:28 +0200

    feat(oblique/schema-validation): add marker for required fields in template driven forms
    OUI-3750

--- projects/oblique/src/lib/schema-validation/schema-required.directive.ts
+++ projects/oblique/src/lib/schema-validation/schema-required.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, HostBinding, Input, OnInit, Optional} from '@angular/core';
+import {AfterViewInit, Directive, ElementRef, HostBinding, Input, OnInit, Optional, Renderer2, inject} from '@angular/core';
 import {NgModelGroup} from '@angular/forms';
 import {ObSchemaValidationDirective} from './schema-validation.directive';
 
@@ -9,9 +9,11 @@ import {ObSchemaValidationDirective} from './schema-validation.directive';
 	host: {class: 'ob-schema-required-validation'},
 	standalone: true
 })
-export class ObSchemaRequiredDirective implements OnInit {
+export class ObSchemaRequiredDirective implements AfterViewInit, OnInit {
 	@HostBinding('attr.aria-required') required: boolean;
 	@Input() name: string;
+	private readonly host = inject(ElementRef);
+	private readonly renderer = inject(Renderer2);
 
 	constructor(
 		@Optional() private readonly schemaValidation: ObSchemaValidationDirective,
@@ -20,7 +22,27 @@ export class ObSchemaRequiredDirective implements OnInit {
 
 	ngOnInit(): void {
 		if (this.schemaValidation) {
-			this.required = this.schemaValidation.isRequired(this.name, this.modelGroup ? this.modelGroup.path : []) || undefined;
+			this.required = this.schemaValidation.isRequired(this.name, this.modelGroup ? this.modelGroup.path : []);
+		}
+	}
+
+	ngAfterViewInit(): void {
+		if (this.required) {
+			this.insertRequiredMarkerInLabel();
+		}
+	}
+
+	// mimics the behavior of Material for required fields
+	private insertRequiredMarkerInLabel(): void {
+		const matFormField = this.host.nativeElement.closest('.mat-mdc-form-field');
+		if (matFormField) {
+			const label = matFormField.querySelector('label');
+			if (label) {
+				const span = this.renderer.createElement('span');
+				span.setAttribute('class', 'mat-mdc-form-field-required-marker mdc-floating-label--required');
+				span.setAttribute('aria-hidden', 'true');
+				this.renderer.appendChild(label, span);
+			}
 		}
 	}
 }
