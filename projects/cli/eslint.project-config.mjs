ebfde469ad60649aeb9aec1cad16fb7c1d2cdef7
Giller René <rene.giller@bit.admin.ch>
2025-04-28 17:07:22 +0200

    refactor(cli/toolchain): add justification for remaining EsLint exceptions
    OUI-3782

--- projects/cli/eslint.project-config.mjs
+++ projects/cli/eslint.project-config.mjs
@@ -13,12 +13,15 @@ export default [
 			'default-case': 'off', // covered by noImplicitReturns
 
 			// rules that are not respected
+			// this rule would need a massive refactoring as the types are not currently safe
 			'@typescript-eslint/no-unsafe-type-assertion': 'off'
 		}
 	},
 	{
 		files: ['projects/cli/src/**/*.spec.ts'],
 		rules: {
+			// these two rules do have valid exceptions and should be deactivated where necessary. This is not possible as the IDE
+			// doesn't see the errors and thus removes the deactivation comment
 			'@typescript-eslint/no-unsafe-assignment': 'off',
 			'no-console': 'off'
 		}
