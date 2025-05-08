1a61f2d3ee08407acee1d24006f5eacff1510793
Nina Egger <nina.egger@bit.admin.ch>
2025-05-08 12:48:37 +0200

    feat(sds/icon): add example for `ob-icon-text`
    OUI-3775

--- projects/sds/src/app/code-examples/code-examples/icons/icons-code-examples.component.ts
+++ projects/sds/src/app/code-examples/code-examples/icons/icons-code-examples.component.ts
@@ -6,6 +6,7 @@ import {CodeExampleComponent} from '../../code-example/code-example.component';
 import {IconsExampleDefaultPreviewComponent} from './previews/default/icons-example-default-preview.component';
 import {IconsExampleFontSizePreviewComponent} from './previews/font-size/icons-example-font-size-preview.component';
 import {IconsExampleIconsGalleryPreviewComponent} from './previews/icons-gallery/icons-example-icons-gallery-preview.component';
+import {IconsExampleTextPreviewComponent} from './previews/text/icons-example-text-preview.component';
 
 @Component({
 	selector: 'app-code-example-icons',
@@ -33,6 +34,15 @@ export class IconsCodeExamplesComponent extends CodeExamples {
 				this.getSnippet('icons', 'font-size/icons-example-font-size-preview.component.html', 'HTML'),
 				this.getSnippet('icons', 'font-size/icons-example-font-size-preview.component.ts', 'TS'),
 				this.getSnippet('icons', 'font-size/icons-example-font-size-preview.component.scss', 'SCSS')
+			]
+		},
+		{
+			component: IconsExampleTextPreviewComponent,
+			idParts: ['text'],
+			title: 'Icon text',
+			snippets: [
+				this.getSnippet('icons', 'text/icons-example-text-preview.component.html', 'HTML'),
+				this.getSnippet('icons', 'text/icons-example-text-preview.component.ts', 'TS')
 			]
 		},
 		{
