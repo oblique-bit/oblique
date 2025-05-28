3c355148e89a7f5206481d8d37d45ab74e9057e5
Nina Egger <nina.egger@bit.admin.ch>
2025-05-28 08:13:41 +0200

    feat(oblique/language): add ´ObDateComponent´
    OUI-3819

--- projects/oblique/src/lib/language/language.module.ts
+++ projects/oblique/src/lib/language/language.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
 import {ObDatePipe} from './date.pipe';
+import {ObDateComponent} from './date/date.component';
 
 export {ObDatePipe} from './date.pipe';
+export {ObEDateFormats} from './date/date.model';
+export {ObDateComponent} from './date/date.component';
 
 @NgModule({
-	exports: [ObDatePipe],
-	imports: [ObDatePipe]
+	exports: [ObDatePipe, ObDateComponent],
+	imports: [ObDatePipe, ObDateComponent]
 })
 export class ObLanguageModule {}
