b59ba60dd23a017117278ee86d61dda6610735e7
Etienne FRANK <etienne.frank@bit.admin.ch>
2025-06-02 16:18:33 +0200

    refactor(oblique/service-navigation): simplify contact-to-link pipe
    TPEFD-9121

--- projects/oblique/src/lib/service-navigation/info/contact-to-links.pipe.ts
+++ projects/oblique/src/lib/service-navigation/info/contact-to-links.pipe.ts
@@ -7,42 +7,39 @@ import {ObISectionLink, ObIServiceNavigationContact} from '../service-navigation
 })
 export class ObContactToLinksPipe implements PipeTransform {
 	transform(values?: ObIServiceNavigationContact): ObISectionLink[] {
-		return Object.keys(values ?? {})
-			.filter(key => !!values[key])
-			.reduce<{type: string; value: string}[]>(
-				(links, key) => [
-					...links,
-					{
-						type: key,
-						value: values[key]
-					}
-				],
-				[]
-			)
-			.map(link => ({
-				url: this.buildUrl(link),
-				label: link.value,
-				isInternalLink: true,
-				ariaLabel: this.buildTelAriaLabel(link)
-			}));
+		return [this.getMail(values?.email), this.getTel(values?.tel)].filter(Boolean);
 	}
 
-	private buildUrl(link: {type: string; value: string}): string {
-		const prefix = link.type === 'email' ? 'mailto' : 'tel';
-		return `${prefix}:${link.value}`;
+	private getMail(value: string | undefined): ObISectionLink | undefined {
+		if (value === undefined) {
+			return undefined;
+		}
+
+		return {
+			url: `mailto:${value}`,
+			label: value,
+			isInternalLink: true
+		};
 	}
 
-	private buildTelAriaLabel(link: {type: string; value: string}): {text: string; parameters: Record<string, unknown>} | undefined {
-		return link.type === 'tel'
-			? {
-					text: 'i18n.oblique.service-navigation.section.tel.aria-label',
-					parameters: {
-						phoneNumber: link.value
-							.split('')
-							.filter(character => character !== ' ')
-							.join(', ')
-					}
+	private getTel(value: string | undefined): ObISectionLink | undefined {
+		if (value === undefined) {
+			return undefined;
+		}
+
+		return {
+			url: `tel:${value}`,
+			label: value,
+			isInternalLink: true,
+			ariaLabel: {
+				text: 'i18n.oblique.service-navigation.section.tel.aria-label',
+				parameters: {
+					phoneNumber: value
+						.split('')
+						.filter(character => character !== ' ')
+						.join(', ')
 				}
-			: undefined;
+			}
+		};
 	}
 }
