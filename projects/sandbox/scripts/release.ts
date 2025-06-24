e68d72c488f6f7292981949d616a543434cc6c6a
Olivier Percebois-Garve <olivier.percebois-garve@bit.admin.ch>
2025-06-24 13:45:10 +0200

    build(scripts): remove updateSonarPropertiesVersion()
    OUI-3874

--- projects/sandbox/scripts/release.ts
+++ projects/sandbox/scripts/release.ts
@@ -1,10 +1,9 @@
 import {version} from '../../../package.json';
 import {Changelog} from '../../../scripts/shared/changelog';
-import {updatePackageJsonVersion, updateSonarPropertiesVersion} from '../../../scripts/shared/utils';
+import {updatePackageJsonVersion} from '../../../scripts/shared/utils';
 import {Log} from '../../../scripts/shared/log';
 
 Log.start(`Release Sandbox ${version}`);
 Changelog.addRelease(version, 'sandbox');
 updatePackageJsonVersion(version);
-updateSonarPropertiesVersion(version);
 Log.success();
--- projects/sds/scripts/release.ts
+++ projects/sds/scripts/release.ts
@@ -1,5 +1,5 @@
 import {Changelog} from '../../../scripts/shared/changelog';
-import {updatePackageJsonVersion, updateSonarPropertiesVersion} from '../../../scripts/shared/utils';
+import {updatePackageJsonVersion} from '../../../scripts/shared/utils';
 import {version} from '../../../package.json';
 import {ObliqueVersion} from './oblique-version';
 import {Log} from '../../../scripts/shared/log';
@@ -8,5 +8,4 @@ Log.start(`Release SDS ${version}`);
 ObliqueVersion.updateObliqueVersion();
 Changelog.addRelease(version, 'sds');
 updatePackageJsonVersion(version);
-updateSonarPropertiesVersion(version);
 Log.success();
