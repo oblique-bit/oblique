d5209c1533ab1608ce4fff446115a543323fe744
Giller René <rene.giller@bit.admin.ch>
2025-07-17 09:26:30 +0200

    build(toolchain/scripts): add a `version` file that always contains the current version number
    The version is updated with each release
    
    OUI-3826

--- projects/toolchain/scripts/release.ts
+++ projects/toolchain/scripts/release.ts
@@ -2,8 +2,10 @@ import {Changelog} from '../../../scripts/shared/changelog';
 import {updatePackageJsonVersion} from '../../../scripts/shared/utils';
 import {version} from '../../../package.json';
 import {Log} from '../../../scripts/shared/log';
+import {Files} from '../../../scripts/shared/files';
 
 Log.start(`Release Toolchain ${version}`);
 Changelog.addRelease(version, 'toolchain');
+Files.write('src/schematics/version.ts', `export const currentToolchainVersion = '${version}';\n`);
 updatePackageJsonVersion(version);
 Log.success();
