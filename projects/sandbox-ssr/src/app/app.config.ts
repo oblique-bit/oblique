2a206ac83623685e79aeee2e4fb2aab6435f0132
Giller René <rene.giller@bit.admin.ch>
2025-07-18 12:34:01 +0200

    refactor(sandbox-ssr): add comment to explain why `provideObliqueTranslation` cannot be used
    OUI-3921

--- projects/sandbox-ssr/src/app/app.config.ts
+++ projects/sandbox-ssr/src/app/app.config.ts
@@ -4,8 +4,14 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
 import {provideTranslateService} from '@ngx-translate/core';
-import {multiTranslateLoader} from './shared/multi-translate-loader/multi-translate-loader.utils';
 import {provideHttpClient, withFetch} from '@angular/common/http';
+/**
+ * Since Sandbox-SSR enforces stricter TypeScript rules than the Oblique library, it cannot import anything directly
+ * from Oblique without causing numerous transpilation errors. Sandbox-SSR would apply its strict type checking to
+ * Oblique’s code, which leads to these transpilation errors.
+ * Therefore, the translation handling code must be duplicated within Sandbox-SSR to avoid these issues.
+ */
+import {multiTranslateLoader} from './shared/multi-translate-loader/multi-translate-loader.utils';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
--- projects/sandbox-ssr/src/app/shared/multi-translate-loader/multi-translate-loader.utils.ts
+++ projects/sandbox-ssr/src/app/shared/multi-translate-loader/multi-translate-loader.utils.ts
@@ -2,6 +2,13 @@ import {HttpClient} from '@angular/common/http';
 import {TranslateLoader, type TranslateModuleConfig} from '@ngx-translate/core';
 import {ObMultiTranslateLoader} from './multi-translate-loader';
 
+/**
+ * Since Sandbox-SSR enforces stricter TypeScript rules than the Oblique library, it cannot import anything directly
+ * from Oblique without causing numerous transpilation errors. Sandbox-SSR would apply its strict type checking to
+ * Oblique’s code, which leads to these transpilation errors.
+ * Therefore, the translation handling code must be duplicated within Sandbox-SSR to avoid these issues.
+ */
+
 function getTranslateLoader(http: HttpClient): ObMultiTranslateLoader {
 	return new ObMultiTranslateLoader(http, [
 		{
