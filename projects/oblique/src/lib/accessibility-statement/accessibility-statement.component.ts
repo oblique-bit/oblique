89d06459b4d3b6d92fe91f7db513179c83888ed9
Giller René <rene.giller@bit.admin.ch>
2025-08-18 14:47:11 +0200

    refactor(oblique/accessibility-statement): split contact parsing into multiple functions
    OUI-3991

--- projects/oblique/src/lib/accessibility-statement/accessibility-statement.component.ts
+++ projects/oblique/src/lib/accessibility-statement/accessibility-statement.component.ts
@@ -8,6 +8,7 @@ import {ObConformity, ObContactData} from '../utilities.model';
 import {OB_ACCESSIBILITY_STATEMENT_CONFIGURATION} from '../utilities';
 import {ObDatePipe} from '../language/date.pipe';
 import {ObEIcon} from '../icon/icon.model';
+import {ObIAccessibilityStatementContactInfo} from './accessibility-statement.model';
 
 @Component({
 	selector: 'ob-accessibility-statement',
@@ -43,37 +44,43 @@ export class AccessibilityStatementComponent {
 			: 'i18n.oblique.accessibility-statement.statement.no-exception';
 	}
 
-	private parseContact(contact: ObContactData): {
-		label: string;
-		url: string;
-		icon: ObEIcon;
-		context?: string;
-		isExternal: boolean;
-	} {
+	private parseContact(contact: ObContactData): ObIAccessibilityStatementContactInfo {
 		if (contact.phone) {
-			return {
-				label: contact.phone,
-				url: `tel:${contact.phone}`,
-				icon: ObEIcon.PHONE,
-				context: contact.context,
-				isExternal: false
-			};
+			return this.buildPhoneContact(contact.phone, contact.context);
 		}
 		if (contact.email) {
-			return {
-				label: contact.email,
-				url: `mailto:${contact.email}`,
-				icon: ObEIcon.MAIL,
-				context: contact.context,
-				isExternal: false
-			};
+			return this.buildMailContact(contact.email, contact.context);
 		}
-		const isExternal = contact.url.startsWith('http');
+		return this.buildUrlContact(contact.url, contact.context);
+	}
+
+	private buildPhoneContact(phone: string, context: string): ObIAccessibilityStatementContactInfo {
 		return {
-			label: contact.url,
-			url: contact.url,
+			label: phone,
+			url: `tel:${phone}`,
+			icon: ObEIcon.PHONE,
+			context,
+			isExternal: false
+		};
+	}
+
+	private buildMailContact(email: string, context: string): ObIAccessibilityStatementContactInfo {
+		return {
+			label: email,
+			url: `mailto:${email}`,
+			icon: ObEIcon.MAIL,
+			context,
+			isExternal: false
+		};
+	}
+
+	private buildUrlContact(url: string, context: string): ObIAccessibilityStatementContactInfo {
+		const isExternal = url.startsWith('http');
+		return {
+			label: url,
+			url,
 			icon: isExternal ? ObEIcon.LINK_EXTERNAL : ObEIcon.LINK,
-			context: contact.context,
+			context,
 			isExternal
 		};
 	}
