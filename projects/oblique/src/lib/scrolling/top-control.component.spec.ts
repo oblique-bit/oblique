60c26bd5f62035720b3cce4bccbffb436ce3e4ec
Giller René <rene.giller@bit.admin.ch>
2025-08-22 16:16:59 +0200

    feat(oblique/scrolling): remove `scrollTarget` property from `ObTopControlComponent`
    This was an internal property that was never documented.
    
    OUI-3924
    BREAKING CHANGE:
    `scrollTarget` has been removed without replacement.

--- projects/oblique/src/lib/scrolling/top-control.component.spec.ts
+++ projects/oblique/src/lib/scrolling/top-control.component.spec.ts
@@ -11,83 +11,39 @@ describe('ObTopControlComponent', () => {
 	let fixture: ComponentFixture<ObTopControlComponent>;
 	let topControlComponent: ObTopControlComponent;
 
-	describe('without a scrollTarget', () => {
-		beforeEach(waitForAsync(() => {
-			TestBed.configureTestingModule({
-				imports: [ObTopControlComponent, ObMockTranslatePipe],
-				schemas: [CUSTOM_ELEMENTS_SCHEMA],
-				providers: [
-					{provide: TranslateService, useClass: ObMockTranslateService},
-					{provide: WINDOW, useValue: window}
-				]
-			}).compileComponents();
-		}));
+	beforeEach(waitForAsync(() => {
+		TestBed.configureTestingModule({
+			imports: [ObTopControlComponent, ObMockTranslatePipe],
+			schemas: [CUSTOM_ELEMENTS_SCHEMA],
+			providers: [
+				{provide: TranslateService, useClass: ObMockTranslateService},
+				{provide: WINDOW, useValue: window}
+			]
+		}).compileComponents();
+	}));
 
-		beforeEach(() => {
-			fixture = TestBed.createComponent(ObTopControlComponent);
-			topControlComponent = fixture.componentInstance;
-			fixture.detectChanges();
-		});
-
-		it('should create', () => {
-			expect(topControlComponent).toBeDefined();
-		});
-
-		it('should have ob-top-control class', () => {
-			expect(fixture.debugElement.nativeElement.classList.contains('ob-top-control')).toBe(true);
-		});
-
-		describe('scrollToTop', () => {
-			it('should be an observable', () => {
-				expect(topControlComponent.scrollToTop instanceof Observable).toBe(true);
-			});
-			it('should emit when scrollTop is called', () => {
-				jest.spyOn(topControlComponent.scrollToTop, 'emit');
-				topControlComponent.scrollTop();
-				expect(topControlComponent.scrollToTop.emit).toHaveBeenCalledWith();
-			});
-		});
-
-		describe('scrollTop', () => {
-			it('should not scroll', () => {
-				topControlComponent.scrollTop();
-				expect(topControlComponent.scrollTarget).toBeUndefined();
-			});
-			it('should emit', () => {
-				topControlComponent.scrollTop();
-				expect(topControlComponent.scrollTarget).toBeUndefined();
-			});
-		});
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ObTopControlComponent);
+		topControlComponent = fixture.componentInstance;
+		fixture.detectChanges();
 	});
 
-	describe('with a scrollTarget', () => {
-		beforeEach(waitForAsync(() => {
-			TestBed.configureTestingModule({
-				imports: [ObTopControlComponent, ObMockTranslatePipe],
-				schemas: [CUSTOM_ELEMENTS_SCHEMA],
-				providers: [
-					{provide: TranslateService, useClass: ObMockTranslateService},
-					{provide: WINDOW, useValue: window}
-				]
-			}).compileComponents();
-		}));
+	it('should create', () => {
+		expect(topControlComponent).toBeDefined();
+	});
 
-		beforeEach(() => {
-			fixture = TestBed.createComponent(ObTopControlComponent);
-			topControlComponent = fixture.componentInstance;
-			topControlComponent.scrollTarget = {scrollTo: jest.fn()} as unknown as HTMLElement;
-			fixture.detectChanges();
+	it('should have ob-top-control class', () => {
+		expect(fixture.debugElement.nativeElement.classList.contains('ob-top-control')).toBe(true);
+	});
+
+	describe('scrollToTop', () => {
+		it('should be an observable', () => {
+			expect(topControlComponent.scrollToTop instanceof Observable).toBe(true);
 		});
-
-		it('should create', () => {
-			expect(topControlComponent).toBeDefined();
-		});
-
-		describe('scrollTop', () => {
-			it('should call target.scrollTo', () => {
-				topControlComponent.scrollTop();
-				expect(topControlComponent.scrollTarget.scrollTo).toHaveBeenCalledWith({top: 0, behavior: 'smooth'});
-			});
+		it('should emit when scrollTop is called', () => {
+			jest.spyOn(topControlComponent.scrollToTop, 'emit');
+			topControlComponent.scrollTop();
+			expect(topControlComponent.scrollToTop.emit).toHaveBeenCalledWith();
 		});
 	});
 });
