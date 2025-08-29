aff0b7599fe5be76f16d3305ded41c9227d9f760
Giller René <rene.giller@bit.admin.ch>
2025-08-29 18:24:02 +0200

    feat(sds/styles): document `prose` headings
    OUI-4010

--- projects/sds/src/app/code-examples/code-examples/typography/previews/headings/typography-example-headings-preview.component.ts
+++ projects/sds/src/app/code-examples/code-examples/typography/previews/headings/typography-example-headings-preview.component.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 @Component({
 	selector: 'app-typography-example-headings-preview',
 	templateUrl: './typography-example-headings-preview.component.html',
+	styleUrl: './typography-example-headings-preview.component.scss',
 	standalone: true
 })
 export class TypographyExampleHeadingsPreviewComponent {}
