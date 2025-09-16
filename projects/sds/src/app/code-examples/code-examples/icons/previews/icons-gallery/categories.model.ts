e9540961f2d0cc4fedac2789cc103eba41ca7a12
Olivier Percebois-Garve <olivier.percebois-garve@bit.admin.ch>
2025-09-16 16:10:01 +0200

    refactor(sds/icon): add ObECategory type
    OUI-3860

---
+++ projects/sds/src/app/code-examples/code-examples/icons/previews/icons-gallery/categories.model.ts
@@ -0,0 +1,15 @@
+export enum ObECategory {
+	NAVIGATION = 'Navigation',
+	INTERFACE = 'Interface',
+	COMMUNICATION = 'Communication',
+	PHYSICAL_ITEMS_AND_INFRASTRUCTURE = 'Physical items and infrastructure',
+	DEVICES_AND_TECH = 'Devices and tech',
+	STATUS_AND_FEEDBACK = 'Status and feedback',
+	PEOPLE_AND_IDENTITY = 'People and identity',
+	DATA_VISUALISATION = 'Data visualisation',
+	ACCESSIBILITY = 'Accessibility',
+	SOCIAL_MEDIA = 'Social media',
+	CONTENT_AND_FILES = 'Content and files',
+	MEDIA_PLAYER = 'Media player',
+	DEPRECATED_ICONS = 'Deprecated icons'
+}
