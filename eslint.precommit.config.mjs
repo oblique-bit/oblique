7182e120d62c802d04e4aa46c8f1d672b5d1e52b
Giller René <rene.giller@bit.admin.ch>
2025-11-03 11:18:48 +0100

    build(lint): add eslint precommit config to avoid strict mode errors
    OUI-4171

---
+++ eslint.precommit.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import baseConfig from './eslint.config.mjs';
+
+// This configuration exists solely for lint-staged.
+// As lint-staged only reads the root tsconfig and strict mode is not enabled there, ESLint treats all projects as
+// non-strict during pre-commit runs. This causes rules that require strict mode to fail.
+export default [
+	...baseConfig,
+	{
+		files: ['**/*.ts'],
+		rules: {
+			'@typescript-eslint/no-unnecessary-boolean-literal-compare': 'off',
+			'@typescript-eslint/no-unnecessary-condition': 'off',
+			'@typescript-eslint/prefer-nullish-coalescing': 'off',
+			'@typescript-eslint/strict-boolean-expressions': 'off'
+		}
+	}
+];
