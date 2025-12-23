1efecee717c77689b2e1e12726831884c6de79fa
Giller René <rene.giller@bit.admin.ch>
2025-12-23 15:20:39 +0100

    build(design-system/scripts): refactor to separate git operations from file manipulations
    OUI-4266

---
+++ projects/design-system/scripts/tokens/git.mjs
@@ -0,0 +1,35 @@
+import {execSync} from 'node:child_process';
+import {rmSync, renameSync} from 'node:fs';
+
+const remote = 'github';
+
+export function checkoutThemeFiles(themesPath) {
+	removeRemote(remote);
+	addRemote(remote, 'https://github.com/oblique-bit/oblique.git');
+	fetchRemote(remote);
+	checkoutFolder(themesPath, 'tokens-main', remote);
+}
+
+function removeRemote(remoteName) {
+	try {
+		execSync(`git remote remove ${remoteName}`);
+	} catch {
+		// nothing to do, it simply means that the remote didn't exist
+	}
+}
+
+function addRemote(remoteName, url) {
+	execSync(`git remote add ${remoteName} ${url}`);
+}
+
+function fetchRemote(remoteName) {
+	execSync(`git fetch ${remoteName}`);
+}
+
+function checkoutFolder(folder, branch, origin = 'origin') {
+	rmSync(folder, {recursive: true, force: true});
+	renameSync('../../.husky/post-checkout', '../../.husky/post-checkout.bak'); // disable husky hook
+	execSync(`git checkout ${origin}/${branch} -- ${folder}`);
+	execSync(`git rm -r --cached ${folder}`);
+	renameSync('../../.husky/post-checkout.bak', '../../.husky/post-checkout'); // re-enable husky hook
+}
--- projects/design-system/scripts/tokens/themes.mjs
+++ projects/design-system/scripts/tokens/themes.mjs
@@ -1,39 +1,9 @@
-import {execSync} from 'node:child_process';
 import {readFileSync} from 'node:fs';
-import {rmSync, renameSync} from 'node:fs';
-
-const remote = 'github';
+import {checkoutThemeFiles} from './git.mjs';
 
 export function getThemes(themesPath) {
-	removeRemote(remote);
-	addRemote(remote, 'https://github.com/oblique-bit/oblique.git');
-	fetchRemote(remote);
-	checkoutFolder(themesPath, 'tokens-main', remote);
+	checkoutThemeFiles(themesPath);
 	return listThemeFiles(themesPath);
-}
-
-function removeRemote(remoteName) {
-	try {
-		execSync(`git remote remove ${remoteName}`);
-	} catch {
-		// nothing to do, it simply means that the remote didn't exist
-	}
-}
-
-function addRemote(remoteName, url) {
-	execSync(`git remote add ${remoteName} ${url}`);
-}
-
-function fetchRemote(remoteName) {
-	execSync(`git fetch ${remoteName}`);
-}
-
-function checkoutFolder(folder, branch, origin = 'origin') {
-	rmSync(folder, {recursive: true, force: true});
-	renameSync('../../.husky/post-checkout', '../../.husky/post-checkout.bak'); // disable husky hook
-	execSync(`git checkout ${origin}/${branch} -- ${folder}`);
-	execSync(`git rm -r --cached ${folder}`);
-	renameSync('../../.husky/post-checkout.bak', '../../.husky/post-checkout'); // re-enable husky hook
 }
 
 function listThemeFiles(sourceFolder) {
