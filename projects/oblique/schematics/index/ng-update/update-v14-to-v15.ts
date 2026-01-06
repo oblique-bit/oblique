0ca1c4645e118a95d90de1fbd959a57a7b9d4b9c
Olivier Percebois-Garve <olivier.percebois-garve@bit.admin.ch>
2026-01-06 14:11:48 +0100

    feat(oblique/icon): update icon set with new, renamed and redrawn icons
    New icons:
    * airplane
    * airplane_landing
    * airplane_takeoff
    * bookmark_filled
    * signal
    * signal_off
    * status_arrow_right
    
    Redrawn icons
    * accommodation
    * antique-building
    * badge
    * bot
    * calculator
    * car
    * exclamation_triangle
    * exclamation_diamond
    * exclamation_circle
    * github
    * graduation-cap
    * id-card
    * person_medic
    * x-twitter
    * info_circle
    
    OUI-4167
    BREAKING CHANGE:
    * The following icons have been renamed (Solved by Schematics):
    ** arrow_down-left      -> arrow_down_left
    ** arrow_down-right     -> arrow_down_right
    ** arrow_up-left        -> arrow_up_left
    ** arrow_up-right       -> arrow_up_right
    ** chevron_left-small   -> chevron_left_small
    ** chevron_double-left  -> chevron_left_double
    ** chevron_right-small  -> chevron_right_small
    ** chevron_double-right -> chevron_right_double
    ** chevron_down-small   -> chevron_down_small
    ** chevron_up-small     -> chevron_up_small
    ** sort-list_descending -> sort_list_descending
    ** sort-list_ascending  -> sort_list_ascending

--- projects/oblique/schematics/index/ng-update/update-v14-to-v15.ts
+++ projects/oblique/schematics/index/ng-update/update-v14-to-v15.ts
@@ -18,7 +18,7 @@ export class UpdateV14toV15 implements ObIMigrations {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	applyMigrations(options: IUpdateV15Schema): Rule {
 		return (tree: Tree, context: SchematicContext) =>
-			chain([warnIfStandalone(), this.removeBrowserAnimationModuleIfUnused()])(tree, context);
+			chain([warnIfStandalone(), this.renameIcons(), this.removeBrowserAnimationModuleIfUnused()])(tree, context);
 	}
 
 	private removeBrowserAnimationModuleIfUnused(): Rule {
@@ -49,6 +49,38 @@ export class UpdateV14toV15 implements ObIMigrations {
 				}
 			};
 			return applyInTree(tree, toApply, '*.ts');
+		});
+	}
+	private renameIcons(): Rule {
+		return createSafeRule((tree: Tree, context: SchematicContext) => {
+			infoMigration(context, 'Rename icons');
+			const toApply = (filePath: string): void => {
+				replaceInFile(tree, filePath, /svgIcon="arrow_down-left"/g, 'svgIcon="arrow_down_left"');
+				replaceInFile(tree, filePath, /ObEIcon\.ARROW_DOWN-LEFT/g, 'ObEIcon.ARROW_DOWN_LEFT');
+				replaceInFile(tree, filePath, /svgIcon="arrow_down-right"/g, 'svgIcon="arrow_down_right"');
+				replaceInFile(tree, filePath, /ObEIcon\.ARROW_DOWN-RIGHT/g, 'ObEIcon.ARROW_DOWN_RIGHT');
+				replaceInFile(tree, filePath, /svgIcon="arrow_up-left"/g, 'svgIcon="arrow_up_left"');
+				replaceInFile(tree, filePath, /ObEIcon\.ARROW_UP-LEFT/g, 'ObEIcon.ARROW_UP_LEFT');
+				replaceInFile(tree, filePath, /svgIcon="arrow_up-right"/g, 'svgIcon="arrow_up_right"');
+				replaceInFile(tree, filePath, /ObEIcon\.ARROW_UP-RIGHT/g, 'ObEIcon.ARROW_UP_RIGHT');
+				replaceInFile(tree, filePath, /svgIcon="chevron_left-small"/g, 'svgIcon="chevron_left_small"');
+				replaceInFile(tree, filePath, /ObEIcon\.CHEVRON_LEFT-SMALL/g, 'ObEIcon.CHEVRON_LEFT_SMALL');
+				replaceInFile(tree, filePath, /svgIcon="chevron_double-left"/g, 'svgIcon="chevron_left_double"');
+				replaceInFile(tree, filePath, /ObEIcon\.CHEVRON_DOUBLE-LEFT/g, 'ObEIcon.CHEVRON_DOUBLE_LEFT');
+				replaceInFile(tree, filePath, /svgIcon="chevron_right-small"/g, 'svgIcon="chevron_right_small"');
+				replaceInFile(tree, filePath, /ObEIcon\.CHEVRON_RIGHT-SMALL/g, 'ObEIcon.CHEVRON_RIGHT_SMALL');
+				replaceInFile(tree, filePath, /svgIcon="chevron_double-right"/g, 'svgIcon="chevron_right_double"');
+				replaceInFile(tree, filePath, /ObEIcon\.CHEVRON_DOUBLE-RIGHT/g, 'ObEIcon.CHEVRON_DOUBLE_RIGHT');
+				replaceInFile(tree, filePath, /svgIcon="chevron_down-small"/g, 'svgIcon="chevron_down_small"');
+				replaceInFile(tree, filePath, /ObEIcon\.CHEVRON_DOWN-SMALL/g, 'ObEIcon.CHEVRON_DOWN_SMALL');
+				replaceInFile(tree, filePath, /svgIcon="chevron_up-small"/g, 'svgIcon="chevron_up_small"');
+				replaceInFile(tree, filePath, /ObEIcon\.CHEVRON_UP-SMALL/g, 'ObEIcon.ACHEVRON_UP_SMALL');
+				replaceInFile(tree, filePath, /svgIcon="sort-list_descending"/g, 'svgIcon="sort_list_descending"');
+				replaceInFile(tree, filePath, /ObEIcon\.SORT-LIST_DESCENDING/g, 'ObEIcon.SORT_LIST_DESCENDING');
+				replaceInFile(tree, filePath, /svgIcon="sort-list_ascending"/g, 'svgIcon="sort_list_ascending"');
+				replaceInFile(tree, filePath, /ObEIcon\.SORT-LIST_ASCENDING/g, 'ObEIcon.SORT_LIST_ASCENDING');
+			};
+			return applyInTree(tree, toApply, '*.{ts,html}');
 		});
 	}
 }
