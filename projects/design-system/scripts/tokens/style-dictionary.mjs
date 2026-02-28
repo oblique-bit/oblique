6b015676cc4f1860d6a5577f312e698ea17d18ea
Giller René BIT <rene.giller@bit.admin.ch>
2026-03-01 00:02:52 +0100

    refactor(design-system/styles): rename `styles` into `css`
    This is done to avoid confusion with Figma where `styles` has another meaning
    
    OUI-4190

--- projects/design-system/scripts/tokens/style-dictionary.mjs
+++ projects/design-system/scripts/tokens/style-dictionary.mjs
@@ -39,7 +39,7 @@ function buildConfigs(modes, libFolder, components) {
 						],
 						files: [
 							{
-								destination: `${libFolder}/styles/oblique-tokens.css`,
+								destination: `${libFolder}/css/oblique-tokens.css`,
 								format: coreFormat.name,
 								options: {
 									mode: mode.selector,
