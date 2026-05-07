04ca771217959bb9bb1520d35a9378cd72aced05
Dario Poggiali <dario.poggiali@bit.admin.ch>
2026-05-07 17:30:47 +0200

    feat(design-system/demo): add demo component using Lit
    Lit is added as a `devDependency` so that it is installed locally and as a `peerDependency` so that
    it is installed for consuming projects
    
    OUI-4439

--- projects/design-system/src/lib/demo/demo.component.spec.ts
+++
@@ -1,8 +0,0 @@
-import {describe, expect, test} from 'vitest';
-import {DemoComponent} from './demo.component';
-
-describe(DemoComponent.name, () => {
-	test('creation', () => {
-		expect(new DemoComponent()).toBeTruthy();
-	});
-});
