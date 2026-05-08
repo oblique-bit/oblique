617a08c64e52495d07e2164a8fcc79d25f680c44
Giller René BIT <rene.giller@bit.admin.ch>
2026-05-08 13:10:48 +0200

    test(design-system): use vitest instead of jest
    OUI-4439

--- projects/design-system/src/lib/demo/demo.component.spec.ts
+++ projects/design-system/src/lib/demo/demo.component.spec.ts
@@ -1,21 +1,8 @@
-import {type ComponentFixture, TestBed} from '@angular/core/testing';
+import {describe, expect, test} from 'vitest';
 import {DemoComponent} from './demo.component';
 
 describe(DemoComponent.name, () => {
-	let component: DemoComponent;
-	let fixture: ComponentFixture<DemoComponent>;
-
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			imports: [DemoComponent],
-		}).compileComponents();
-
-		fixture = TestBed.createComponent(DemoComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
-
 	test('creation', () => {
-		expect(component).toBeTruthy();
+		expect(new DemoComponent()).toBeTruthy();
 	});
 });
