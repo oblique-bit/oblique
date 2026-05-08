f69767b36dbc4f6fb48bd549e26cd43c18957d27
Giller René BIT <rene.giller@bit.admin.ch>
2026-05-08 13:10:48 +0200

    test(design-system): use vitest instead of jest
    OUI-4439

---
+++ projects/design-system/vitest.config.ci.ts
@@ -0,0 +1,19 @@
+import {defineConfig, mergeConfig} from 'vitest/config';
+import baseConfig from './vitest.config';
+import {resolve} from 'node:path';
+
+export default mergeConfig(
+	baseConfig,
+	defineConfig({
+		test: {
+			environment: 'jsdom',
+			browser: {
+				enabled: false,
+			},
+			reporters: ['default', 'junit'],
+			outputFile: {
+				junit: resolve(__dirname, '../../coverage/sqr.xml'),
+			},
+		},
+	})
+);
