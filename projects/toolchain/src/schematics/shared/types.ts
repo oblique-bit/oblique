e558fa7436fc120f0288202b0f697a4217d9a9c5
Giller René BIT <rene.giller@bit.admin.ch>
2026-05-28 18:10:45 +0200

    refactor(toolchain/schematics): add shared `createFromTemplate` function
    OUI-4058

--- projects/toolchain/src/schematics/shared/types.ts
+++ projects/toolchain/src/schematics/shared/types.ts
@@ -6,3 +6,7 @@ export interface ObJsonProperty {
 }
 
 export type ObMakeOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+export interface ObSchemaOptions {
+	silent: boolean;
+}
