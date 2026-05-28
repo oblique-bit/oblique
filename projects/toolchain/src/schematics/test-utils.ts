f9a28d64867fc044b21d76a5136afde88d71d36a
Giller René BIT <rene.giller@bit.admin.ch>
2026-05-28 17:16:59 +0200

    test(toolchain/schematics): add `mockCreateFromTemplate` function
    OUI-4058

--- projects/toolchain/src/schematics/test-utils.ts
+++ projects/toolchain/src/schematics/test-utils.ts
@@ -1,6 +1,9 @@
+import {join} from 'node:path';
 import type * as fs from 'fs';
+import {readFileSync, readdirSync} from 'fs';
 import type {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
-import type {Rule} from '@angular-devkit/schematics';
+import type {Rule, Tree} from '@angular-devkit/schematics';
+import * as template from './shared/template/template';
 
 type FsFunctions = keyof Pick<
 	typeof fs,
@@ -29,5 +32,22 @@ export async function runRule(runner: SchematicTestRunner, rule: Rule, tree: Uni
 	return new Promise((resolve, reject) => {
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
 		runner.callRule(rule, tree).subscribe({next: treeResolve => resolve(treeResolve as UnitTestTree), error: reject});
+	});
+}
+
+/**
+ * In tests based on {@link callRule}, the SchematicContext do not contain the template directory, meaning that
+ * {@link createFromTemplate} throws an error. To circumvent this problem, `mockCreateFromTemplate` manually
+ * creates the files
+ */
+export function mockCreateFromTemplate(collection: string): void {
+	jest.spyOn(template, 'createFromTemplate').mockImplementation((templateDir: string) => (tree: Tree) => {
+		const dir = join(__dirname, `/${collection}/${templateDir}`);
+		readdirSync(dir).forEach(file => {
+			const content = readFileSync(`${dir}/${file}`);
+			tree.create(`/${file}`, content);
+		});
+
+		return tree;
 	});
 }
