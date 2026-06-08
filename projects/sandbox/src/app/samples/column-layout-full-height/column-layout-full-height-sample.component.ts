9922024aaefe7659a0d70f50639d796ae56cf23a
Olivier Percebois-Garve <olivier.percebois-garve@bit.admin.ch>
2026-06-08 16:55:54 +0200

    feat(sandbox/column-layout): ensure toggling of layout setting on destroy
    OUI-4502

--- projects/sandbox/src/app/samples/column-layout-full-height/column-layout-full-height-sample.component.ts
+++ projects/sandbox/src/app/samples/column-layout-full-height/column-layout-full-height-sample.component.ts
@@ -13,18 +13,27 @@ export class ColumnLayoutFullHeightSampleComponent implements OnInit, OnDestroy 
 	wider = false;
 	private readonly masterLayoutService = inject(ObMasterLayoutService);
 
+	private originalHasLayout!: boolean;
+	private originalIsStickyFooter!: boolean;
+	private originalIsStickyHeader!: boolean;
+
 	ngOnInit(): void {
 		// Update the value in the next change detection cycle in order to
 		// avoid a ExpressionChangedAfterItHasBeenCheckedError.
 		// More information: https://angular.dev/errors/NG0100
 		setTimeout(() => {
+			this.originalHasLayout = this.masterLayoutService.layout.hasLayout;
+			this.originalIsStickyFooter = this.masterLayoutService.footer.isSticky;
+			this.originalIsStickyHeader = this.masterLayoutService.header.isSticky;
 			this.masterLayoutService.layout.hasLayout = false;
 			this.masterLayoutService.footer.isSticky = true;
+			this.masterLayoutService.header.isSticky = true;
 		}, 0);
 	}
 
 	ngOnDestroy(): void {
-		this.masterLayoutService.layout.hasLayout = true;
-		this.masterLayoutService.footer.isSticky = false;
+		this.masterLayoutService.layout.hasLayout = this.originalHasLayout;
+		this.masterLayoutService.footer.isSticky = this.originalIsStickyFooter;
+		this.masterLayoutService.header.isSticky = this.originalIsStickyHeader;
 	}
 }
