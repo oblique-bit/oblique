0061c48973170e46d28df994127343f29a57ccf5
Giller René BIT <rene.giller@bit.admin.ch>
2026-06-10 11:25:12 +0200

    refactor(oblique/material): move provider and types to dedicated files
    OUI-4518

---
+++ projects/oblique/src/lib/material/material.model.ts
@@ -0,0 +1,27 @@
+import {InjectionToken} from '@angular/core';
+import {MatFormFieldDefaultOptions} from '@angular/material/form-field';
+import {MatCheckboxDefaultOptions} from '@angular/material/checkbox';
+import {MatRadioDefaultOptions} from '@angular/material/radio';
+import {MatSlideToggleDefaultOptions} from '@angular/material/slide-toggle';
+import {MatTabsConfig} from '@angular/material/tabs';
+import {StepperOptions} from '@angular/cdk/stepper';
+
+export type ObMaterialProvider = keyof Required<ObIMaterialConfig>;
+
+export interface ObIMaterialProviderConfiguration<Type> {
+	provide: InjectionToken<Type>;
+	useValue: Type;
+}
+
+export type ObIMaterialProviders = {
+	[Property in ObMaterialProvider]: ObIMaterialProviderConfiguration<Required<ObIMaterialConfig>[Property]>;
+};
+
+export interface ObIMaterialConfig {
+	MAT_FORM_FIELD_DEFAULT_OPTIONS?: MatFormFieldDefaultOptions;
+	STEPPER_GLOBAL_OPTIONS?: StepperOptions;
+	MAT_CHECKBOX_OPTIONS?: MatCheckboxDefaultOptions;
+	MAT_RADIO_OPTIONS?: MatRadioDefaultOptions;
+	MAT_SLIDE_TOGGLE_OPTIONS?: MatSlideToggleDefaultOptions;
+	MAT_TABS_CONFIG?: MatTabsConfig;
+}
