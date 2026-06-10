4b1d8310b9e4d5f3519631d0061639b7e7d8984e
Giller René BIT <rene.giller@bit.admin.ch>
2026-06-10 11:25:12 +0200

    refactor(oblique/material): move provider and types to dedicated files
    OUI-4518

---
+++ projects/oblique/src/lib/material/index.ts
@@ -0,0 +1,1 @@
+export {ObIMaterialConfig} from './material.model';
--- projects/oblique/src/lib/utilities.model.ts
+++ projects/oblique/src/lib/utilities.model.ts
@@ -1,16 +1,10 @@
-import type {InjectionToken} from '@angular/core';
-import {MatFormFieldDefaultOptions} from '@angular/material/form-field';
-import {MatCheckboxDefaultOptions} from '@angular/material/checkbox';
-import {MatRadioDefaultOptions} from '@angular/material/radio';
-import {StepperOptions} from '@angular/cdk/stepper';
 import {ObEPamsEnvironment} from './service-navigation/service-navigation.model';
-import {MatSlideToggleDefaultOptions} from '@angular/material/slide-toggle';
-import {MatTabsConfig} from '@angular/material/tabs';
 import {TranslateModuleConfig} from '@ngx-translate/core';
 import {ObIconConfig} from './icon/icon.model';
 import {ObILocale} from './master-layout/master-layout.model';
 import {ObITranslationFile} from './multi-translate-loader/multi-translate-loader.model';
 import {ObIAccessibilityStatementConfiguration} from './accessibility-statement/accessibility-statement.model';
+import {ObIMaterialConfig} from './material/material.model';
 
 export interface ObIBanner {
 	text?: string;
@@ -24,15 +18,6 @@ export interface ObIPamsConfiguration {
 }
 
 export type ObTBanner = string | ObIBanner | undefined;
-
-export interface ObIMaterialConfig {
-	MAT_FORM_FIELD_DEFAULT_OPTIONS?: MatFormFieldDefaultOptions;
-	STEPPER_GLOBAL_OPTIONS?: StepperOptions;
-	MAT_CHECKBOX_OPTIONS?: MatCheckboxDefaultOptions;
-	MAT_RADIO_OPTIONS?: MatRadioDefaultOptions;
-	MAT_SLIDE_TOGGLE_OPTIONS?: MatSlideToggleDefaultOptions;
-	MAT_TABS_CONFIG?: MatTabsConfig;
-}
 
 export interface ObIObliqueConfiguration {
 	accessibilityStatement: ObIAccessibilityStatementConfiguration;
@@ -48,17 +33,6 @@ export interface ObIHistoryState {
 
 export type ObIObliqueTestingConfiguration = Omit<ObIObliqueConfiguration, 'accessibilityStatement'> & {
 	accessibilityStatement?: ObIAccessibilityStatementConfiguration;
-};
-
-export type ObMaterialProvider = keyof Required<ObIMaterialConfig>;
-
-export interface ObIMaterialProviderConfiguration<Type> {
-	provide: InjectionToken<Type>;
-	useValue: Type;
-}
-
-export type ObIMaterialProviders = {
-	[Property in ObMaterialProvider]: ObIMaterialProviderConfiguration<Required<ObIMaterialConfig>[Property]>;
 };
 
 export type ObIObliqueConfigurationWithDefaults = Omit<
