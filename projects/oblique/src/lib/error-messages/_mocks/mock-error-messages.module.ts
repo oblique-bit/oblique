44cce4e8556f8852b6098099dba1da95ab3489eb
Martin Krutzfeld AMT <steve-martin.krutzfeld@bit.admin.ch>
2026-06-15 23:03:19 +0200

    feat(oblique/error-messages): remove ObErrorMessagesService
    OUI-4499
    BREAKING CHANGE:
    * ObErrorMessagesService has been removed without replacement

--- projects/oblique/src/lib/error-messages/_mocks/mock-error-messages.module.ts
+++ projects/oblique/src/lib/error-messages/_mocks/mock-error-messages.module.ts
@@ -4,7 +4,6 @@ import {ObMockMatErrorDirective} from './mock-mat-error.directive';
 import {ObMockErrorMessagesComponent} from './mock-error-messages.component';
 import {ObMockErrorMessagesDirective} from './mock-error-messages.directive';
 import {ObMockErrorMessagesService} from './mock-error-messages.service';
-import {ObErrorMessagesService} from '../error-messages.service';
 
 export {ObMockMatErrorDirective} from './mock-mat-error.directive';
 export {ObMockErrorMessagesComponent} from './mock-error-messages.component';
@@ -16,7 +15,7 @@ export {ObMockErrorMessagesService} from './mock-error-messages.service';
  */
 @NgModule({
 	imports: [ObMockErrorMessagesComponent, ObMockErrorMessagesDirective, ObMockMatErrorDirective],
-	providers: [{provide: ObErrorMessagesService, useClass: ObMockErrorMessagesService}],
+	providers: [ObMockErrorMessagesService],
 	exports: [ObMockErrorMessagesComponent, ObMockErrorMessagesDirective, ObMockMatErrorDirective],
 })
 export class ObMockErrorMessagesModule {}
