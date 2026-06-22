1e3683c384c11324b8056d886bd4d2313658ae6d
Martin Krutzfeld AMT <steve-martin.krutzfeld@bit.admin.ch>
2026-06-22 14:09:37 +0200

    test(oblique/nav-tree): cover filtering branches
    OUI-4534

--- projects/oblique/jest.config.js
+++ projects/oblique/jest.config.js
@@ -23,12 +23,6 @@ module.exports = {
 		'projects/oblique/src/lib/language/language.service.ts': {
 			branches: 95,
 		},
-		'projects/oblique/src/lib/nav-tree/*.ts': {
-			statements: 91,
-			branches: 70,
-			functions: 96,
-			lines: 91,
-		},
 		'projects/oblique/src/lib/nested-form/*.ts': {
 			statements: 66,
 			branches: 90,
