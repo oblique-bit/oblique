fbfbbd164223b8e0422c0469d7ecbf1e859c41c0
Giller René BIT <rene.giller@bit.admin.ch>
2026-06-29 18:47:32 +0200

    refactor(toolchain/schematics): simplify `ng-add` and its tests
    OUI-4159
    
    # Conflicts:
    #	projects/toolchain/src/schematics/ng-add/rules/add-browserslistrc.spec.ts

--- projects/toolchain/src/schematics/ng-add/rules/add-browserslistrc.rule.ts
+++ projects/toolchain/src/schematics/ng-add/rules/add-browserslistrc.rule.ts
@@ -1,15 +1,15 @@
-import type {Rule, Tree} from '@angular-devkit/schematics';
+import {type Rule, type Tree, template} from '@angular-devkit/schematics';
 import type {ObGroupLogger} from '../../../logger';
-import {createFromTemplate} from '../utils';
-
-const browserslistrcFileName = '.browserslistrc';
+import {createFromTemplate} from '../../shared/template/template';
 
 export function addBrowserslistrc(logger: ObGroupLogger): Rule {
 	return (tree: Tree) => {
+		const browserslistrcFileName = '.browserslistrc';
 		if (tree.exists(browserslistrcFileName)) {
 			return tree;
 		}
+
 		logger.step(`Create "${browserslistrcFileName}" file`);
-		return createFromTemplate(browserslistrcFileName, browserslistrcFileName, tree);
+		return createFromTemplate('./templates', [template({})]);
 	};
 }
