daf4544ddf9ff9cdb3ad254381dde554bc349158
Giller René BIT <rene.giller@bit.admin.ch>
2026-06-30 20:17:33 +0200

    refactor(toolchain/schematics): remove unused code
    OUI-4159

--- projects/toolchain/src/schematics/test-utils.ts
+++ projects/toolchain/src/schematics/test-utils.ts
@@ -1,39 +1,7 @@
 import {join} from 'node:path';
-import type * as fs from 'fs';
 import {readFileSync, readdirSync} from 'fs';
-import type {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
-import type {Rule, Tree} from '@angular-devkit/schematics';
+import type {Tree} from '@angular-devkit/schematics';
 import * as template from './shared/template/template';
-
-type FsFunctions = keyof Pick<
-	typeof fs,
-	'readFileSync' | 'existsSync' | 'writeFileSync' | 'unlinkSync' | 'mkdirSync' | 'readdirSync'
->;
-
-type FsMocks = Partial<Record<FsFunctions, jest.Mock>>;
-
-export function mockFs(...functionsToMock: FsFunctions[]): FsMocks {
-	const mocks: FsMocks = {};
-
-	for (const fn of functionsToMock) {
-		mocks[fn] = jest.fn();
-	}
-
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-	jest.mock('fs', () => ({
-		...jest.requireActual('fs'),
-		...mocks,
-	}));
-
-	return mocks;
-}
-
-export async function runRule(runner: SchematicTestRunner, rule: Rule, tree: UnitTestTree): Promise<UnitTestTree> {
-	return new Promise((resolve, reject) => {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
-		runner.callRule(rule, tree).subscribe({next: treeResolve => resolve(treeResolve as UnitTestTree), error: reject});
-	});
-}
 
 /**
  * In tests based on {@link callRule}, the SchematicContext do not contain the template directory, meaning that
