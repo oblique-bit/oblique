5e09a0e0e608b2656592a5c1efed1ee8068202d6
Nina Egger <nina.egger@bit.admin.ch>
2026-07-03 14:32:33 +0200

    test(toolchain/schematics): fix createFromTemplate tests on Windows
    OUI-4159

--- projects/toolchain/src/schematics/shared/template/template.index.ts
+++ projects/toolchain/src/schematics/shared/template/template.index.ts
@@ -1,15 +1,14 @@
 import {type Rule, type SchematicContext, type Tree, chain, template} from '@angular-devkit/schematics';
 import {createFromTemplate} from './template';
-import path from 'path';
 import type {ObSchemaOptions} from '../types';
 
 export function templateIndex(): Rule {
 	return (tree: Tree, context: SchematicContext) => {
-		return chain([createFromTemplate(path.join(__dirname, 'templates'))])(tree, context);
+		return chain([createFromTemplate('./templates')])(tree, context);
 	};
 }
 export function templateOptions(options: ObSchemaOptions): Rule {
 	return (tree: Tree, context: SchematicContext) => {
-		return chain([createFromTemplate(path.join(__dirname, 'templates'), [template(options)])])(tree, context);
+		return chain([createFromTemplate('./templates', [template(options)])])(tree, context);
 	};
 }
