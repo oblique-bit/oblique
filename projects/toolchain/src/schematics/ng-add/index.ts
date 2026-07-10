d8f1df890705acbf0a1583abd43b323b8f1c3753
Nicola Pfister <nicola.pfister@bit.admin.ch>
2026-07-10 19:37:51 +0200

    feat(toolchain/schematics): add proxy.conf.json creation to ng-add schematic
    OUI-4525

--- projects/toolchain/src/schematics/ng-add/index.ts
+++ projects/toolchain/src/schematics/ng-add/index.ts
@@ -4,10 +4,16 @@ import {closeLogger} from '../shared/logger-close';
 import {addBrowserslistrc} from './rules/add-browserslistrc';
 import type {NgAddOptions} from './types';
 import addNpmrc from './rules/add-npmrc';
+import addProxy from './rules/add-proxy';
 
 export function toolchain(options: NgAddOptions): Rule {
 	return (tree: Tree, context: SchematicContext) => {
 		const logger = obCreateSchematicsLogger(context).group('Add @oblique/toolchain');
-		return chain([addBrowserslistrc(logger), addNpmrc(logger, options.npmrc), closeLogger(logger)])(tree, context);
+		return chain([
+			addBrowserslistrc(logger),
+			addNpmrc(logger, options.npmrc),
+			addProxy(logger, options.proxy),
+			closeLogger(logger),
+		])(tree, context);
 	};
 }
