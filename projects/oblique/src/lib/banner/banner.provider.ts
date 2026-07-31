d95946df6cd765d35d5193abfd015d6825641c4e
Olivier Percebois-Garve <olivier.percebois-garve@bit.admin.ch>
2026-07-31 18:44:36 +0200

    feat(oblique): deprecate `OB_BANNER`
    OUI-4574

--- projects/oblique/src/lib/banner/banner.provider.ts
+++ projects/oblique/src/lib/banner/banner.provider.ts
@@ -1,6 +1,9 @@
 import {InjectionToken, Provider} from '@angular/core';
 import {ObTBanner} from './banner.model';
 
+/**
+ *  @deprecated since Oblique 16.0.0. To be removed in Oblique 17.0.0. Use provideObliqueConfiguration instead.
+ */
 export const OB_BANNER = new InjectionToken<ObTBanner>('Banner');
 
 export function obProvideBanner(config: ObTBanner): Provider[] {
