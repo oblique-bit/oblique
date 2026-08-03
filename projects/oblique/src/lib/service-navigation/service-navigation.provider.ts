5b6f2929a91cc2238713306d1116c3f45a05e45e
Giller René BIT <rene.giller@bit.admin.ch>
2026-08-03 11:04:57 +0200

    feat(oblique/service-navigation): deprecate `OB_PAMS_CONFIGURATION`
    OUI-4575

--- projects/oblique/src/lib/service-navigation/service-navigation.provider.ts
+++ projects/oblique/src/lib/service-navigation/service-navigation.provider.ts
@@ -3,6 +3,9 @@ import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {ObIPamsConfiguration} from './service-navigation.model';
 import {ObEportalCsrfInterceptor} from './eportal-csrf-interceptor/eportal-csrf-interceptor';
 
+/**
+ * @deprecated since 16.0.0, will be removed with version 17. Use provideObliqueConfiguration.pams instead
+ */
 export const OB_PAMS_CONFIGURATION = new InjectionToken<ObIPamsConfiguration>(
 	'Provides the mandatory PAMS environment as well as an optional root url.'
 );
