b03ab8fef33d5ac8a1a9d81552c7b5f1ad106b70
Nicola Pfister <nicola.pfister@bit.admin.ch>
2026-08-27 15:43:53 +0200

    refactor(oblique/service-navigation): migrate authentication widget to signals and `OnPush`
    OUI-4559

--- projects/oblique/src/lib/service-navigation/authentication/service-navigation-authentication.component.ts
+++ projects/oblique/src/lib/service-navigation/authentication/service-navigation-authentication.component.ts
@@ -1,16 +1,15 @@
-import {ChangeDetectionStrategy, Component, Input, ViewEncapsulation, input, output} from '@angular/core';
+import {Component, ViewEncapsulation, input, output} from '@angular/core';
 
 @Component({
 	selector: 'ob-service-navigation-authentication',
 	standalone: false,
 	templateUrl: './service-navigation-authentication.component.html',
 	styleUrls: ['./service-navigation-authentication.component.scss'],
-	changeDetection: ChangeDetectionStrategy.Eager,
 	encapsulation: ViewEncapsulation.None,
 	host: {class: 'ob-service-navigation-authentication'},
 })
 export class ObServiceNavigationAuthenticationComponent {
-	@Input() loginUrl = '';
+	readonly loginUrl = input('');
 	readonly isLoggedIn = input(false);
 	readonly logoutClicked = output();
 
