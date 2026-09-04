a3ee79f4afe582aad0fbf222cad8473da2e7dc6c
Giller René BIT <rene.giller@bit.admin.ch>
2026-09-04 10:25:47 +0200

    refactor(oblique/master-layout): remove locale from `ObMockMasterLayoutConfig`
    `locale` has already been removed from `ObMasterLayoutConfig`, but the mock has been forgotten
    
    OUI-4499

--- projects/oblique/src/lib/master-layout/_mocks/mock-master-layout.config.ts
+++ projects/oblique/src/lib/master-layout/_mocks/mock-master-layout.config.ts
@@ -5,7 +5,6 @@ import {
 	ObIMasterLayoutHeader,
 	ObIMasterLayoutNavigation,
 } from '../master-layout.model';
-import {ObILocale} from '../../language/language.model';
 
 /**
  *  @deprecated since Oblique 11. No removal version is planned. Use the real instances instead
@@ -15,17 +14,6 @@ export class ObMockMasterLayoutConfig {
 	homePageRoute = '/home';
 	scrollToTopDuration = 200;
 	showAccessibilityTitle = true;
-	locale: ObILocale = {
-		locales: ['de-CH', 'fr-CH', 'it-CH'],
-		defaultLanguage: 'de',
-		disabled: false,
-		languages: {
-			de: 'Deutsch',
-			fr: 'Français',
-			it: 'Italiano',
-			en: 'English',
-		},
-	};
 	layout = {
 		hasCover: false,
 		hasMainNavigation: true,
